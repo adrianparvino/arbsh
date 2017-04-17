b860c370191909ef9d9227e4cfed8ba5abf7fb91
cmgraff <cm0graff@gmail.com>
2017-04-17 15:56:10 -0500

    [arbprec] dynamically allocate a bigflt from a string

--- new_arbprec/src/_bigflt_alloc.c
+++ new_arbprec/src/_bigflt_alloc.c
@@ -1,9 +1,40 @@
 #include <arbprec/arbprec.h>
 
+bigflt *str_to_bigflt(const char *str)
+{
+	size_t i = 0;
+	size_t chunk = 512;
+	bigflt *ret = arbprec_malloc(sizeof(bigflt));
+
+	ret->len = 0;
+	ret->allocated = chunk;
+	ret->number = arbprec_malloc(sizeof(int) * ret->allocated);
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (str[i] == '.')
+			ret->float_pos = i;
+		else if (str[i] == '+')
+			ret->sign = '+';
+		else if (str[i] == '-')
+			ret->sign = '-';
+		else {
+			if ( ret->len == ret->allocated)
+			{
+				ret->allocated += chunk;
+				ret->number = arbprec_realloc(ret->number, ret->allocated);
+			}
+			ret->number[ret->len++] = str[i] - '0';
+		}
+		
+	}
+	return ret;
+}
+
 bigflt *arba_alloc(size_t len)
 {
-	bigflt *ret = malloc(sizeof(bigflt));
-	ret->number = malloc(sizeof(int) * len);
+	bigflt *ret = arbprec_malloc(sizeof(bigflt));
+	ret->number = arbprec_malloc(sizeof(int) * len);
 	ret->sign = '+';
 	ret->float_pos = 0;
 	ret->len = len;
