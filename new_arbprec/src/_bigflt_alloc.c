d3a098eebd8fc2cd1826e6c6ad4bc74cb99f7773
cmgraff <cm0graff@gmail.com>
2017-04-18 07:05:24 -0500

    [arbprec] print a bigflt

--- new_arbprec/src/_bigflt_alloc.c
+++ new_arbprec/src/_bigflt_alloc.c
@@ -2,25 +2,51 @@
 
 void arbprec_print(bigflt *flt)
 {
-	size_t i = 0;
-	printarray(flt->number, flt->len);
-		
-}
+
+        size_t i = 0;
+	if ( flt->sign )
+		printf("%c", flt->sign);
+
+        for ( i = 0; i < flt->len ; ++i)
+	{
+		if ( flt->float_pos == i )
+			printf("."); 
+		else
+			printf("%d", flt->number[i]);
+	}
+
+        printf("\n");
+} 
 
 bigflt *str_to_bigflt(const char *str)
 {
+	/*
+		convert a string to a bigflt and attempt a series of chunk 
+		based `realloc's to hold the generated values.
+
+		Try to keep the chunk size alligned with future devices
+		by aligning with a size macro SIZE_MACRO / 16
+	*/
+
 	size_t i = 0;
-	size_t chunk = 256;
+	size_t chunk = BUFSIZ / 16;
+	int flt_set = 0;
 	bigflt *ret = arbprec_malloc(sizeof(bigflt));
 
+	ret->sign = '+';
 	ret->len = 0;
 	ret->allocated = chunk;
 	ret->number = arbprec_malloc(sizeof(int) * ret->allocated);
+	
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{
+		
 		if (str[i] == '.')
+		{
+			flt_set = 1;
 			ret->float_pos = i;
+		}
 		else if (str[i] == '+')
 			ret->sign = '+';
 		else if (str[i] == '-')
@@ -37,6 +63,8 @@ bigflt *str_to_bigflt(const char *str)
 			ret->number[ret->len++] = str[i] - '0';
 		}
 	}
+	if ( flt_set == 0 ) /* no float */
+		ret->float_pos = ret->len + 1;
 	return ret;
 }
 
