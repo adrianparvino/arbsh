a0203ebbf3fb75b78eb55ceb3d3457703a2ea602
cmgraff <cm0graff@gmail.com>
2017-04-18 12:02:42 -0500

    [arbprec] add two bigflt

--- new_arbprec/src/addition.c
+++ new_arbprec/src/addition.c
@@ -1,6 +1,35 @@
 #include <arbprec/arbprec.h> 
 
 /* Functions */ 
+
+int *arbprec_add(bigflt *a, bigflt *b, bigflt *c)
+{
+	size_t width = 0;
+	int sum = 0;
+	int carry = 0;
+	c->len = 0;
+
+	width = MAX(a->len, a->len);
+	
+        for( c->len = 0; c->len < width ; c->len++)
+	{
+		sum = hasplace(a->number, c->len, a->len) + hasplace(b->number, c->len, b->len) + carry;
+                carry = 0;
+                if(sum >= base){
+                        carry = 1;
+                        sum -= base;
+                }
+		/* add a vector resize here */
+                c->number[c->len] = sum;
+        }
+        if (carry)
+		c->number[c->len++] = 1;
+       
+	arbprec_reverse(c->number, c->len);
+	return c;
+}
+
+
 int *addition(int *a, int *b, int *c)
 {
 	/* 
