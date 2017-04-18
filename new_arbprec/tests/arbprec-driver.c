570672698c669576237a4bd7661b87502a265b3d
cmgraff <cm0graff@gmail.com>
2017-04-18 15:38:46 -0500

    [arbprec] div, mul, add and sub working

--- new_arbprec/tests/arbprec-driver.c
+++ new_arbprec/tests/arbprec-driver.c
@@ -24,8 +24,6 @@ int main(int argc, char **argv)
 	printf("/ %20lf\n", a / b);
 
 
-
-	
 	bigflt *flt1 = str_to_bigflt(argv[1]);
 	bigflt *flt2 = str_to_bigflt(argv[2]);
 	bigflt *flt3 = arba_alloc(1000);
@@ -37,13 +35,10 @@ int main(int argc, char **argv)
 	arbprec_print(flt3);
 
 	flt3 = arbprec_mul(flt1, flt2, flt3);
-	arbprec_print(flt3);
-
+	arbprec_print(flt3); 
 	
 	flt3 = arbprec_div(flt1, flt2, flt3);
 	arbprec_print(flt3);
-
-
 
 	return 0;
 }
