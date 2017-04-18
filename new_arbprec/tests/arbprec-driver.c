7cc4596d2855e334b6d4a3e1cebc6462f3d35ec4
cmgraff <cm0graff@gmail.com>
2017-04-18 13:18:33 -0500

    [arbprec] only use the bigflt functions

--- new_arbprec/tests/arbprec-driver.c
+++ new_arbprec/tests/arbprec-driver.c
@@ -7,36 +7,15 @@
 int base = 10; 		/* global base */
 
 int main(int argc, char **argv)
-{ 
-	
-	int o = 0;
-	double a = 0;
-	double b = 0;
-
-	while ((o = getopt (argc, argv, "vb:")) != -1)
-		switch (o) { 
-			case 'v':
-				verbosity(NULL, NULL, 1, 0);
-				break;
-			case 'b': /* Override base */
-				base = strtoul(optarg, 0, 10);
-				break;
-			default:
-				break;
-		}
-
-	argv += optind;
-	argc -= optind;
-
+{
 
 	if ( argc < 2 )
 		die("Needs 2 args\n");
 
-	a = strtod(argv[0], 0);
-	b = strtod(argv[1], 0);
+	double a = strtod(argv[1], 0);
+	double b = strtod(argv[2], 0);
 
 	printf("  %20lf\n", a);
-
 	printf("  %20lf\n", b);
 	printf("  %20s\n", "-------------------");
 	printf("+ %20lf\n", a + b);
@@ -44,9 +23,11 @@ int main(int argc, char **argv)
 	printf("* %20lf\n", a * b);
 	printf("/ %20lf\n", a / b);
 
+
+
 	
-	bigflt *flt1 = str_to_bigflt(argv[0]);
-	bigflt *flt2 = str_to_bigflt(argv[1]);
+	bigflt *flt1 = str_to_bigflt(argv[1]);
+	bigflt *flt2 = str_to_bigflt(argv[2]);
 	bigflt *flt3 = arba_alloc(1000);
 
 	flt3 = arbprec_add(flt1, flt2, flt3);
@@ -61,3 +42,4 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
