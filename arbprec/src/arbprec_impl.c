09b4cd7a6a02c9388e4b0a6ccb23dcb32a869606
cmgraff <cm0graff@gmail.com>
2017-04-25 16:15:32 -0500

    [arbprec] alphabetize

--- arbprec/src/arbprec_impl.c
+++ arbprec/src/arbprec_impl.c
@@ -1,11 +1,38 @@
 #include <arbprec/arbprec.h>
 
-/* Functions */
+/* Functions */ 
+void arbprec_die(char *message)
+{
+	fprintf(stderr, "%s", message);
+	exit(1);
+} 
+
+int arbprec_isnegati(bigflt *flt)
+{
+        if ( flt->sign == '-' )
+                return 1;
+        return 0;
+}
+
+void arbprec_reverse(int *x, size_t lim)
+{
+        size_t i = 0;
+        int swap = 0;
+        size_t half = lim / 2;
+
+        for ( ; i < half ; i++)
+        {
+                swap = x[i];
+                x[i] = x[lim - i - 1];
+                x[lim - i - 1] = swap;
+        }
+}
+
 void *arbprec_malloc(size_t len)
 {
 	void *ret;
 	if(!(ret = malloc(len)))
-		die("malloc failed\n"); 
+		arbprec_die("malloc failed\n"); 
 	return ret;
 }
 
@@ -13,7 +40,7 @@ void *arbprec_realloc(void *ptr, size_t len)
 {
 	void *ret;
 	if(!(ret = realloc(ptr, len)))
-		die("realloc failed\n"); 
+		arbprec_die("realloc failed\n"); 
 	return ret;
 }
 
@@ -23,24 +50,6 @@ void copyarray(int *answer, int *from, size_t len)
 	for( i = 0; i < len ; i++)
 		answer[i] = from[i];
 }
-
-void die(char *message)
-{
-	fprintf(stderr, "%s", message);
-	exit(1);
-}
-
-int hasplace2(int *s, size_t idx, size_t len, size_t float_pos)
-{ 
-	//if ( (len - float_pos) > (len-idx))
-	//	return 0;
-	if ( idx < len - float_pos)
-		return 0;
-        if (idx < len )
-        	return s[len - idx - 1 ];
-        return 0;
-}
-
 int hasplace(int *s, size_t idx, size_t len)
 {
         if (idx < len)
@@ -63,44 +72,3 @@ void setarray(int *answer, int delim, size_t len)
 	for( i = 0; i < len; i++)
 		answer[i] = delim;
 }
-
-bigflt *arbprec_copy(bigflt *dest, bigflt *src)
-{
-	dest = arbprec_expand_vector(dest, src->len);
-	copyarray(dest->number, src->number, src->len);
-	copyarray(dest->mirror, src->mirror, src->len);
-	dest->sign = src->sign;
-	/* these should already have been set by expand_vector */
-	dest->len = src->len;
-	dest->chunk = src->chunk;
-	dest->allocated = src->allocated;
-	dest->float_pos = src->float_pos;
-
-	return dest;
-}
-
-bigflt *arbprec_copy_sparse(bigflt *dest, bigflt *src)
-{
-	dest->sign = src->sign;
-	dest->number = src->number;
-	dest->mirror = src->mirror;
-	dest->len = src->len;
-	dest->chunk = src->chunk;
-	dest->allocated = src->allocated;
-	dest->float_pos = src->float_pos;
-	return dest;
-}
-
-bigflt *arbprec_dupa(bigflt *flt)
-{ 
-	bigflt *ret = arbprec_malloc(sizeof(bigflt));
-	arbprec_copy(ret, flt);
-	return ret;
-}
-bigflt *arbprec_dup_sparse(bigflt *flt)
-{
-	bigflt *ret = arbprec_expand_vector(NULL, flt->len); 
-	arbprec_copy_sparse(ret, flt);
-	return ret;
-}
-
