ed686a5b955ae973e9f6b99f4c1da5e9a2cd312f
cmgraff <cm0graff@gmail.com>
2017-04-27 17:33:24 -0500

    [posix] have cat only close files that it explicitly opened

--- posix/cat.c
+++ posix/cat.c
@@ -4,21 +4,21 @@
 #include <fcntl.h> 
 
 /* 
-	Copyright 2017, CM Graff, `cat'
+	Copyright 2014-2017, `cat' "cat.c", Christopher M. Graff
 */ 
 
-int concatenate(int);
+int concatenate(int, int);
 
 int main(int argc, char *argv[])
 {
 	if ( argc == 1 ) 
-		concatenate(0); 
+		concatenate(0, 0); 
         while  (*++argv) 
-		concatenate(open(*argv, O_RDONLY)); 
+		concatenate(open(*argv, O_RDONLY), 1); 
 	return 0; 
 }
 
-int concatenate(int source)
+int concatenate(int source, int opened)
 {
         ssize_t n = 0;
         char buf[4096];
@@ -32,7 +32,7 @@ int concatenate(int source)
 	if ( n == -1 )
 		return -1;
 
-	if (source != 1)
+	if (opened)
         	close(source);
 
 	return 1;
