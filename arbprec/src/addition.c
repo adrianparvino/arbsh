99b7b7c184a2f7f3f0b00a429511272bad255242
cmgraff <cm0graff@gmail.com>
2017-04-28 07:15:26 -0500

    [arbprec] concept for radix support in addition bulked out

--- arbprec/src/addition.c
+++ arbprec/src/addition.c
@@ -2,17 +2,50 @@
 
 bigflt *arbprec_add_inter(bigflt *a, bigflt *b, bigflt *c)
 {
+
+	
 	size_t width = 0;
 	int sum = 0;
 	int carry = 0;
-	
-	width = MAX(a->len, b->len); 
-	//c->float_pos = a->float_pos; 
-	c->len = 0;
 
-        for( c->len = 0; c->len < width ; c->len++)
+	size_t radix_right_a = (a->len - a->float_pos);
+	size_t radix_right_b = (b->len - b->float_pos);
+	size_t diff = 0;
+	c->len = 0;
+	width = MAX(a->len, b->len); 
+
+	if( radix_right_a > radix_right_b ) 
 	{
-		sum = hasplace(a->number, c->len, a->len) + hasplace(b->number, c->len, b->len) + carry;
+		diff =  radix_right_a - radix_right_b; 
+		size_t lim = a->len;
+		size_t limi = diff;
+		for (; limi> 0; --limi)
+			c->number[c->len++] = a->number[a->len-- - 1]; 
+
+		c->float_pos = b->len;
+	
+	}
+	else if ( radix_right_b > radix_right_a )
+	{
+		diff = radix_right_b  - radix_right_a; 
+		size_t lim = b->len;
+		size_t limi= diff;
+		for (; limi> 0; --limi)
+			c->number[c->len++] = b->number[b->len-- - 1]; 
+		c->float_pos = a->len;
+	}
+
+	
+
+	//
+
+	size_t i = 0;
+
+
+        for( ; c->len < width ; c->len++, i++)
+	{
+		sum = hasplace(a->number, i, a->len) + hasplace(b->number, i, b->len) + carry;
+		
                 carry = 0;
                 if(sum >= base){
                         carry = 1;
@@ -22,7 +55,7 @@ bigflt *arbprec_add_inter(bigflt *a, bigflt *b, bigflt *c)
         }
         if (carry) 
 		c->number[c->len++] = 1;
-       
+       	//c->len += 5;
 	arbprec_reverse(c->number, c->len);
 	return c;
 }
