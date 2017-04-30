f0628c5c85d64cb65700acb3f6bdb3376dfe6e79
cmgraff <cm0graff@gmail.com>
2017-04-29 21:33:50 -0500

    [arbprec] use functions to reveal radix point logic

--- arbprec/src/addition.c
+++ arbprec/src/addition.c
@@ -1,64 +1,40 @@
-
-#include <arbprec/arbprec.h> 
-
-size_t helper(bigflt *a, bigflt *b, bigflt *c)
-{
-	size_t radix_right_a = (a->len - a->float_pos);
-	size_t radix_right_b = (b->len - b->float_pos);
-	size_t diff = radix_right_a - radix_right_b; 
-	size_t limi = diff;
-	size_t lim = a->len -1;
-
-	for (; limi> 0; limi--, c->len++, lim--) 
-		c->number[c->len] = a->number[lim]; 
-
-	c->float_pos = MAX(a->float_pos, b->float_pos);
-	return diff;
-}
+#include <arbprec/arbprec.h>
 
 bigflt *arbprec_add_inter(bigflt *a, bigflt *b, bigflt *c)
 { 
-	size_t width = 0;
+	size_t width = 0; 
+	size_t diff = 0;
+	size_t i = 0;
+	size_t al = a->len;
+	size_t bl = b->len;
 	int sum = 0;
 	int carry = 0;
 
-	size_t radix_right_a = (a->len - a->float_pos);
-	size_t radix_right_b = (b->len - b->float_pos);
-	size_t diff = 0;
-	size_t i = 0;
-	
+	c->float_pos = MAX(rl(a), rl(b));
 	c->len = 0;
-	width = MAX(a->len, b->len);
 
-	size_t al = a->len;
-	size_t bl = b->len;
-	c->float_pos = MAX(a->float_pos, b->float_pos);
-
-	if( radix_right_a > radix_right_b ) 
-	{
-		diff = helper(a, b, c);
+	if( rr(a) > rr(b))
+	{ 
+		diff = arbprec_balance_sum(a, b, c, diff);
 		al = a->len - diff;
 	}
-	else if ( radix_right_b > radix_right_a ) 
+	else if ( rr(b) > rr(a))
 	{
-	
-		diff = helper(b, a, c);
+		diff = arbprec_balance_sum(b, a, c, diff);
 		bl = b->len - diff;
 	} 
+
 	width = MAX(al, bl);
 	
         for( ; c->len < width + diff ; c->len++, i++)
-	{
-		//arbprec_expand_vector(c, c->len + i );
+	{ 
 		sum = hasplace(a->number, i, al) + hasplace(b->number, i, bl) + carry;
-		
                 carry = 0;
                 if(sum >= base){
                         carry = 1;
                         sum -= base;
                 }
                 c->number[c->len] = sum;
-		//arbprec_print(c);
         }
         if (carry) 
 		c->number[c->len++] = 1;
