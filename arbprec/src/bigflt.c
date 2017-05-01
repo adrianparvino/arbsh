38c0f829af82367b160bc98d1a32d85434f30efc
cmgraff <cm0graff@gmail.com>
2017-05-01 09:41:28 -0500

    [arbprec] tests for hypot

--- arbprec/src/bigflt.c
+++ arbprec/src/bigflt.c
@@ -1,5 +1,6 @@
 #include <arbprec/arbprec.h>
 
+
 bigflt *arbprec_initsign(bigflt *flt)
 {
 	/* 	(internal)
@@ -40,13 +41,23 @@ bigflt *arbprec_print_simple(bigflt *flt)
 		Print a bigflt
 	*/
         size_t i = 0; 
+	
+	if ( flt->sign )
+		printf("%c", flt->sign);
+
+	if (flt->nan == 1)
+	{
+		printf("nan\n");
+		return flt;
+	}
 
         for (i = 0; i < flt->len ; ++i)
 	{ 
 		if ( flt->float_pos == i ) 
 			printf("."); 
 		printf("%d", flt->number[i]); 
-	} 
+	}
+	printf("\n");
 	return flt;
 }
 
@@ -330,3 +341,94 @@ bigflt *arbprec_add_precision(bigflt *flt, size_t len)
 	return flt;
 	
 }
+#include <arbprec/arbprec.h>
+
+/* Functions */ 
+void arbprec_die(char *message)
+{
+	fprintf(stderr, "%s", message);
+	exit(1);
+} 
+
+int arbprec_isnegati(bigflt *flt)
+{
+        if ( flt->sign == '-' )
+                return 1;
+        return 0;
+}
+
+void arbprec_reverse(int *x, size_t lim)
+{
+        size_t i = 0;
+        int swap = 0;
+        size_t half = lim / 2;
+
+        for ( ; i < half ; i++)
+        {
+                swap = x[i];
+                x[i] = x[lim - i - 1];
+                x[lim - i - 1] = swap;
+        }
+}
+
+void *arbprec_malloc(size_t len)
+{
+	void *ret;
+	if(!(ret = malloc(len)))
+		arbprec_die("malloc failed\n"); 
+	return ret;
+}
+
+void *arbprec_realloc(void *ptr, size_t len)
+{
+	void *ret;
+	if(!(ret = realloc(ptr, len)))
+		arbprec_die("realloc failed\n"); 
+	return ret;
+}
+
+void copyarray(int *answer, int *from, size_t len)
+{ 
+	size_t i = 0;
+	for( i = 0; i < len ; i++)
+		answer[i] = from[i];
+}
+
+int hasplace2(int *s, size_t idx, size_t len, size_t flt)
+{
+	// past a radix 
+
+	if ( len > flt)
+	{
+		if ( len - idx > len)
+			return 0;
+	}
+        if (idx < len)
+        	return s[len - idx - 1];
+        return 0;
+} 
+
+
+int hasplace(int *s, size_t idx, size_t len)
+{
+        if (idx < len)
+        	return s[len - idx - 1];
+        return 0;
+} 
+
+int iszero(int *answer, size_t lim)
+{
+	size_t i = 0;
+	for ( i = 0; i < lim ; i++)
+		if ( answer[i] )
+			return 1;
+	return 0;
+}
+
+void setarray(int *answer, int delim, size_t len)
+{
+	size_t i = 0;
+	for( i = 0; i < len; i++)
+		answer[i] = delim;
+}
+
