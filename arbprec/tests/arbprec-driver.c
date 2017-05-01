27dab47091e0d5013e0d95590bbfe72f4541bcaf
cmgraff <cm0graff@gmail.com>
2017-05-01 00:59:41 -0500

    [arbprec] sqrt is passing basic tests

--- arbprec/tests/arbprec-driver.c
+++ arbprec/tests/arbprec-driver.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 #include <arbprec/arbprec.h> 
 
 
 int main(int argc, char **argv)
 {
-	//scale = 100;
-
+	scale = 10;
+	
 	if ( argc < 3 )
 		arbprec_die("Needs 2 args\n");
 
@@ -23,18 +24,19 @@ int main(int argc, char **argv)
 	printf("- %20.10lf\n", a - b);
 	printf("* %20.10lf\n", a * b);
 	printf("/ %20.10lf\n", a / b);
+	printf("# %20.10lf\n", sqrt(a));
 	printf("\n\narbprec:\n___________\n\n");
 	fflush(stdout);
 
 	bigflt *flt1 = str_to_bigflt(argv[1]);
 	bigflt *flt2 = str_to_bigflt(argv[2]);
 
-	flt1 = arbprec_add_precision(flt1, scale);
-	arbprec_match_precision(flt1, flt2);
+	//flt1 = arbprec_add_precision(flt1, scale);
+	//arbprec_match_precision(flt1, flt2);
 
 
 	bigflt *flt3 = arbprec_expand_vector(NULL, flt1->len + flt2->len);
-
+	/*
 	flt3 = arbprec_add(flt1, flt2, flt3);
 	arbprec_print(flt3);
 
@@ -46,10 +48,14 @@ int main(int argc, char **argv)
 	
 	flt3 = arbprec_div(flt1, flt2, flt3);
 	arbprec_print(flt3);
+	*/
+	//flt1 = arbprec_add_precision(flt1, scale);
+	flt3 = arbprec_sqrt(flt1);
+	arbprec_print(flt3);
 
-	arba_free(flt1);
-	arba_free(flt2);
-	arba_free(flt3);
+	//arba_free(flt1);
+	//arba_free(flt2);
+	//arba_free(flt3);
 
 	printf("\n\n");
 
