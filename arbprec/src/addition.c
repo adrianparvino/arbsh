4b65e6095377f11a6922718c80a19d457c76056e
cmgraff <cm0graff@gmail.com>
2017-04-30 20:53:40 -0500

    [arbprec] addition identity bug fixed

--- arbprec/src/addition.c
+++ arbprec/src/addition.c
@@ -40,7 +40,7 @@ bigflt *arbprec_add_inter(bigflt *a, bigflt *b, bigflt *c)
 	{
 		c->number[c->len++] = 1;
 		c->float_pos += 1;
-		c->len--;
+		//c->len--;
 	}
 
 	arbprec_reverse(c->number, c->len);
