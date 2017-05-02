4359a1431b6b4717093b4e732ca89f8ced5a6114
cmgraff <cm0graff@gmail.com>
2017-05-02 14:27:52 -0500

    [arbprec] continued fraction exp bulked out

---
+++ arbprec/src/exp.c
@@ -0,0 +1,66 @@
+#include <arbprec/arbprec.h>
+
+bigflt *arbprec_exp(bigflt *x)
+{
+	size_t j = 30;
+	//size_t i = 100;
+	//s = (((4 * i) - 2) + ((x * x)/s));
+
+	bigflt *one = str_to_bigflt("1.0000"); 
+	bigflt *two = str_to_bigflt("2.0000"); 
+	bigflt *four = str_to_bigflt("4.00000");
+	bigflt *i = str_to_bigflt("30.00000");
+	bigflt *prod = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *prod2 = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *quo = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *z = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *k = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *y = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *sum1 = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *sum2 = arbprec_expand_vector(NULL, x->len * 200);
+	
+	
+	bigflt *r = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *ret = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *swap;
+	
+	//i = arbprec_add(x, x, i);
+	
+	bigflt *s = str_to_bigflt("1.0");
+	s = arbprec_expand_vector(s, x->len * 200); 
+
+	/* precompute r */
+	r = arbprec_mul(x, x, r);
+
+
+	start:
+	arbprec_init(prod);
+	arbprec_init(prod2);
+	arbprec_init(quo); 
+
+	prod = arbprec_mul(i, four, prod); 
+	prod2 = arbprec_sub(prod, two, prod2);
+	//arbprec_print(prod2);
+	quo = arbprec_div(r, s, quo);
+	//arbprec_print(quo);
+	s = arbprec_add(prod2, quo, s);
+	//arbprec_print(s);
+	//s = ret;
+	--j;
+	z = arbprec_sub(i, one, z); 
+	//arbprec_print(z);
+	swap = i;
+	i = z;
+	z = swap;
+	if ( j >0 )
+		goto start;
+
+	//y = (s + x)/(s - x);
+
+	sum1 = arbprec_add(s, x, sum1);
+	sum2 = arbprec_sub(s, x, sum2);
+	y = arbprec_div(sum1, sum2, y);
+	return y;
+	
+
+}
