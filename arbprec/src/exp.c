e58ce36fd74fc984baa0a202b7ab1827343ed285
cmgraff <cm0graff@gmail.com>
2017-05-02 16:23:16 -0500

    [arbprec] shorten code

--- arbprec/src/exp.c
+++ arbprec/src/exp.c
@@ -4,31 +4,28 @@ bigflt *arbprec_exp(bigflt *x)
 {
 	/*
                 Exp using continued fraction expansion
+
         */
 
 	size_t j = 30;
+	bigflt * s = str_to_bigflt("1.0");
 	bigflt *one = str_to_bigflt("1.0000");
 	bigflt *two = str_to_bigflt("2.0000");
 	bigflt *four = str_to_bigflt("4.00000");
 	bigflt *i = str_to_bigflt("30.00000");
-	bigflt *prod = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *prod2 = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *quo = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *z = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *k = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *y = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *sum1 = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *sum2 = arbprec_expand_vector(NULL, x->len * 200);
+	bigflt *z = arbprec_dupa(i);
 	bigflt *r = arbprec_expand_vector(NULL, x->len * 200);
-	bigflt *swap;
-	bigflt *s;
+	bigflt *prod = arbprec_dupa(r);
+	bigflt *prod2 = arbprec_dupa(r);
+	bigflt *quo = arbprec_dupa(r);
+	bigflt *y = arbprec_dupa(r);
+	bigflt *sum1 = arbprec_dupa(r);
+	bigflt *sum2 = arbprec_dupa(r);
+	bigflt *swap; 
 	
-	s = str_to_bigflt("1.0");
-	s = arbprec_expand_vector(s, x->len * 200); 
 
 	/* precompute r */
 	r = arbprec_mul(x, x, r);
-
 
 	start: 
 
@@ -42,11 +39,10 @@ bigflt *arbprec_exp(bigflt *x)
 	z = swap;
 
 	--j;
-	if ( j >0 )
+	if (j >0 )
 		goto start;
 
-	/* y = (s + x)/(s - x); */ 
-
+	/* y = (s + x)/(s - x) finishes off exp  */
 	sum1 = arbprec_add(s, x, sum1);
 	sum2 = arbprec_sub(s, x, sum2); 
 	y = arbprec_div(sum1, sum2, y);
