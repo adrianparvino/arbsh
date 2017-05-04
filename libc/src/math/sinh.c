1928e197a8be323e6e8fcbb8c3b713070bf8da52
cmgraff <cm0graff@gmail.com>
2017-05-04 09:08:46 -0500

    [libc] move tolerance checking to a seperate function

--- libc/src/math/sinh.c
+++ libc/src/math/sinh.c
@@ -6,11 +6,10 @@ double gsinh(double x)
         long j; 
         double product;
         double last = 0;
-        double sum = 0; 
+        double sum = 0;
 
-	x = _arg_reduction(x);
 
-        for (i = 0; i < 10 ; i++)
+        for (i = 0; i < 1000 ; i++)
         {
                 product = 1.0;
                 for (j = (2*i) + 1 ; j > 0 ; j--)
