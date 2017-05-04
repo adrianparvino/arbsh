a448ccf265ba1ce9de31634595c19b8153f75439
cmgraff <cm0graff@gmail.com>
2017-05-04 06:12:18 -0500

    [libm] make log less computationally expensive by moving static multiplications out of the main loop

--- libc/src/math/log.c
+++ libc/src/math/log.c
@@ -1,35 +1,30 @@
 #include "math.h"
 
-double mylog(double x)
+double glog(double x)
 {
         int n = 1;
         double y = 0;
         double z = 0;
+	double Z = (x-1)/(x+1);
 
         /*
-                Taylor series 
+                Log.c using the Taylor series:
                 ln(x) = 2((x-1/x+1) + 1/3(x-1/x+1)^3 + 1/5(x-1/x+1) + ... ) 
         */
 
         if (x <= 0 )
-                return MYNAN; 
-
+                return MYNAN;
 	
-	
-        while ( 1 && n < 10000)
+        while (n < 100)
         {
-        	y += (mypow((x - 1)/(x + 1), n) / n ); 
-
-                n+=2;
-	
+        	y += (gpow(Z, n) / n ); 
+                n+=2; 
                 if ( y == z )
                         break;
                 z = y;
-		
         }
 
-	return  y * 2; 
-
+	return  y * 2;
 }
 
 
