149e7c3929aa8c0976f3255c0d8d09574a62a583
cmgraff <cm0graff@gmail.com>
2017-05-04 19:28:42 -0500

    [libm] variable naming and code structure conventions

--- libc/src/math/exp.c
+++ libc/src/math/exp.c
@@ -1,35 +1,35 @@
 #include <gmath.h>
+#include <gstddef.h>
 
 double gexp(double x)
 {
-        int i;
-        int n = 0;
-        double y = 0;
-        double d = 0;
-        double z = 0;
+	size_t i;
+	size_t n = 2;
+	double y = 1.0 + x;
+	double d = 0;
+	double last = 0;
 
-        if (x == 1.0) {
-                y = EULER;
-        } else if (x < 0) {
-                y = 1.0 / gexp(-x);
-        } else {
-                n = 2;
-                y = 1.0 + x;
-                while ( 1 )
-                {
-                        d = x;
-                        for (i = 2; i <= n; i++)
-                        {
-                                d *= x / i; 
-                        }
-                        y += d;
-                        n++;
-                        if ( y == z )
-                                break;
-                        z = y;
-                }
-        }
-        return y;
+	if (x == 1.0) 
+		return EULER;
+	else if (x < 0) 
+		return 1.0 / gexp(-x); 
+	
+
+	for ( ; 1 ; n++)
+	{
+		d = x;
+		for (i = 2; i <= n; i++)
+		{
+			d *= x / i; 
+		}
+		y += d;
+
+		if (_check_tolerance(last, y))
+			break;
+		last = y;
+	}
+	
+	return y;
 }
 
 
