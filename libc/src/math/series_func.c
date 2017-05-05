eaf13ee8ecf1787af46c0fb51d67996b3c440098
cmgraff <cm0graff@gmail.com>
2017-05-05 06:35:29 -0500

    [libm] combine several series into a single function

---
+++ libc/src/math/series_func.c
@@ -0,0 +1,61 @@
+#include <gmath.h>
+
+double series_func(double x, int one, int toggle);
+
+double series_func_driver(double x, int selector)
+{ 
+        double sum = 0;
+
+        
+	if ( selector == 0 )
+	{
+		x = _arg_reduction(x);
+		sum = series_func(x, 0, 1);/* cos */
+	}
+	if ( selector == 1 )
+		sum = series_func(x, 0, 0);/* cosh */
+	if ( selector == 2 )
+	{
+		x = _arg_reduction(x);
+		sum = series_func(x, 1, 1);/* sin */
+	}
+	if ( selector == 3 )
+		sum = series_func(x, 1, 0);/* sinh */
+        return sum;
+}
+
+
+double series_func(double x, int one, int toggle)
+{
+        size_t i = 0;
+        size_t j = 0;
+       
+        double product = 1.0;
+        double sum = 0;
+        double last = 0; 
+
+        for (i = 0; i < 1000; i++)
+        {
+                product = 1.0;
+                for (j = (2*i) + one; j > 0 ; j--)
+                        product *= x / j;
+
+		if ( toggle != 0 )
+		{
+                	sum += product * toggle;
+			toggle = -toggle;
+		}
+		else 
+			sum += product;
+
+                if (_check_tolerance(last, sum))
+                        break;
+
+                last = sum;
+
+                
+        }
+
+        return sum;
+}
+
