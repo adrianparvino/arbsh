750bdc0e6138eba41b053cda63d9992069784f34
cmgraff <cm0graff@gmail.com>
2017-05-09 21:54:23 -0500

    [arbprec] have hypot use a third arg

--- arbprec/src/hypot.c
+++ arbprec/src/hypot.c
@@ -1,24 +1,26 @@
 #include <arbprec/arbprec.h>
 
-bigflt *arbprec_hypot(bigflt *x, bigflt *y)
+bigflt *arbprec_hypot(bigflt *x, bigflt *y, bigflt *c)
 {
 
 	/*
 		hypot.c implemented using the identity:
 
 			hypot(x.y) = sqrt((x*x)+(y*y))
-	*/
-
-	bigflt *ret;
+	*/ 
+	
 	bigflt *pro1 = arbprec_expand_vector(NULL, y->len + x->len);
         bigflt *pro2 = arbprec_expand_vector(NULL, x->len + x->len);
         bigflt *sum = arbprec_expand_vector(NULL, y->len + x->len);
 
-
 	pro1 = arbprec_mul(x, x, pro1);
 	pro2 = arbprec_mul(y, y, pro2);
 	sum = arbprec_add(pro1, pro2, sum);
-	//ret = arbprec_sqrt(sum);
+	c = arbprec_sqrt(sum, c);
 
-	return ret;
+	arba_free(pro1);
+	arba_free(pro2);
+	arba_free(sum);
+
+	return c;
 }
