dedf4696fbc10f21d4873594ad053d47453700bd
cmgraff <cm0graff@gmail.com>
2017-05-09 19:41:07 -0500

    [arbprec] example algorithm

---
+++ arbprec/src/sqrt_longhand.c
@@ -0,0 +1,26 @@
+#include <arbprec/arbprec.h>
+
+/*
+
+           2  3. 8
+	  ___________
+	\/ 5,69.00,00
+           4
+          ----
+      4_3) 1,69
+           1,29
+           -----
+       46_8) 40.00
+             37.44
+
+                      ...
+*/
+
+
+bigflt *arbprec_sqrt_longhand(bigflt *flt)
+{
+	/* not yet implemented, use the note below to finish this */
+	
+	return flt;
+}
+
