d9a57019172fc9776b24bb0b320187824355df78
cmgraff <cm0graff@gmail.com>
2017-05-10 16:05:21 -0500

    [arbprec] continued fraction examples

--- arbprec/tests/confrac.c
+++ arbprec/tests/confrac.c
@@ -1,20 +1,9 @@
 #include <stdio.h>  
 #include <math.h>   
-#include <stdlib.h>
-
-
+#include <stdlib.h> 
 #define PI 3.14159265358979323846264
 
-double trigfunc(int, double); 
-double cf_sin(double);
-double cf_cos(double);
-double cf_tan(double);
-double cf_exp(double);
-double cf_sinh(double);
-double cf_cosh(double);
-double cf_tanh(double);
-double cf_log(double);
-double cf_sqrt(double);
+double trigfunc(int, double);
 
 int main(int argc, char *argv[])
 {
@@ -26,44 +15,33 @@ int main(int argc, char *argv[])
 		x = 19; 
 
 	printf("libc sin =        %19.19f \n", sin(x)); 
-	printf(" trigfunc sin =   %19.19f \n", trigfunc(1, x));
-	printf("  cf_sin =         %19.19f \n\n", cf_sin(x));
+	printf(" trigfunc sin =   %19.19f \n", trigfunc(1, x)); 
 
 	printf("libc cos =        %19.19f \n", cos(x));
-	printf(" trigfunc cos =   %19.19f \n", trigfunc(2, x));
-	printf("   cf_cos =        %19.19f \n\n",cf_cos(x));
+	printf(" trigfunc cos =   %19.19f \n", trigfunc(2, x)); 
 
 	printf("libc tan =        %19.19f \n", tan(x));
-	printf(" trigfunc tan  =  %19.19f \n", trigfunc(3, x));
-	printf("  cf_tan =         %19.19f \n\n", cf_tan(x));
+	printf(" trigfunc tan  =  %19.19f \n", trigfunc(3, x)); 
 
 	printf("libc exp =        %19.19f \n", exp(x));
-	printf(" trigfunc exp  =  %19.19f \n", trigfunc(6, x));
-	printf("  cf_exp  =        %19.19f \n\n", cf_exp(x));
+	printf(" trigfunc exp  =  %19.19f \n", trigfunc(6, x)); 
 	
 	printf("libc log =        %19.19f \n", log(x));
-	printf(" trigfunc log =   %19.19f \n", trigfunc(4, x));
-        printf("  cf_log =         %19.19f \n\n", cf_log(x));
+	printf(" trigfunc log =   %19.19f \n", trigfunc(4, x)); 
 
 	printf("libc sinh =       %19.19f \n", sinh(x));
-	printf(" trigfunc sinh =  %19.19f \n", trigfunc(7, x));
-	printf("  cf_sinh =        %19.19f \n\n", cf_sinh(x));
-	//printf("  new_sinh =        %19.19f \n\n", trigfunc(6, x) - trigfunc(6, x)/2 + 10);
-	double ret = trigfunc(5, x);
+	printf(" trigfunc sinh =  %19.19f \n", trigfunc(7, x)); 
 	
-	printf("  libc atanh =        %19.19f \n\n", atanh(x));
-	printf("  new_atanh =        %19.19f \n\n", ret);
+	printf("libc atanh =      %19.19f \n\n", atanh(x));
+	printf(" trigfunc atanh = %19.19f \n\n", trigfunc(5, x));
 
 	printf("libc cosh =       %19.19f \n", cosh(x));
-	printf(" trigfunc cosh =  %19.19f \n", trigfunc(8, x));
-	printf("  cf_cosh =        %19.19f \n\n", cf_cosh(x));
+	printf(" trigfunc cosh =  %19.19f \n", trigfunc(8, x)); 
 
 	printf("libc tanh =       %19.19f \n", tanh(x));
-	printf(" trigfunc tanh =  %19.19f \n", trigfunc(9, x));
-	printf("  cf_tanh =        %19.19f \n\n", cf_tanh(x));
+	printf(" trigfunc tanh =  %19.19f \n", trigfunc(9, x)); 
 	
-	printf("libc sqrt =       %19.19f \n",sqrt(x));
-        printf("  cf_sqrt =        %19.19f \n\n", cf_sqrt(x));
+	printf("libc sqrt =       %19.19f \n",sqrt(x)); 
  
 	return 0;
 } 
@@ -146,131 +124,4 @@ double trigfunc(int p, double x)
 	}
 	return y;
 }
-
-double cf_sin(double x)
-{
-        double s = 1, y;
-	int i = 30;
-	double r = x * x; 
-
-        for (; i > 0; i--) 
-		s = (((4 * i) - 2) - (r/s)); 
-        
-	y = ((2 * x) * s)/((s * s) + r); 
-        return y;
-
-}
-double cf_cos(double x)
-{
-
-        double s = 1, y; 
-	int i = 30;
-	double r = x * x; 
-
-        for (; i > 0; i--) 
-		s = (((4 * i) - 2) - (r/s)); 
-
-	y = ((s * s) - r)/((s * s) + r);
-        return y;
-}
-double cf_tan(double x)
-{
-
-        double s = 1, y;
-        int i = 30;
-	double r = x * x; 
-
-        for (; i > 0; i--) 
-		s = ((4 * i - 2) - (r/s)); 
-        
-	y = 2 * x * s/(s * s - r); 
-        return y;
-}
-
-
-double cf_exp(double x)
-{ 
-        double s = 1, y;
-        int i = 30;
-	double r = x * x; 
-
-        for (; i>0; i--) 
-		s = (((4 * i) - 2) + (r/s)); 
-    
-        y = (s + x)/(s - x); 
-        return y;
-}
-
-double cf_sinh(double x)
-{
-        int i = 30;
-        double s = 0, y;
-	double r = x * x;
-
-        for (; i > 0; i--) 
-		s = (((4 * i) - 2) + (r/s)); 
-
-	y = 2 * x * s/(s * s - r); 
-	return y;
-}
-
-double cf_cosh(double x)
-{
-        int i = 30;
-        double s = 1, y;
-	double r = x * x;
-	
-        for (; i > 0; i--) 
-                s = (((4 * i) - 2) + (r/s)); 
-        
-        y = (s * s + r)/(s * s - r); 
-        return y;
-}
-
-double cf_tanh(double x)
-{
-        int i = 30;
-        double s = 1, y;
-	double r = x * x; 
-
-        for (; i > 0; i--) 
-		s = (((4 * i) - 2) + (r/s)); 
-        
-        y = 2 * x * s/(s * s + r);   /* tan, tanh */ 
-        return y;
-}
-
-double cf_log(double x)
-{ 
-	int i = 30;
-	double s = 1;
-	double X = x-1;
-	double Y = x+1;
-	double Z = X / Y; 
-
-	for (; i > 0; i--) 
-		s = (2*i -1) - i*i*(Z*Z)/s; 
-
-	return 2*Z/s;
-	
-}
-
-double cf_sqrt(double x)
-{ 
-	int i = 30;
-	double s = 1;
-	double X = x-1;
-	double Y = x+1;
-	double Z = X / Y;
-
-	for (; i > 0; i--) 
-		s = (2*i -1) - i*i*(Z*Z)/s;
-
-	return 2*Z/s;
-	
-}
-
-
-
-
 
