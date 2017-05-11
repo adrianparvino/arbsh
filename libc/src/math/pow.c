2c2a9b8ff30fb8770a6c7da50630c59b08f75227
cmgraff <cm0graff@gmail.com>
2017-05-11 06:23:39 -0500

    [arbprec] add arbprec clean target

--- libc/src/math/pow.c
+++ libc/src/math/pow.c
@@ -1,24 +1,22 @@
 #include <gmath.h>
 
 double gpow(double x, double n)
-{
+{ 
+	double y = 1;
 
-        double y = 1;
+	/* negative */
+	if (n < 0)
+		y = 1.0 / gpow(x, -n);
+	/* whole number */
+	else if (n == (long long)n)
+	{
+		while (n--)
+		      y *= x;
+	}
+	/* fractional */
+	else
+		y = gexp(n * glog(x)); 
 
-        /* negative */
-        if (n < 0)
-                y = 1.0 / gpow(x, -n);
-        /* whole number */
-        else if (n == (long long)n)
-        {
-                while (n--)
-                      y *= x;
-        }
-        /* fractional */
-        else
-                y = gexp(n * glog(x)); 
-
-        return y;
+	return y;
 }
-
 
