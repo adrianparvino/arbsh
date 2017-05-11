51c68f8e1f3c5eac35536caf3c1100b2a34396bf
cmgraff <cm0graff@gmail.com>
2017-05-10 22:53:11 -0500

    [arbprec] notes

--- arbprec/tests/confrac.c
+++ arbprec/tests/confrac.c
@@ -58,6 +58,7 @@ double trigfunc(int p, double x)
 
 	if ( p == 5)
 		Z=x; 
+
 	ZZ = Z * Z;
 
 	if (p <= 3)
@@ -95,9 +96,33 @@ double trigfunc(int p, double x)
 			y = (2*Z/s) / 2;		/* atanh */
 			break;
 		case 6 :
-			break; 
-	
+			break;
 	}
 	return y;
 }
+
+/*
+                        These all have identities relating to log
+                        so can likely be expressed using this series
+                        function
+
+                        asinh(x) = ln( x + [sqrt](x2 + 1) ) 
+                        acosh(x) = ln( x [sqrt](x2 - 1) ) 
+                        atanh(x) = 1/2 ln( (1+x)/(1-x) ) 
+                        acsch(x) = ln( (1+[sqrt](1+x2) )/x ) 
+                        asech(x) = ln( (1[sqrt](1-x2) )/x ) 
+                        acoth(x) = 1/2 ln( (x+1)/(x-1) ) 
+
+
+                        Conversely, these functions all bear a
+                        relations ship to exp()
+
+                        sinh(x) = ( e(x) - e(-x) )/2 
+                        csch(x) = 1/sinh(x) = 2/( e(x) - e(-x) ) 
+                        cosh(x) = ( e(x) + e(-x) )/2 
+                        sech(x) = 1/cosh(x) = 2/( e(x) + e(-x) ) 
+                        tanh(x) = sinh(x)/cosh(x) = ( e(x) - e(-x) )/( e(x) + e(-x) ) 
+                        coth(x) = 1/tanh(x) = ( e(x) + e(-x))/( e(x) - e(-x) ) 
+                */
+
 
