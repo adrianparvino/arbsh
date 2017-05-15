61bed7b7732f94f26f0e152809a8a63d9fec2724
cmgraff <cm0graff@gmail.com>
2017-05-15 18:17:41 -0500

    [arbprec] short algorithms

--- arbprec/src/short_arithmetic.c
+++ arbprec/src/short_arithmetic.c
@@ -19,27 +19,34 @@ void arbprec_short_mul(bigflt *a, int b)
 
 void arbprec_short_div(bigflt *a, int b)
 {
-
+	int carry = 0;
+	size_t i = 0;
+	for( i = 0; i < a->len; i++)
+	{
+		a->number[i] += carry * base;
+		carry = a->number[i] % b;
+		a->number[i] /= b;
+	}
 }
 
 bigflt *arbprec_short_add_r(bigflt *a, int b, bigflt *c)
 {
-
+	return c;
 }
 
 bigflt *arbprec_short_sub_r(bigflt *a, int b, bigflt *c)
 {
-
+	return c;
 }
 
 bigflt *arbprec_short_mul_r(bigflt *a, int b, bigflt *c)
 {
-
+	return c;
 }
 
 bigflt *arbprec_short_div_r(bigflt *a, int b, bigflt *c)
 {
-
+	return c;
 }
 
 
