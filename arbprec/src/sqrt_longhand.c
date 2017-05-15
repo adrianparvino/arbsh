864d53bf976affce984594f1e56778945376eb9c
cmgraff <cm0graff@gmail.com>
2017-05-15 17:21:20 -0500

    [arbprec] short algorithm protoypes

--- arbprec/src/sqrt_longhand.c
+++ arbprec/src/sqrt_longhand.c
@@ -16,34 +16,33 @@
                       ...
 */
 
-
 bigflt *arbprec_sqrt_longhand(bigflt *a, bigflt *c)
 {
 	/* not yet implemented, use the notes above to finish this */
-	
 	size_t i = 0;
-	/*size_t j = 0;
-	size_t k = 0;
-
-        int *mir = arbprec_malloc(sizeof(int) * a->len);
-        int *tmir = arbprec_malloc(sizeof(int) * a->len);
-	int *and = arbprec_malloc(sizeof(int) * a->len);
-	int *guess = arbprec_malloc(sizeof(int) * a->len);
-
+	//size_t j = 0;
 	int set[2] = { 0, 0 };
-	*/
-	int iseven = 0;
-
-	size_t total_len = a->float_pos;
 	
-	if ( total_len % 2 == 0 )
+	int iseven = 0;
+	size_t total_len = a->float_pos;
+
+
+	if (total_len % 2 == 0)
 		iseven = 1;
 
-	for (i = 0 ; i < total_len ; ++i)
+	if (!(iseven))
+		set[0] = a->number[0];
+
+	for (i = 0;i < total_len; ++i)
 	{
-		if (iseven)
-			{};
-		//guess = arbprec_sub(
+		
+		if (i % 2 == 0)
+			set[0] = a->number[i];
+		//	printf("%d\n", i); 
+		
+	
+		
+		
 	}
 	
 	return c;
