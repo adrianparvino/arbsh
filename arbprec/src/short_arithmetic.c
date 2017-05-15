2574f5ea1f3f0cd490cb3eb8e199d62d0ac027a9
cmgraff <cm0graff@gmail.com>
2017-05-15 17:21:20 -0500

    [arbprec] short algorithm protoypes

---
+++ arbprec/src/short_arithmetic.c
@@ -0,0 +1,45 @@
+#include <arbprec/arbprec.h>
+
+
+
+void arbprec_short_add(bigflt *a, int b)
+{
+
+}
+
+void arbprec_short_sub(bigflt *a, int b)
+{
+
+}
+
+void arbprec_short_mul(bigflt *a, int b)
+{
+
+}
+
+void arbprec_short_div(bigflt *a, int b)
+{
+
+}
+
+bigflt *arbprec_short_add_r(bigflt *a, int b, bigflt *c)
+{
+
+}
+
+bigflt *arbprec_short_sub_r(bigflt *a, int b, bigflt *c)
+{
+
+}
+
+bigflt *arbprec_short_mul_r(bigflt *a, int b, bigflt *c)
+{
+
+}
+
+bigflt *arbprec_short_div_r(bigflt *a, int b, bigflt *c)
+{
+
+}
+
+
