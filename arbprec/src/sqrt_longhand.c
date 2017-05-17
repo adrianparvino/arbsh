3bd3ea679a7712d6fac9b8b577f113123afc8bce
cmgraff <cm0graff@gmail.com>
2017-05-16 19:11:24 -0500

    [arbprec] memory management

--- arbprec/src/sqrt_longhand.c
+++ arbprec/src/sqrt_longhand.c
@@ -21,23 +21,23 @@ bigflt *arbprec_sqrt_longhand(bigflt *a, bigflt *c)
 	/* not yet implemented, use the notes above to finish this */
 	size_t i = 0;
 	//size_t j = 0;
-	int set[2] = { 0, 0 };
+	//int set[2] = { 0, 0 };
 	
-	int iseven = 0;
+	//int iseven = 0;
 	size_t total_len = a->float_pos;
 
 
-	if (total_len % 2 == 0)
-		iseven = 1;
+	//if (total_len % 2 == 0)
+	//	iseven = 1;
 
-	if (!(iseven))
-		set[0] = a->number[0];
+	//if (!(iseven))
+	//	set[0] = a->number[0];
 
 	for (i = 0;i < total_len; ++i)
 	{
 		
-		if (i % 2 == 0)
-			set[0] = a->number[i];
+		//if (i % 2 == 0)
+		//	set[0] = a->number[i];
 		//	printf("%d\n", i); 
 		
 	
