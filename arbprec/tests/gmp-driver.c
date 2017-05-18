a09759609d11585dcfa2a498387dac58d7c36769
cmgraff <cm0graff@gmail.com>
2017-05-17 20:25:27 -0500

    [arbprec] some simple tests for gmp

---
+++ arbprec/tests/gmp-driver.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <gmp.h> 
+#include <assert.h>
+
+int main()
+{ 
+	char inputStr[1024]; 
+	mpf_t n;
+
+	printf ("Enter your number: ");
+	scanf("%1023s" , inputStr); 
+	mpf_init(n); 
+	mpf_set_str(n,inputStr, 10); 
+	mpf_out_str(stdout,10,20, n);
+	printf ("\n");
+}
