f27288c11637c2d8e3129660291188e178d974e1
cmgraff <cm0graff@gmail.com>
2017-05-18 14:47:07 -0500

    [curses] bare minimum function usage

--- curses/curses.c
+++ curses/curses.c
@@ -73,16 +73,6 @@ void setcursorchars(size_t x, size_t y, char s)
 	write(0, str, len);
 }
 
-void ansihorizon(size_t x, size_t X)
-{ 
-	/* [20;40r	Scrolling region set to lines 20 -> 40  	*/
-	/* \033[?;?r */
-	char str[1025] = { 0 };
-	size_t len = 0;
-	len = gsprintf(str, "\033[%zu;%zur", x, X);
-	write(0, str, len);
-}
-
 int ansiinit(void)
 {
 	int ret = 0;
@@ -96,53 +86,6 @@ int ansiinit(void)
 	write(1,T_INSERTRESET, T_INSERTRESET_SZ);
 	return ret;
 } 
-
-int ansicreate(void)
-{
-	size_t len = 0;
-	len = ANSIWINDOW[ansiglb.t].len = ( ansiglb.col * ansiglb.row );
-	ANSIWINDOW[ansiglb.t].ansilastmap = gmalloc(sizeof(char) * len * 10);
-	ANSIWINDOW[ansiglb.t].ansiwinbuf = gmalloc(sizeof(char) * len * 10); 
-	gmemset(ANSIWINDOW[ansiglb.t].ansilastmap, 0, (len * 10));
-	ansiglb.c = ansiglb.t;
-	size_t i = 0; 
-	ansiglb.t++;
-	return (ansiglb.t) - 1;
-}
-
-int ansidestroy(void)
-{
-	gfree(ANSIWINDOW[ansiglb.c].ansilastmap);
-	gfree(ANSIWINDOW[ansiglb.c].ansiwinbuf);
-	return 0;
-}
-
-int ansiwaddch(int ch, int position)
-{
-	/* ansiwaddch adds a char into the current cursor position on screen */ 
-	ANSIWINDOW[ansiglb.c].ansiwinbuf[position] = ch;
-	return 0;
-} 
-
-int ansiaddmem(char *str, int position, size_t end)
-{
-	/* ansiwaddmem adds a series of bytes to the current ANSIWINDOW[ */ 
-	size_t i = position;
-	size_t ret = 0;
-	for( ret = 0;i < end ; ++i, ++ret) 
-		ANSIWINDOW[ansiglb.c].ansiwinbuf[i] = str[ret]; 
-	return ret;
-}
-
-size_t ansiaddstr(char *str, size_t position)
-{
-	/* add a string, discard the '\0' */
-	size_t i = position;
-	size_t ret = 0; 
-	for( ret = 0;str[ret] != '\0' ; ++i, ++ret) 
-		ANSIWINDOW[ansiglb.c].ansiwinbuf[i] = str[ret]; 
-	return ret;
-}
 
 int ansiredraw(size_t lim, size_t x, size_t y, size_t rightmarg)
 {
@@ -168,4 +111,16 @@ int ansiredraw(size_t lim, size_t x, size_t y, size_t rightmarg)
 		}
 	}
 	return 0;
+}
+int ansicreate(void)
+{
+	size_t len = 0;
+	len = ANSIWINDOW[ansiglb.t].len = ( ansiglb.col * ansiglb.row );
+	ANSIWINDOW[ansiglb.t].ansilastmap = gmalloc(sizeof(char) * len * 10);
+	ANSIWINDOW[ansiglb.t].ansiwinbuf = gmalloc(sizeof(char) * len * 10); 
+	gmemset(ANSIWINDOW[ansiglb.t].ansilastmap, 0, (len * 10));
+	ansiglb.c = ansiglb.t;
+	size_t i = 0; 
+	ansiglb.t++;
+	return (ansiglb.t) - 1;
 }
