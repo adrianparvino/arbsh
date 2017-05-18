403eec05a723cfb817ab6295d12b2a8addc52d33
cmgraff <cm0graff@gmail.com>
2017-05-17 20:31:06 -0500

    [arbprec] more gmp tests

--- arbprec/tests/gmp-driver.c
+++ arbprec/tests/gmp-driver.c
@@ -11,6 +11,7 @@ int main()
 	scanf("%1023s" , inputStr); 
 	mpf_init(n); 
 	mpf_set_str(n,inputStr, 10); 
-	mpf_out_str(stdout,10,20, n);
+	//mpf_out_str(stdout,10,20, n);
+	gmp_printf("%.100Ff\n", n);
 	printf ("\n");
 }
