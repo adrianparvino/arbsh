3210f3a55cbfcbe778d1fe09ecfe6388c43946a7
cmgraff <cm0graff@gmail.com>
2017-05-18 04:59:30 -0500

    drop shell library

---
+++ scripts/du_wrapper.sh
@@ -0,0 +1,13 @@
+# du wrapper
+duprint()
+{ 
+        if [ "$#" -gt 1 ]
+        then    printf "%s" $( eval printf '$'$(( $# - 1 )))
+        fi
+}
+
+duwrap()
+{
+        duprint $(du -s $@ 2>/dev/null)
+}
+
