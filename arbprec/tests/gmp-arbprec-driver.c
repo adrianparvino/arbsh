899cf9820c27121d7b2e3509a573eae923ed2983
cmgraff <cm0graff@gmail.com>
2017-05-18 05:39:53 -0500

    get rid of cruft

---
+++ arbprec/tests/gmp-arbprec-driver.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <gmp.h> 
+#include <assert.h>
+
+int main(int argc, char *argv[])
+{ 
+
+	mpf_t n;
+	mpf_t j;
+	mpf_init(n); 
+	mpf_init(j); 
+	mpf_set_str(n,argv[1], 10); 
+	gmp_printf("%.100Ff\n", n); 
+	mpf_sqrt(j, n);
+	gmp_printf("gmp sqrt %.100Ff\n", j); 
+
+	printf ("\n");
+}
