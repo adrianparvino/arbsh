383f4fed5e3a48a265ce94bf48c83e89798fdf22
cmgraff <cm0graff@gmail.com>
2017-05-19 20:38:26 -0500

    curses standard

--- curses/src/curses/waddch.c
+++ curses/src/curses/waddch.c
@@ -2,15 +2,17 @@
 
 int waddch(WINDOW *win, chtype ch)
 {
+	
 	if (win->buf == NULL)
 	{
+		write(1,"was here", 10);
 		win->len = (win->x * win->y);
 		if ((win->buf = malloc(sizeof(char) * win->len)) == NULL)
-		{
+		{ 
 			win->len = 0;
 			return -1;
 		}
-		win->buf = win->rp;
+		win->rp = win->buf;
 	}
 	if (win->len > 0)
 		win->len--;
