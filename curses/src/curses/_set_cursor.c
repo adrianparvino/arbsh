3afd60eb5e2a8d35b021bb48c67cb0ea1cda3270
cmgraff <cm0graff@gmail.com>
2017-05-19 22:19:36 -0500

    refresh bulked out

---
+++ curses/src/curses/_set_cursor.c
@@ -0,0 +1,9 @@
+#include <curses.h>
+
+void _setcursor(size_t x, size_t y)
+{
+        char str[1025] = { 0 };
+        size_t len = 0;
+        len = sprintf(str, "\033[%zu;%zuH", x, y);
+        write(0, str, len);
+}
