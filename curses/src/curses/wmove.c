6a8536afe88808e60bd8246f37c1522ab16d0bce
cmgraff <cm0graff@gmail.com>
2017-05-19 22:19:36 -0500

    refresh bulked out

--- curses/src/curses/wmove.c
+++ curses/src/curses/wmove.c
@@ -1,13 +1,8 @@
 #include <curses.h>
 
 int wmove(WINDOW *win, size_t x, size_t y)
-{
-	/* [24;80H	Pos to line 24 col 80 (any line 1 to 24, any col 1 to 132)*/
-	/* \033[?;?H */
-	char str[128] = { 0 };
-	size_t len = 0;
-	len = sprintf(str, "\033[%zu;%zuH", x, y);
-	//write(1, str, len);
-	write(win->fd, str, len);
+{ 
+	win->px = x;
+	win->py = y;
 	return 1;
 }
