2f53684af3571020f7a2b40c61eada36a74e379c
cmgraff <cm0graff@gmail.com>
2017-05-20 13:54:45 -0500

    curses standard working but requires terminating line sequences

--- curses/src/curses/wrefresh.c
+++ curses/src/curses/wrefresh.c
@@ -5,12 +5,15 @@ int wrefresh(WINDOW *win)
 	char buf[32];
 	size_t len = 0;
 	_setcursor(1, 1);
+	//_setcursor(win->px, win->py);
+	//clrtobot();
 	//if (win->clearok == 1)
 	//{
 		len = sprintf(buf, "%s", ansiglb.t_eraseall);
 		write(win->fd, buf, len);
 	//}
 	write(win->fd, win->buf, win->rp - win->buf);
+	win->rp = win->buf;
 	_setcursor(win->px, win->py);
 	return 0;
 }
