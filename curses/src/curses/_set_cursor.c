da2ebb9fc15232ac0aeb92e62d6b5e585c185592
cmgraff <cm0graff@gmail.com>
2017-05-20 13:54:45 -0500

    curses standard working but requires terminating line sequences

--- curses/src/curses/_set_cursor.c
+++ curses/src/curses/_set_cursor.c
@@ -4,6 +4,7 @@ void _setcursor(size_t x, size_t y)
 {
         char str[1025] = { 0 };
         size_t len = 0;
+	y = y + 1;/* count y from 1 */
         len = sprintf(str, "\033[%zu;%zuH", x, y);
         write(0, str, len);
 }
