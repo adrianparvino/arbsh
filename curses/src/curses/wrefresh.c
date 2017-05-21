a93d5f09e6564b92c70e86857577da28b91c0465
cmgraff <cm0graff@gmail.com>
2017-05-20 19:51:27 -0500

    clean up curses

--- curses/src/curses/wrefresh.c
+++ curses/src/curses/wrefresh.c
@@ -5,13 +5,12 @@ int wrefresh(WINDOW *win)
 	char buf[32];
 	size_t len = 0;
 	_setcursor(1, 0);
-	//_setcursor(win->px, win->py);
-	//clrtobot();
-	//if (win->clearok == 1)
-	//{
+	clrtobot();
+	if (win->clearok == 1)
+	{
 		len = sprintf(buf, "%s", ansiglb.t_eraseall);
 		write(win->fd, buf, len);
-	//}
+	}
 	write(win->fd, win->buf, win->rp - win->buf);
 	win->rp = win->buf;
 	_setcursor(win->px, win->py);
