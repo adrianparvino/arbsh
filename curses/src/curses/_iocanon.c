85d472fcca846a59b006752a93651fa166537bb4
cmgraff <cm0graff@gmail.com>
2017-05-20 19:51:27 -0500

    clean up curses

--- curses/src/curses/_iocanon.c
+++ curses/src/curses/_iocanon.c
@@ -1,8 +1,8 @@
 #include <curses.h>
 
 WINDOW _IO_canon[CURSES_MAX] = {
-	{ 1, 0, NULL, 0, NULL, NULL, { 0 }, { 0 }, { 0 }, 0, 0, 0, 0, 0},
-	{ 1, 0, NULL, 0, NULL, NULL, { 0 }, { 0 }, { 0 }, 0, 0, 0, 0, 0},
-	{ 1, 0, NULL, 0, NULL, NULL, { 0 }, { 0 }, { 0 }, 0, 0, 0, 0, 0}
+	{ 1, NULL ,NULL, 0, 0, 0, 0, 0, 0},
+	{ 1, NULL ,NULL, 0, 0, 0, 0, 0, 0},
+	{ 1, NULL ,NULL, 0, 0, 0, 0, 0, 0}
 };
 
--- curses/src/curses/_vt52_erase.c
+++
@@ -1,7 +0,0 @@
-#include <curses.h>
-
-char *_vt52_erase(char *buf)
-{
-	sprintf(buf, "%s%s", ansiglb.t_gohome, ansiglb.t_clrcur2bot);
-	return buf;
-}
