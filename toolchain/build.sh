c3a7370c37f9e26978c179e175dcb471fce85e6c
cmgraff <cm0graff@gmail.com>
2017-05-25 15:00:34 -0500

    update glibc and uclibc

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -9,11 +9,11 @@ make clean OM_ARCH='x86_64' OM_LIBC='musl' BUILDNO=$buildno
 make OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno
 make clean OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno
 
-make OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno
-make clean OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno
-make OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno
+make OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
+make clean OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno 
+make OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="x86_64" OM_BACKWARDCOMPAT="x86_64"
 make clean OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno
-make OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno
+make OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="arm64"
 make clean OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno
 
 make OM_ARCH='i686' OM_LIBC='gnu' BUILDNO=$buildno
