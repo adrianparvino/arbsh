f1f6863d2319d7a57f64b1087fbe899a2508f1ca
cmgraff <cm0graff@gmail.com>
2017-05-24 20:53:13 -0500

    autologging

---
+++ toolchain/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+buildno="1"
+
+make OM_ARCH='i686' OM_LIBC='musl' BUILDNO=$buildno
+make OM_ARCH='x86_64' OM_LIBC='musl' BUILDNO=$buildno
+make OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno
+
+make OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno
+make OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno
+make OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno
+
+make OM_ARCH='i686' OM_LIBC='gnu' BUILDNO=$buildno
+make OM_ARCH='x86_64' OM_LIBC='gnu' BUILDNO=$buildno
+make OM_ARCH='aarch64' OM_LIBC='gnu' BUILDNO=$buildno
+make finishreport
+
+buildno=$((buildno + 1))
+
+
+
+
+
+
+
