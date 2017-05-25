68816624aab1f82d47ee3853a403f90cef472373
cmgraff <cm0graff@gmail.com>
2017-05-24 20:58:47 -0500

    autologging

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -3,16 +3,25 @@
 buildno="1"
 
 make OM_ARCH='i686' OM_LIBC='musl' BUILDNO=$buildno
+make clean OM_ARCH='i686' OM_LIBC='musl' BUILDNO=$buildno
 make OM_ARCH='x86_64' OM_LIBC='musl' BUILDNO=$buildno
+make clean OM_ARCH='x86_64' OM_LIBC='musl' BUILDNO=$buildno
 make OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno
+make clean OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno
 
 make OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno
+make clean OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno
 make OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno
+make clean OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno
 make OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno
+make clean OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno
 
 make OM_ARCH='i686' OM_LIBC='gnu' BUILDNO=$buildno
+make clean OM_ARCH='i686' OM_LIBC='gnu' BUILDNO=$buildno
 make OM_ARCH='x86_64' OM_LIBC='gnu' BUILDNO=$buildno
+make clean OM_ARCH='x86_64' OM_LIBC='gnu' BUILDNO=$buildno
 make OM_ARCH='aarch64' OM_LIBC='gnu' BUILDNO=$buildno
+make clean OM_ARCH='aarch64' OM_LIBC='gnu' BUILDNO=$buildno
 make finishreport
 
 buildno=$((buildno + 1))
