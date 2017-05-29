35e48d6c792c94440eed0f38f2b4d928fde4d086
cmgraff <cm0graff@gmail.com>
2017-05-29 04:18:34 -0500

    sh arch

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -7,6 +7,7 @@ ulimit -S -t 3600 -v 2000000 -u 1200
 buildno="2"
 
 make startreport
+
 make OM_ARCH='i686' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
 make clean OM_ARCH='i686' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
 make OM_ARCH='x86_64' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="x86_64" OM_BACKWARDCOMPAT="x86_64"
@@ -14,7 +15,10 @@ make clean OM_ARCH='x86_64' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="x86_64
 make OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="arm64"
 make clean OM_ARCH='aarch64' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="arm64" 
 make OM_ARCH='mips' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips"
-make clean OM_ARCH='mips' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips"
+make clean OM_ARCH='mips' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips" 
+make OM_ARCH='sh' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="sh" OM_BACKWARDCOMPAT="sh"
+make clean OM_ARCH='sh' OM_LIBC='musl' BUILDNO=$buildno OM_LINUXARCH="sh" OM_BACKWARDCOMPAT="sh"
+
 
 make -i OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
 make clean OM_ARCH='i686' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
@@ -23,7 +27,10 @@ make clean OM_ARCH='x86_64' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="x86_
 make -i OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="aarch64"
 make clean OM_ARCH='aarch64' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="aarch64"
 make -i OM_ARCH='mips' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips"
-make clean OM_ARCH='mips' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="mips"
+make clean OM_ARCH='mips' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips"
+make -i OM_ARCH='sh' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="sh" OM_BACKWARDCOMPAT="sh"
+make clean OM_ARCH='sh' OM_LIBC='uclibc' BUILDNO=$buildno OM_LINUXARCH="sh" OM_BACKWARDCOMPAT="sh"
+
 
 make OM_ARCH='i686' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
 make clean OM_ARCH='i686' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="x86" OM_BACKWARDCOMPAT="i386"
@@ -33,8 +40,8 @@ make OM_ARCH='aarch64' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BA
 make clean OM_ARCH='aarch64' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="arm64" OM_BACKWARDCOMPAT="arm64"
 make OM_ARCH='mips' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips"
 make clean OM_ARCH='mips' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="mips" OM_BACKWARDCOMPAT="mips"
-
-
+make OM_ARCH='sh' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="sh" OM_BACKWARDCOMPAT="sh"
+make clean OM_ARCH='sh' OM_LIBC='gnu' BUILDNO=$buildno OM_LINUXARCH="sh" OM_BACKWARDCOMPAT="sh"
 
 make startreport
 
