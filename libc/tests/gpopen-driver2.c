2d5aae84ecf70a9f403b6ed5a4b99d7cde13155a
cmgraff <cm0graff@gmail.com>
2017-05-29 20:08:56 -0500

    popen tests

--- libc/tests/gpopen-driver2.c
+++ libc/tests/gpopen-driver2.c
@@ -6,17 +6,16 @@ int main(void)
 	GFILE *fp;
 	int status;
 	char path[4096];
+	int c = 0;
 
 	if (( fp = gpopen("ls -la", "r")) == NULL)
 		return 1;
-
-	//while (fgets(path, 4096, fp) != NULL)
-	//	printf("%s", path); 
-	int c = 0;
+	
 	while ((c = gfgetc(fp)) != EOF)
 		gfputc(c, gstdout);
 
-	return status = gpclose(fp);
+	gpclose(fp);
+	return 0;
 }
 
 
