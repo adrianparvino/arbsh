7361e2a786af59a5a08266333e2205d90a5d007b
cmgraff <cm0graff@gmail.com>
2017-05-31 11:05:20 -0500

    standardize readline. seperate functions

---
+++ readline/src/readgetch.c
@@ -0,0 +1,97 @@
+#include <readline/readline.h>
+
+
+size_t greadgetch(char *l)
+{
+        static size_t len = 0;
+	static size_t ret = 0;
+	size_t z = 0;
+        int c; 
+       
+	c = readchar(); 
+        
+        switch (c) { 
+	case K_ESCAPE:
+		c = readchar();
+		switch (c) {
+		case '[':
+                        c = readchar();
+			switch (c) { 
+			case 'A': /* arrow up */
+				if ( hglb.c > 0 )
+				{
+					--hglb.c;
+					strcpy(l, hist[hglb.c].line);
+					len = hist[hglb.c].len;
+					hglb.laro = 0;
+				}
+				break;
+			case 'B': /* arrow down */
+				if ( hglb.c < hglb.t )
+				{
+					++hglb.c;
+					strcpy(l, hist[hglb.c].line);
+					len = hist[hglb.c].len;
+					hglb.laro = 0;
+				}
+				break;
+			case 'C': /* right arrow */ 
+				if ( hglb.laro > 0 )
+					--hglb.laro;
+                                break;
+			case 'D': /* left arrow */ 
+				if ( hglb.laro < len )
+					++hglb.laro; 
+				break; 
+			case 'H': /* Home */
+				hglb.laro = len;
+                                break;
+			case '5': /* page up ( not used ) */ 
+				c = readchar(); 
+				break;
+			case '6': /* page down ( not used ) */ 
+				c = readchar();
+				break;
+			}
+		}
+		return len;
+	case '\t':
+		//write(1, "tab complete\n", 13);
+		break;
+        case '\n':
+		write(1, "\n", 1);
+		l[len] = '\0';
+		ret = len;
+		len = 0;
+		hglb.r = 0;
+		hglb.laro = 0;
+                return ret;
+	case K_BACKSPACE:
+		if ( len == 0 ) 
+			return len; 
+		if ( hglb.laro ) 
+		{
+			z = (len - hglb.laro);
+                        memmove(l + z - 1, l + z, hglb.laro);
+		}
+
+		l[--len] = '\0';
+
+		return len; 
+        default:
+		if ( hglb.laro )
+		{
+			z = (len - hglb.laro);
+			memmove(l + z + 1, l + z, hglb.laro);
+			l[z] = c; 
+	}
+		else { 
+			l[len] = c;
+		}
+
+		l[++len] = '\0';
+
+                break;
+        }
+	return len;
+}
