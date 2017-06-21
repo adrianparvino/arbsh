42c6b03c6e79111ff7661f9a937b8e9b95eab3b1
cmgraff <cm0graff@gmail.com>
2017-06-20 21:53:56 -0500

    unused variables, incorrect return values

--- readline/tests/ircline-driver.c
+++ readline/tests/ircline-driver.c
@@ -5,16 +5,14 @@
 int main(void)
 {
         char *userline = malloc(BUFSIZ * sizeof(char));
-	size_t len = 0;
 	
         while ( 1 )
         {
 		userline[0] = '\0'; 
-                len = ircline(userline, "[ircline]>> ", 12);
+                ircline(userline, "[ircline]>> ", 12);
 		printf ("Finished line was: \n%s\n", userline);
 		add_history(userline);
         }
-	//free(hist);
         return 0;
 }
 
