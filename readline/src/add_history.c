adc0fb01e1aea89a0ea887e4f746e6560896406f
cmgraff <cm0graff@gmail.com>
2017-06-20 21:53:56 -0500

    unused variables, incorrect return values

--- readline/src/add_history.c
+++ readline/src/add_history.c
@@ -6,13 +6,12 @@ extern void add_history(const char *l)
 	if ( len > 1)
         { 
 		if (!(hist = realloc(hist, sizeof(struct hist) * ( hglb.t + 1))))
-                	return 0; // This _must_ be changed to a fatal error 
+                	return; // This _must_ be changed to a fatal error 
         	//strcpy(hist[hglb.t].line, l);
 		memcpy(hist[hglb.t].line, l, len + 1);
 		hist[hglb.t].len = len;
         	++hglb.t;
         }
 	hglb.c = hglb.t;
-	return 1;
 }
 
