5b76037b0c679b973be46717dbb602559b1dc3bf
cmgraff <cm0graff@gmail.com>
2017-06-20 22:07:23 -0500

    use a single write

--- readline/src/readprint.c
+++ readline/src/readprint.c
@@ -1,49 +1,52 @@
 #include <readline/readline.h>
 
 void greadprint(char *l, size_t len, char *prompt, size_t plen)
-{ 
-	
+{
         static size_t deep = 0;
 	size_t i;
 	size_t j;
 	size_t y;
 	size_t z;
+	char s[4096];
+	size_t a = 0;
         
 	/* calculate row pos and shove lines up */
         for (i = plen, j = len; j > (hglb.w - i) + 1; i = 0)
         { 
 		if ( deep == 0 ) /* if user is scanning don't shove lines */
-                	write(1, T_CURSUP1ROW, T_CURSUP1ROW_SZ);
+			a += sprintf(s + a, "%s", T_CURSUP1ROW);
 		else if ( deep > 0 )
 			--deep;
 		j -= ( hglb.w - i);
         }
    
 	/* clear character attributes */
-	write(1, T_CLRCHARS, T_CLRCHARS_SZ);
+	a += sprintf(s + a, "%s", T_CLRCHARS); 
 
         /* clear the line */
-        write(1, "\r", 1);
+	a += sprintf(s + a, "%s", "\r"); 
 
         /* write the prompt out */
-        write(1, prompt, plen);
+	a += sprintf(s + a, "%s", prompt); 
 
         /* write the user's line out */
-        write(1, l, len);
+	l[len] = 0;
+	a += sprintf(s + a, "%s", l); 
 
         /* clear to the end of the line ( ' ' .. + '\b' .. works too) */
-        write(1, T_CLRCUR2END, T_CLRCUR2END_SZ);
+	a += sprintf(s + a, "%s", T_CLRCUR2END); 
 
         /* walk the cursor backward to the user's position */
         for (i=0, y = len + plen; i < hglb.laro ; ++i, --y)
         { 
-               	write(1, T_CURSBK1COL, T_CURSBK1COL_SZ);
+		a += sprintf(s + a, "%s", T_CURSBK1COL); 
 		if ( y % (hglb.w) == 0 )
-		{
-			write(1, T_CURSUP1ROW, T_CURSUP1ROW_SZ);
+		{ 
+			a += sprintf(s + a, "%s", T_CURSUP1ROW);
 			for ( z=0; z < hglb.w ; ++z)
 				write(1, T_CURS4D1COL, T_CURS4D1COL_SZ);
 			++deep; 
 		}
         }
+	write(1, s, a);
 }
