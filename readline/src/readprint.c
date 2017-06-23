a9288063b9e5b846ece0075dd4de4ae24f16df3e
cmgraff <cm0graff@gmail.com>
2017-06-23 09:05:54 -0500

    use a compile time limit

--- readline/src/readprint.c
+++ readline/src/readprint.c
@@ -28,9 +28,9 @@ void greadprint(char *l, size_t len, char *prompt, size_t plen)
 	size_t j;
 	size_t y;
 	size_t z;
-	char s[4096];
+	char s[READLINE_LIMIT];
 	size_t a = 0;
-	size_t limit = 4095;
+	size_t limit = READLINE_LIMIT - 1;
         
 	s[0] = 0;
 	
@@ -38,37 +38,37 @@ void greadprint(char *l, size_t len, char *prompt, size_t plen)
         for (i = plen, j = len; j > (hglb.w - i) + 1; i = 0)
         { 
 		if ( deep == 0 ) /* if user is scanning don't shove lines */
-			a += szstrcatn(s + a, T_CURSUP1ROW, 4095, a);
+			a += szstrcatn(s + a, T_CURSUP1ROW, limit, a);
 		else if ( deep > 0 )
 			--deep;
 		j -= ( hglb.w - i);
         }
    
 	/* clear character attributes */
-	a += szstrcatn(s + a, T_CLRCHARS, 4095, a);
+	a += szstrcatn(s + a, T_CLRCHARS, limit, a);
 
         /* clear the line */
-	a += szstrcatn(s + a, "\r", 4095, a);
+	a += szstrcatn(s + a, "\r", limit, a);
 
         /* write the prompt out */
-	a += szstrcatn(s + a, prompt, 4095, a);
+	a += szstrcatn(s + a, prompt, limit, a);
 
         /* write the user's line out */
 	l[len] = 0;
-	a += szstrcatn(s + a, l, 4095, a);
+	a += szstrcatn(s + a, l, limit, a);
 
         /* clear to the end of the line ( ' ' .. + '\b' .. works too) */
-	a += szstrcatn(s + a, T_CLRCUR2END, 4095, a);
+	a += szstrcatn(s + a, T_CLRCUR2END, limit, a);
 
         /* walk the cursor backward to the user's position */
         for (i=0, y = len + plen; i < hglb.laro ; ++i, --y)
         { 
-		a += szstrcatn(s + a, T_CURSBK1COL, 4095, a);
+		a += szstrcatn(s + a, T_CURSBK1COL, limit, a);
 		if ( y % (hglb.w) == 0 )
 		{ 
-			a += szstrcatn(s + a, T_CURSUP1ROW, 4095, a);
+			a += szstrcatn(s + a, T_CURSUP1ROW, limit, a);
 			for ( z=0; z < hglb.w ; ++z)
-				a += szstrcatn(s + a, T_CURS4D1COL, 4095, a);
+				a += szstrcatn(s + a, T_CURS4D1COL, limit, a);
 			++deep; 
 		}
         }
