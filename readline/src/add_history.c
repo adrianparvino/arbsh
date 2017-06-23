b75acfaae8e65b6bb0a09dc631186cdde6f7c08d
cmgraff <cm0graff@gmail.com>
2017-06-23 09:05:54 -0500

    use a compile time limit

--- readline/src/add_history.c
+++ readline/src/add_history.c
@@ -3,11 +3,10 @@
 extern void add_history(const char *l)
 {
 	size_t len = strlen(l);
-	if ( len > 1)
+	if ( len > 0)
         { 
-		if (!(hist = realloc(hist, sizeof(struct hist) * ( hglb.t + 1))))
-                	return; // This _must_ be changed to a fatal error 
-        	//strcpy(hist[hglb.t].line, l);
+		if (!(hist = realloc(hist, sizeof(struct hist) * (hglb.t + 1))))
+                	return; // This _must_ be changed to a fatal error
 		memcpy(hist[hglb.t].line, l, len + 1);
 		hist[hglb.t].len = len;
         	++hglb.t;
