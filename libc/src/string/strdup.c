bfb35669063be75bbaace916665fd0eb3cffd27a
cmgraff <cm0graff@gmail.com>
2017-06-24 03:46:35 -0500

    ensure that malloc succeeds or return NULL

--- libc/src/string/strdup.c
+++ libc/src/string/strdup.c
@@ -1,16 +1,17 @@
 #include <gstddef.h>
-
 #include <gstring.h>
-
 #include <gstdlib.h>
 
 char *gstrdup(const char *s)
 {
-        char *ret;
-        size_t len = gstrlen(s) + 1;
-        ret = gmalloc(len);
-        gmemcpy(ret, s, len);
-        return ret;
+	char *ret;
+	size_t len = gstrlen(s) + 1;
+
+	if (!(ret = gmalloc(len)))
+		return NULL:
+
+	gmemcpy(ret, s, len);
+	return ret;
 }
 
 
