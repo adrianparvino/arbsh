35389d97659e7459ebb2292db3d599baf7ca26ee
cmgraff <cm0graff@gmail.com>
2017-07-09 07:39:08 -0500

    libc has moved to grafmusl / graflibc

--- oldlibc/src/ctype/isalnum.c
+++
@@ -1,7 +0,0 @@
-#include <gctype.h>
-int gisalnum(int x)
-{ 
-	if (gisalpha(x) || gisdigit(x))
-		return 1;
-	return 0;
-}
--- oldlibc/src/string/strcmp.c
+++
@@ -1,9 +0,0 @@
-#include <gstring.h>
-
-int gstrcmp(const char *s, const char *t)
-{
-        for ( ; *s == *t; s++, t++)
-                if (*s == '\0')
-                        return 0;
-        return *s - *t;
-}
