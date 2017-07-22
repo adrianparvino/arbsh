7a6b091b8f7ce5a658c1e0288d5c58c6d78359e8
cmgraff <cm0graff@gmail.com>
2017-07-22 14:57:54 -0500

    reasonably fast refresh

--- curses/src/curses/wrefresh.c
+++ curses/src/curses/wrefresh.c
@@ -1,42 +1,41 @@
 #include <curses.h> 
 
+
+
 int wrefresh(WINDOW *win)
 {
 	char buf[32];
-	size_t len = 0;
-	
-	_setcursor(0, 1);
-	//write(1, "fuck\n\n\n", 7);
-	clrtobot();
-//	if (win->clearok == 1)
-//	{
+	size_t len = 0; 
+	//_setcursor(0, 1); 
+	//clrtobot();
+	//if (win->clearok == 1)
+	//{
 	//	len = sprintf(buf, "%s", ansiglb.t_eraseall);
 	//	write(win->fd, buf, len);
 	//	_setcursor(1, 1);
-//	}
+	//}
 
 	size_t i = 0;
 	size_t j = 1;
 	size_t k = 1; 
-	static char last[10000] = { 0};
-	
 
 	for (;i < (win->rp - win->buf) ; ++i)
 	{ 
-
-
-		_setcursor(k, j); 
-                write(1, win->buf + i, 1); 
-
+		if (win->last[i] != win->buf[i] )
+		{
+			_setcursor(k, j); 
+			write(1, win->buf + i, 1); 
+			win->last[i] = win->buf[i];
 	
-		
-		
+		}
+	
                 if (j == COLS ) 
                 {
                         j = 1;
                         ++k;
                 } else ++j;
 	}
+
 	_setcursor(win->px, win->py + 1); 
 
         win->rp = win->buf;
