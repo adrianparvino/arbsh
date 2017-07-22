12e7b7380b7ad790d9d928cb95bbaa9b0a1511ec
cmgraff <cm0graff@gmail.com>
2017-07-22 14:04:50 -0500

    rehaul

--- curses/src/curses/wrefresh.c
+++ curses/src/curses/wrefresh.c
@@ -1,70 +1,45 @@
-#include <curses.h>
-
-
-
-typedef struct range {
-	size_t o;
-	size_t l;
-}range[1000];
-
-int _simple_refresh(WINDOW *win)
-{
-	size_t i = 1;
-	size_t j = 0;
-	size_t k = 1;
-	
-	static char last[4096] = { 0 };
-	//write(win->fd, win->buf, win->rp - win->buf);
-	for (i=0;i< (win->rp - win->buf);++i,++k)
-	{
-		if ( k == COLS )
-		{
-			k = 1;
-			++j;
-		}
-		
-		if ( last[i] != win->buf[i])
-		{
-			_setcursor(k, j);
-			write(win->fd, win->buf[i], 1);
-			last[i] = win->buf[i];
-		}
-		
-	}
-}
-
+#include <curses.h> 
 
 int wrefresh(WINDOW *win)
 {
 	char buf[32];
 	size_t len = 0;
-	_setcursor(1, 0);
+	
+	_setcursor(0, 1);
+	//write(1, "fuck\n\n\n", 7);
 	clrtobot();
-	if (win->clearok == 1)
-	{
-		len = sprintf(buf, "%s", ansiglb.t_eraseall);
-		write(win->fd, buf, len);
+//	if (win->clearok == 1)
+//	{
+	//	len = sprintf(buf, "%s", ansiglb.t_eraseall);
+	//	write(win->fd, buf, len);
+	//	_setcursor(1, 1);
+//	}
+
+	size_t i = 0;
+	size_t j = 1;
+	size_t k = 1; 
+	static char last[10000] = { 0};
+	
+
+	for (;i < (win->rp - win->buf) ; ++i)
+	{ 
+
+
+		_setcursor(k, j); 
+                write(1, win->buf + i, 1); 
+
+	
+		
+		
+                if (j == COLS ) 
+                {
+                        j = 1;
+                        ++k;
+                } else ++j;
 	}
-	//_simple_refresh(win);
-	write(win->fd, win->buf, win->rp - win->buf);
-	//size_t i = 0;
-	/*
-	for ( ; i < win->len ; ++i)
-	{
-		if (!(win->last[i] != win->buf[i]))
-		{
-			range[k].o = i;
-			for ( ; i < win->len ; ++i)
-				if (!(win->last[i] != win->buf[i]))
-				{
-					range[k].l++;
-					win->last[i] = win->buf[i];
-				}
-			k++;
-		} 
-	}
-	*/
-	win->rp = win->buf;
-	_setcursor(win->px, win->py);
+	_setcursor(win->px, win->py + 1); 
+
+        win->rp = win->buf;
+        win->len = (win->x * win->y);
 	return 0;
 }
