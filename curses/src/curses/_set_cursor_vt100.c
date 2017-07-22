a7d9889752e7bdc85c731ccaf7a33159fb8c4aef
cmgraff <cm0graff@gmail.com>
2017-07-22 14:04:50 -0500

    rehaul

--- curses/src/curses/_set_cursor_vt100.c
+++ curses/src/curses/_set_cursor_vt100.c
@@ -2,10 +2,13 @@
 
 void _setcursor(size_t x, size_t y)
 {
+	// x = LINES
+	// y = COLS
         char str[1024] = { 0 };
         size_t len = 0;
-	y = y + 1;/* count y from 1 */
+	//y = y + 1;/* count y from 1 */
+	//x +=1;
         len = sprintf(str, "\033[%zu;%zuH", x, y);
-        write(0, str, len);
+        write(1, str, len);
 }
 
