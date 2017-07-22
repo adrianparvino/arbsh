5a62da5327ad0a7cc81c812b80a2f69996c6adc8
cmgraff <cm0graff@gmail.com>
2017-07-22 15:05:03 -0500

    clean

--- curses/src/curses/wrefresh.c
+++ curses/src/curses/wrefresh.c
@@ -1,44 +1,44 @@
-#include <curses.h> 
-
-
+#include <curses.h>
 
 int wrefresh(WINDOW *win)
 {
 	char buf[32];
 	size_t len = 0; 
-	//_setcursor(0, 1); 
-	//clrtobot();
-	//if (win->clearok == 1)
-	//{
-	//	len = sprintf(buf, "%s", ansiglb.t_eraseall);
-	//	write(win->fd, buf, len);
-	//	_setcursor(1, 1);
-	//}
+	size_t i, j, k;
+	
+	/*
+	if (win->clearok == 1)
+	{
+		_setcursor(0, 1); 
+		len = sprintf(buf, "%s", ansiglb.t_eraseall);
+		write(win->fd, buf, len); 
+	}
+	*/
 
-	size_t i = 0;
-	size_t j = 1;
-	size_t k = 1; 
-
-	for (;i < (win->rp - win->buf) ; ++i)
+	for (i=0, j=1, k=1;i < (win->rp - win->buf) ; ++i)
 	{ 
 		if (win->last[i] != win->buf[i] )
 		{
-			_setcursor(k, j); 
-			write(1, win->buf + i, 1); 
+			_setcursor(k, j);
+			write(1, win->buf + i, 1);
 			win->last[i] = win->buf[i];
-	
 		}
 	
                 if (j == COLS ) 
                 {
                         j = 1;
                         ++k;
-                } else ++j;
+                } 
+		else 
+			++j;
 	}
 
+	/* set the final user cursor position */
 	_setcursor(win->px, win->py + 1); 
 
+	/* reset the buffer pointer and the len count */
         win->rp = win->buf;
         win->len = (win->x * win->y);
+
 	return 0;
 }
