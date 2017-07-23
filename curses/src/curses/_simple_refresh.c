27286010ecf17141e06650b66adc85b83c4d1c40
cmgraff <cm0graff@gmail.com>
2017-07-23 13:05:49 -0500

    clean up

--- curses/src/curses/_simple_refresh.c
+++ curses/src/curses/_simple_refresh.c
@@ -19,14 +19,3 @@ void _simple_refresh(WINDOW *win)
         }
 }
 
-void _clear_ok_wrap(WINDOW *win)
-{
-        char buf[32];
-        size_t len = 0;
-        if (win->clearok == 1)
-        {
-                _setcursor(0, 1);
-                len = sprintf(buf, "%s", ansiglb.t_eraseall);
-                write(win->fd, buf, len);
-        }
-}
