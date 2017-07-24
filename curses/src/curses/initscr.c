1ea8eee555825efb2f6171000c4d52799c04862a
cmgraff <cm0graff@gmail.com>
2017-07-24 16:43:52 -0500

    support window resizing

--- curses/src/curses/initscr.c
+++ curses/src/curses/initscr.c
@@ -26,10 +26,15 @@ WINDOW *initscr(void)
 	
 	if (win->buf)
 	{ 
+		/* cheap hack, instead of resizing the buffer and keeping
+		   track of window change state, just destroy the buffer
+		   which causes it to get remade to the correct size upon
+		   the next call to waddch
+		*/
 		win->clearok = 1;
 		_clear_ok_real(win); 
-		free(win->buf);// yes this is cheap
-		free(win->last);// yes this is cheap 
+		free(win->buf);
+		free(win->last);
 		win->buf = win->last = NULL;
 		win->rp = win->buf;
 	}
