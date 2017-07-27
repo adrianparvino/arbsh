a19b7817a0b32ad2b251219ac58191fe940eeec3
cmgraff <cm0graff@gmail.com>
2017-07-27 09:05:24 -0500

    tests

--- csafety/safe_free.c
+++ csafety/safe_free.c
@@ -15,18 +15,19 @@ void safe_free(void *a, void **b)
 { 
 	if (a != 0)
 	{
-		printf("A true free was detected\n"); 
 		free(a);
+		fprintf(stderr, "A true free was detected\n");
 	}
 	else {
-		printf("A double free was detected\n"); 
-	} 
+		fprintf(stderr, "A double free was detected and prevented\n");
+	}
 	*b = NULL;
-} 
+}
 
 int main(void)
 { 
-	c.rp = malloc(1000);
+	
+	c.rp = malloc(1000); 
 	safe_free(c.rp,(void**)&(c.rp)); 
 	safe_free(c.rp,(void**)&(c.rp)); 
 	safe_free(c.rp,(void**)&(c.rp)); 
@@ -37,5 +38,26 @@ int main(void)
         safe_free(d->rp,(void**)&(d->rp));
         safe_free(d->rp,(void**)&(d->rp));
         safe_free(d->rp,(void**)&(d->rp));
+
+	char *l = malloc(1000); 
+	safe_free(l, (void**)&l);
+	safe_free(l, (void**)&l);
+	safe_free(l, (void**)&l);
+	safe_free(l, (void**)&l);
+
+	bb *ff;
+	ff = malloc(sizeof(bb));
+	ff->rp = malloc(1000);
+	safe_free(ff->rp, (void**)&(ff->rp));
+	safe_free(ff->rp, (void**)&(ff->rp));
+	safe_free(ff->rp, (void**)&(ff->rp));
+	safe_free(ff->rp, (void**)&(ff->rp));
+	safe_free(ff->rp, (void**)&(ff->rp));
+	safe_free(ff, (void**)&ff);
+	safe_free(ff, (void**)&ff);
+	safe_free(ff, (void**)&ff);
+	safe_free(ff, (void**)&ff);
+	
+
 	return 0; 
 }
