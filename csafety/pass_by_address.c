989600f476127c8dd8d3de755f2ba836cf7fbcc2
cmgraff <cm0graff@gmail.com>
2017-07-28 12:13:29 -0500

    pass by address of a pointer to a pointer to a type

---
+++ csafety/pass_by_address.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+	C does not support "pass by reference" nonetheless by passing a
+	the value of an address to a function accepting a pointer to a 
+	pointer to a type we can modify a value in C similiar to C++'s
+	"pass by reference". The canonical example of this is getline()
+	from the C standard library.
+
+	Presented below is the function pass_bappt() (Pass by address of a
+	pointer to a pointer to a type).
+*/
+
+struct c { 
+        char *rp; 
+} c;
+
+typedef struct{ 
+        char *rp; 
+} bb;
+
+bb d[10]; 
+
+void does_not_work(void *a) 
+{
+	/* Doesn't work! (obviously) */
+	a = "Does not work!";
+}
+
+void pass_bappt(void **a) 
+{ 
+	/*
+		Pass by pointer to the first member's value
+	*/
+	*a = "hello world";
+}
+
+int main(void)
+{ 
+	bb *ff;
+	ff = malloc(sizeof(bb));
+	ff->rp = malloc(1000);
+
+	ff->rp = "This string should be overwritten with a new pointer to \"hello world\" ";
+
+     	printf("%s\n", ff->rp);
+
+        pass_bappt((void**)&(ff->rp));
+   
+    	printf("%s\n", ff->rp);
+     
+      	does_not_work(ff->rp);  /* Doesn't work! (obviously) */
+	
+	printf("%s\n", ff->rp);
+	
+	return 0; 
+}
