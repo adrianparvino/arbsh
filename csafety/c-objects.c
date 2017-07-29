c0e3a373c5672f4d255cf6db42a8645e6dff764d
cmgraff <cm0graff@gmail.com>
2017-07-28 20:10:22 -0500

    objects with C

--- csafety/c-objects.c
+++ csafety/c-objects.c
@@ -12,20 +12,26 @@ int main(void)
 	size_t i = 0;
 	size_t lim = 10;
 
-	object *obja = malloc(sizeof(object) * lim); 
+	/* create an array of objects */
+	object *o = malloc(sizeof(object) * lim); 
 
+	/* allocate memory for and populate the members of the array of objects */
 	for ( ; i < lim ; ++i)
 	{
-		(obja+i)->rp = malloc(sizeof(char) * 100);
-		(obja+i)->len = sprintf((obja+i)->rp, "string %zu\n", i); 
+		(o+i)->rp = malloc(sizeof(char) * 100);
+		(o+i)->len = sprintf((o+i)->rp, "string %zu\n", i); 
 	} 
 
+	/* test the array of objects (fast write) */
 	for (i=0 ; i < lim ; ++i)
-		write(1, (obja+i)->rp, (obja+i)->len);
+		write(1, (o+i)->rp, (o+i)->len);
 
+	/* free the objects' member memory allocations */
 	for (i=0 ; i < lim ; ++i)
-                free((obja+i)->rp);
+                free((o+i)->rp);
 
-	free(obja);
+	/* free the object */
+	free(o);
 	return 0; 
 }
+
