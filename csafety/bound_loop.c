74351b53b2afdd8218648c6a729f871380dac58b
cmgraff <cm0graff@gmail.com>
2017-07-28 19:31:33 -0500

    bound_loop.c

--- csafety/bound_loop.c
+++ csafety/bound_loop.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
-#include <stdio.h> 
-
-#define double_bound_loop(v,m,z) for(v=0; v<(m)&&v<(z); v++)
+#include <stdio.h>
 
 int main(void)
 {
-	size_t a;
-	size_t b = 10;
-
-	double_bound_loop(a, b, (size_t) -1) 
-		printf("Inside the loop %zu\n", a);
+	size_t i = 0;
+	size_t limit = 10;
+	
+	for (; i < limit && i <(size_t)-1;++i)
+		printf("Inside the hard bound loop \n");
 
 	return 0;
 }
+
+
