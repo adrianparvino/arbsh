f06895530179035b89098f6c30dcc48a55503742
cmgraff <cm0graff@gmail.com>
2017-07-28 20:15:19 -0500

    objects with C

--- csafety/c-objects.c
+++ csafety/c-objects.c
@@ -2,6 +2,18 @@
 #include <stdio.h> 
 #include <unistd.h>
 
+/*
+	The C standard identifies objects in a very broad sense and applies the
+	term to "any object in memory". However this definition of object does
+	not address the power that C has to create and use objects in a modern
+	sense of it being the primary predecessor to "object oriented
+	programing".
+
+	Here is a method of "programming with objects" in C.
+
+*/
+
+
 typedef struct{ 
         char *rp; 
 	size_t len;
