4fba3dafb0009b09ff81b0a4e6a1947e6bf8163d
cmgraff <cm0graff@gmail.com>
2017-07-30 20:25:07 -0500

    typo

--- csafety/opaque/main-driver.c
+++ csafety/opaque/main-driver.c
@@ -7,5 +7,5 @@ int main(void)
 {
 	object *o;
 	o = object_init(o);
-	// o->rp = "hello";  //<--- This will fail if it a truly opaque type
+	//o->rp = "hello";  //<--- This will fail if it is a truly opaque type
 }
