de74424c3d2ce43cb21adcfcdff730997ac920db
cmgraff <cm0graff@gmail.com>
2017-07-31 08:23:16 -0500

    free

--- csafety/opaque/shared.h
+++ csafety/opaque/shared.h
@@ -2,6 +2,6 @@ typedef struct object object;
 size_t object_size(void); 
 object *object_init(object *);
 object *object_populate(object *, char *);
-void object_write(object *o);
-
+void object_write(object *);
+void object_free(object *);
 
