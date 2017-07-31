8e549dd2359726be37a329a3ca6e5076946fe1fb
cmgraff <cm0graff@gmail.com>
2017-07-30 19:11:08 -0500

    works

---
+++ csafety/opaque-new/driver.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "obj.h"
+
+int main(void)
+{
+	struct obj *o;
+	o->id = 0;
+	//o->rp[0] = 0;
+}
---
+++ csafety/opaque-new/obj.c
@@ -0,0 +1,25 @@
+/* obj.c */
+#include <stdio.h>
+#include "obj.h"
+
+struct obj {
+    int id;
+};
+
+/*
+ * The caller will handle allocation.
+ * Provide the required information only
+ */
+
+size_t obj_size(void) {
+    return sizeof(struct obj);
+}
+
+void obj_setid(struct obj *o, int i) {
+    o->id = i;
+}
+
+int obj_getid(struct obj *o) {
+    return o->id;
+}
+
