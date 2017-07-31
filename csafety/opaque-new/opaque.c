fcabdfb154f3b2cf9386541c74353c94e6955d2b
cmgraff <cm0graff@gmail.com>
2017-07-30 20:20:45 -0500

    opaque types

---
+++ csafety/opaque-new/opaque.c
@@ -0,0 +1,26 @@
+/* obj.c */
+#include <stdlib.h>
+#include "shared.h"
+
+struct object {
+	char *rp;
+	size_t len;
+}; 
+
+size_t object_size(void)
+{
+	return sizeof(object);
+}
+
+object *object_init(object *o)
+{
+	o = malloc (object_size());
+	o->rp = malloc(1000);
+	o->len = 0;
+	return o;
+}
+
+
+
+
+
