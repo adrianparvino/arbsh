3939a65f01c9485c16330953461789f51900e5a1
cmgraff <cm0graff@gmail.com>
2017-08-01 04:24:37 -0500

    comments

--- csafety/opaque/opaque.c
+++ csafety/opaque/opaque.c
@@ -4,6 +4,17 @@
 #include <string.h>
 #include "shared.h"
 
+/*
+	This is the actual opaque API library implementation. We declare an
+	instantiation of the primary API structure from "shared.h" our shared
+	header. But in this version we actually define its members. Inside of
+	this file we can access all of the members of our opaque type but the
+	end user of the API, or indeed anyone outside of this file can not.
+	However, they can use the library as a library of opaque objects. The
+	canonical example of the opaque object in C is the FILE * structure
+	from stdio.  
+*/
+
 struct object {
 	char *rp;
 	size_t len;
