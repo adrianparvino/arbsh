d774c8d511c6f2a9466b9e973a7156ac0e45869f
cmgraff <cm0graff@gmail.com>
2017-08-10 11:53:35 -0500

    pipes

--- csafety/src/unlimited_pipeline.c
+++ csafety/src/unlimited_pipeline.c
@@ -6,11 +6,13 @@ typedef struct{
         char **cmd;
 	int in;
 	int out;
+	pid_t pids;
+	int err;
 } object;
 
 int main(void)
 { 
-	int filedes[2];
+	int fildes[2];
 	size_t lim = 10;
 
 	object *o = malloc(sizeof(object) * 10);
@@ -29,10 +31,33 @@ int main(void)
 
 	for(i=0;i<lim;++i)
 	{
-		if ( fork() == 0)
-		{
+		
+		if ( (o+i)->pids = fork() == 0)
+		{ 
+                        if ( pipe(fildes) == -1 )
+                                return 0;
+			(o+i)->in = fildes[0];
+			(o+i)->out = fildes[1];
+			dup2((o+i)->in, STDIN_FILENO);
+                        
+                        dup2((o+i)->out, STDOUT_FILENO);
+
+                        //cmds[k + 1].in = fildes[0];
+                        //cmds[k].out = fildes[1];
+			
+               
 			execvp((o+i)->cmd[0], (o+i)->cmd);
+
 			_exit(1);
+
+			waitpid((o+i)->pids, &(o+i)->err, 0);
+
+                       
+		
+                        close((o+i)->out);
+                        
+                        close((o+i)->in);
+	
 		} 
 	}
 
