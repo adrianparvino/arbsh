7f89ee276dc1d5915c9e53e2d9418fdfb4d11e54
cmgraff <cm0graff@gmail.com>
2017-08-10 11:42:29 -0500

    pipes

--- csafety/src/unlimited_pipeline.c
+++ csafety/src/unlimited_pipeline.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 typedef struct{ 
-        char cmd[1024];
+        char **cmd;
 	int in;
 	int out;
 } object;
@@ -11,6 +11,30 @@ typedef struct{
 int main(void)
 { 
 	int filedes[2];
+	size_t lim = 10;
+
+	object *o = malloc(sizeof(object) * 10);
+
+	size_t i = 0;
+	(o+i)->cmd = malloc(sizeof(char *) * 10);
+	(o+i)->cmd[0] = "ls";	
+	(o+i)->cmd[1] = "-l";	
+
+	for(i=1;i<lim;++i)
+	{
+		(o+i)->cmd = malloc(sizeof(char *) * 10);
+		(o+i)->cmd[0] = "wc";
+		(o+i)->cmd[1] = "-l";
+	}
+
+	for(i=0;i<lim;++i)
+	{
+		if ( fork() == 0)
+		{
+			execvp((o+i)->cmd[0], (o+i)->cmd);
+			_exit(1);
+		} 
+	}
 
 	return 0; 
 }
