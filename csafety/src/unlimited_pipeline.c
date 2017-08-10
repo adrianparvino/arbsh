d954e90821046013db130255e7dfbaadb4d02935
cmgraff <cm0graff@gmail.com>
2017-08-10 11:32:37 -0500

    unlimited unix pipe

---
+++ csafety/src/unlimited_pipeline.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h> 
+#include <unistd.h>
+
+typedef struct{ 
+        char cmd[1024];
+	int in;
+	int out;
+} object;
+
+int main(void)
+{ 
+	int filedes[2];
+
+	return 0; 
+}
+
