0d3c0ec1c4035e09adc196efc9ef88a8e95b3fab
cmgraff <cm0graff@gmail.com>
2017-08-21 10:21:03 -0500

    use 80 char width

--- csafety/src/safe_free.c
+++ csafety/src/safe_free.c
@@ -1,32 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h> 
 /* 
-The C11 draft states "If ptr is a null pointer, no action occurs."
-therefore preventing "double frees" is reduced to the simple
-operation of explicitly setting deallocated variables to NULL.
+The C11 draft states "If ptr is a null pointer, no action occurs." therefore 
+preventing "double frees" is reduced to the simple operation of explicitly 
+setting deallocated variables to NULL.
 */
 
+	#include <stdlib.h>
+	#include <stdio.h>
 
-typedef struct{ 
-        char *rp; 
-	size_t len;
-}object;
+	typedef struct {
+	        char *rp;
+		size_t len;
+	} object;
 
-void *safe_free(void *a) 
-{ 
-	free(a); 
-	return NULL; 
-}
+	void *safe_free(void *a) 
+	{ 
+		free(a);
+		return NULL;
+	}
 
-int main(void)
-{ 
-	object *o;
-	if(!(o = malloc(sizeof(object))))
-		return 1;
-	o->rp = malloc(1000); 
-	o->rp = safe_free(o->rp);
-	o->rp = safe_free(o->rp); // does nothing
-	o = safe_free(o);
-	o = safe_free(o); // does nothing
-	return 0; 
-}
+	int main(void)
+	{	 
+		object *o;
+		if(!(o = malloc(sizeof(object))))
+			return 1;
+		o->rp = malloc(1000); 
+		o->rp = safe_free(o->rp);
+		o->rp = safe_free(o->rp); /* does nothing */
+		o = safe_free(o);
+		o = safe_free(o); /* does nothing */
+		return 0; 
+	}
