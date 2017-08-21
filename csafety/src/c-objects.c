28067b540a3165fde9086aaed355023e9ecaf011
cmgraff <cm0graff@gmail.com>
2017-08-21 09:50:34 -0500

    improve code -- use pointer diffs

--- csafety/src/c-objects.c
+++ csafety/src/c-objects.c
@@ -1,52 +1,48 @@
-#include <stdlib.h>
-#include <stdio.h> 
-#include <unistd.h>
-
 /*
-	The C standard identifies objects in a very broad sense and applies the
-	term to "any object in memory". However this definition of object does
-	not address the power that C has to create and use objects in a modern
-	sense of it being the primary predecessor to "object oriented
-	programing".
+The C standard identifies objects in a very broad sense and applies the term to
+"any object in memory". However this definition of object does not address the 
+power that C has to create and use objects in a modern sense of it being the 
+primary predecessor to "object oriented programing".
 
-	Here is a method of "programming with objects" in C.
-
+Presented below is a method of "programming with objects" in C.
 */
 
+	#include <stdlib.h>
+	#include <stdio.h> 
+	#include <unistd.h>
 
-typedef struct{ 
-        char *rp; 
-	size_t len;
-} object; 
+	typedef struct{ 
+	        char *rp; 
+		size_t len;
+	} object; 
 
-int main(void)
-{ 
-	size_t i = 0;
-	size_t lim = 10;
-
-	/* create an array of objects */
-	object *p, *o;
-	if (!(o = p = malloc(sizeof(object) * lim)))
-		return 1;
-
-	/* allocate memory and populate array members */
-	for (o=p; i < lim ; ++i, ++o)
+	int main(void)
 	{
-		o->rp = malloc(sizeof(char) * 100);
-		o->len = sprintf(o->rp, "string %zu\n", i); 
-	} 
+		size_t lim = 10;
+	
+		/* create an array of objects */
+		object *p, *o;
+		if (!(o = p = malloc(sizeof(object) * lim)))
+			return 1;
 
-	/* test the array of objects (fast write!) */
-	for (i=0, o=p; i < lim ; ++i, ++o)
-		write(1, o->rp, o->len);
+		/* allocate memory and populate array members */
+		for (o=p; o-p < lim ; ++o)
+		{
+			o->rp = malloc(sizeof(char) * 100);
+			o->len = sprintf(o->rp, "string %zu\n", o-p); 
+		} 
 
-	/* free the objects' member memory allocations */
-	for (i=0, o=p; i < lim ; ++i, ++o)
-                free(o->rp);
+		/* test the array of objects (fast write!) */
+		for (o=p; o-p < lim ; ++o)
+			write(1, o->rp, o->len);
 
-	/* free the object */
-	free(p);
+		/* free the objects' member memory allocations */
+		for (o=p; o-p < lim ; ++o)
+	                free(o->rp);
 
-	return 0; 
-}
+		/* free the object */
+		free(p);
+
+		return 0; 
+	}
 
