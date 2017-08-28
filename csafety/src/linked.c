4a9c4e035d592e61f3f2fc89c8f57f1b707f0329
cmgraff <cm0graff@gmail.com>
2017-08-27 20:22:52 -0500

    free the list

--- csafety/src/linked.c
+++ csafety/src/linked.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct object{
-	int value;
+typedef struct object{ 
+	size_t i;
 	struct object *next;
 } object; 
 
-object *populate(object *head, int value)
+object *freeobj(object*);
+
+void *safe_free(void *a)
 {
-	object* o = malloc(sizeof(object));
-	o->value = value; 
+        free(a);
+        return NULL;
+}
+
+object *populate(object *head, size_t i)
+{
+	object* o;
+	if (!(o = malloc(sizeof(object))))
+	{
+		fprintf(stderr, "Malloc request failed, freeing list\n");
+		return head = freeobj(head);/* NULL */
+	}
+	o->i = i; 
 	o->next = head;
 	return o; 
 }
@@ -18,37 +31,47 @@ void iterate(object* head)
 {
 	object *o;
 	for(o = head;o;o = o->next)
-		printf("[%d]-->", o->value); 
+		printf("(%zu)-->", o->i); 
 	printf("END\n\n");
 }
 
-void freeobj(object* head)
+object *freeobj(object* head)
 {
         object *o;
 	object *hold = NULL;
         for(o = head;o;o = o->next)
 	{
 		if (hold) 
-                	free(hold); 
+		{
+			printf("free(%zu)-->", hold->i);
+                	hold = safe_free(hold); 
+		}
 		hold = o; 
 	}
-	
-	free(hold);
-        printf("free\n\n");
+	if (hold)
+	{
+		printf("free(%zu)-->", hold->i);
+		hold = safe_free(hold);
+	}
+	if (o == head)
+		printf("Nothing to be freed  ");
+        printf("END\n\n");
+	return hold;
 }
-int main(int argc, char const *argv[])
-{
-	object* head = NULL;
-	size_t i;
 
-	object *hold = head;
+int main(int argc, char const *argv[])
+{ 
+	size_t i;
+	object* head = NULL;
+
 	for(i = 0; i<=20; i++) 
 		head = populate(head, i);
-
+	head = populate(head, i);
+	head = populate(head, i);
+	iterate(head);
 	iterate(head); 
-	
-	iterate(head); 
-	freeobj(head);
-	
+	head = freeobj(head);
+	head = freeobj(head);
+	head = freeobj(head);
 	return 0;
 }
