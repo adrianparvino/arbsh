abb8509098e49732c71b199d4dec7d9aa759dd50
cmgraff <cm0graff@gmail.com>
2017-08-29 07:52:28 -0500

    use verbose malloc

--- csafety/src/linked.c
+++ csafety/src/linked.c
@@ -7,104 +7,115 @@ direction. Presented below are the functions forpop and backpop which create a
 forward and backward populated linked list respectively,
 */
 
-#include <stdio.h>
-#include <stdlib.h> 
-typedef struct object{ 
-	size_t i;
-	struct object *next;
-} object;
+	#include <stdio.h>
+	#include <stdlib.h>
 
-object *freeobj(object*);
+	typedef struct object{ 
+		size_t i;
+		struct object *next;
+	} object;
 
-void *safe_free(void *a)
-{
-        free(a);
-        return NULL;
-}
+	object *freeobj(object*);
 
-object *forpop(object *head, size_t item)
-{
-	/* modified linked list "holds hands" forwards */
-        static object *tail = NULL;
-        object *ptr;
-	if(!(ptr = malloc(sizeof(object))))
+	void *safe_free(void *a)
 	{
-		fprintf(stderr, "Malloc request failed, freeing list\n");
-		return head = freeobj(head);
+	        free(a);
+	        return NULL;
 	}
-        ptr->next = NULL;
-        ptr->i = item;
-        if (!(head))
-                tail = head = ptr;
-        else if (tail){
-                tail->next = ptr;
-                tail = ptr;
-        }
-        return head;
-}
-object *backpop(object *head, size_t i)
-{
-	/* simple linked list "holds hands" backwards */
-	object* o;
-	if (!(o = malloc(sizeof(object))))
+
+	void *verbose_malloc(size_t i)
 	{
-		fprintf(stderr, "Malloc request failed, freeing list\n");
-		return head = freeobj(head);
+		void *ret;
+		if (!(ret = malloc(i))) 
+			perror("malloc error: ");
+		return ret;
 	}
-	o->i = i;
-	o->next = head;
-	return o;
-}
 
-void iterate(object* head)
-{
-	object *o;
-	for(o = head;o;o = o->next)
-		printf("(%zu)-->", o->i); 
-	printf("END\n\n");
-}
-
-object *placefreeobj(object *hold)
-{
-	if (hold)
-        {
-                printf("free(%zu)-->", hold->i);
-                hold = safe_free(hold);
-        }
-	return hold;
-}
-
-object *freeobj(object *head)
-{
-        object *o;
-	object *hold = NULL;
-        for(o = head;o;o = o->next)
+	object *forpop(object *head, size_t i)
 	{
+		/* modified linked list shares members forwards */
+	        static object *tail = NULL;
+	        object *ptr;
+		if(!(ptr = verbose_malloc(sizeof(object))))
+			return head = freeobj(head);
+
+	        ptr->next = NULL;
+	        ptr->i = i;
+	        if (!(head))
+	                tail = head = ptr;
+	        else if (tail){
+	                tail->next = ptr;
+	                tail = ptr;
+	        }
+	        return head;
+	}
+
+	object *backpop(object *head, size_t i)
+	{
+		/* simple linked list shares members backwards */
+		object* o;
+		if (!(o = verbose_malloc(sizeof(object))))
+			return head = freeobj(head);
+		o->i = i;
+		o->next = head;
+		return o;
+	}
+	
+	void iterate(object* head)
+	{
+		object *o;
+		for(o = head;o;o = o->next)
+			printf("(%zu)-->", o->i); 
+		printf("END\n\n");
+	}
+
+	object *placefreeobj(object *hold)
+	{
+		if (hold)
+	        {
+	                printf("free(%zu)-->", hold->i);
+	                hold = safe_free(hold);
+	        }
+		return hold;
+	}
+
+	object *freeobj(object *head)
+	{
+	        object *o;
+		object *hold = NULL;
+	        for(o = head;o;o = o->next)
+		{
+			hold = placefreeobj(hold);
+			hold = o; 
+		}
 		hold = placefreeobj(hold);
-		hold = o; 
+	
+		if (o == head)
+			printf("Nothing to be freed  ");
+	        printf("END\n\n");
+		return hold;
 	}
-	hold = placefreeobj(hold);
 
-	if (o == head)
-		printf("Nothing to be freed  ");
-        printf("END\n\n");
-	return hold;
-}
+	int main(void)
+	{ 
+		size_t i;
+		object* head = NULL; 
+		for(i = 0; i<=20; i++) 
+			if (!(head = forpop(head, i)))
+				return 1;
+	
+		iterate(head);
+		head = freeobj(head);
+	
+		for(i = 0; i<=20; i++) 
+			if (!(head = backpop(head, i)))
+				return 1;
+	
+		iterate(head);
+		head = freeobj(head);
+		char buf[1024];
+		char *num = fread(buf, 1, 1, stdin);
 
-int main(void)
-{ 
-	size_t i;
-	object* head = NULL; 
-	for(i = 0; i<=20; i++) 
-		head = forpop(head, i); 
-	
-	iterate(head);
-	head = freeobj(head);
-	
-	for(i = 0; i<=20; i++) 
-		head = backpop(head, i); 
-	
-	iterate(head);
-	head = freeobj(head);
-	return 0;
-}
+		return 0;
+	}
+
