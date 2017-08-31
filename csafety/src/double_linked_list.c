a038e5e49ddc917cec2c139f9fa8494f1dcccfcd
cmgraff <cm0graff@gmail.com>
2017-08-31 03:23:39 -0500

    individuate

--- csafety/src/double_linked_list.c
+++ csafety/src/double_linked_list.c
@@ -10,7 +10,21 @@ typedef struct node {
 
 node *head, *tail;
 
-void insert(int item, bool at_tail)
+void inserthead(int item)
+{
+	node *ptr = malloc(sizeof(node));
+	ptr->item = item;
+	ptr->prev = ptr->next = NULL;
+
+	if (NULL == head) {
+		head = tail = ptr; 
+	} else {
+		ptr->next = head;
+		head->prev = ptr;
+		head = ptr;
+	}
+} 
+void inserttail(int item)
 {
 	node *ptr = malloc(sizeof(node));
 	ptr->item = item;
@@ -18,15 +32,11 @@ void insert(int item, bool at_tail)
 
 	if (NULL == head) {
 		head = tail = ptr;
-	} else if (at_tail) {
+	} else {
 		tail->next = ptr;
 		ptr->prev = tail;
 		tail = ptr;
-	} else {
-		ptr->next = head;
-		head->prev = ptr;
-		head = ptr;
-	}
+	} 
 } 
 
 void list(node *ptr)
@@ -42,7 +52,12 @@ int main(int argc, char *argv[])
 {
 	size_t i;
 	for (i = 1; i <= 10; i++)
-		insert(i, i < 6);
+		inserthead(i);
+
+	list(head); // 10 9 8 7 6 1 2 3 4 5 
+	
+	for (; i <= 20; i++)
+		inserttail(i);
 
 	list(head); // 10 9 8 7 6 1 2 3 4 5 
 }
