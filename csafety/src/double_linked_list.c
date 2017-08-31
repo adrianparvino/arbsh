57a6cf2eaa3d118e62a29b3ef731c94055f62c16
cmgraff <cm0graff@gmail.com>
2017-08-31 11:51:41 -0500

    version which does not use globals

--- csafety/src/double_linked_list.c
+++ csafety/src/double_linked_list.c
@@ -10,6 +10,22 @@ typedef struct node {
 
 node *head, *tail;
 
+node *inserthead2(node *tal, node *hed, int item)
+{ 
+	node *ptr = malloc(sizeof(node));
+	ptr->item = item;
+	ptr->prev = ptr->next = NULL;
+
+	if (NULL == hed) {
+		hed = tal = ptr; 
+	} else {
+		ptr->next = hed;
+		hed->prev = ptr;
+		hed = ptr;
+	}
+	return hed;
+}
+
 void inserthead(int item)
 { 
 	node *ptr = malloc(sizeof(node));
@@ -23,6 +39,23 @@ void inserthead(int item)
 		head->prev = ptr;
 		head = ptr;
 	}
+}
+
+
+node *inserttail2(node *hed, node *tal, int item)
+{ 
+	node *ptr = malloc(sizeof(node));
+	ptr->item = item;
+	ptr->prev = ptr->next = NULL;
+
+	if (NULL == hed) {
+		hed = tal = ptr;
+	} else {
+		tal->next = ptr;
+		ptr->prev = tal;
+		tal = ptr;
+	} 
+	return tal;
 } 
 void inserttail(int item)
 { 
@@ -38,9 +71,6 @@ void inserttail(int item)
 		tail = ptr;
 	} 
 } 
-
-
-
 void listbackward(node *ptr)
 { 
 	while (NULL != ptr)
@@ -50,6 +80,7 @@ void listbackward(node *ptr)
 	} 
 	printf("\n");
 }
+
 void listforward(node *ptr)
 { 
 	while (NULL != ptr)
@@ -59,9 +90,26 @@ void listforward(node *ptr)
 	} 
 	printf("\n");
 }
+
 int main(int argc, char *argv[])
 {
 	size_t i;
+
+	node *hed = NULL;
+	node *tal = NULL;
+	tal = hed = inserthead2(tal, hed, 0);
+	for (i = 1; i <= 10; i++)
+		hed = inserthead2(tal, hed, i);
+	listforward(hed);
+	listbackward(tal);
+
+
+	for (i; i <= 20; i++)
+		tal = inserttail2(hed, tal, i);
+	listforward(hed);
+	listbackward(tal);
+
+	return 0;
 	for (i = 1; i <= 10; i++)
 		inserthead(i);
 
