27027e9f5d0d1cb15c29b32933c2dd3a821cde17
cmgraff <cm0graff@gmail.com>
2017-09-09 13:10:54 -0500

    node deletion

--- csafety/src/trie.c
+++ csafety/src/trie.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 /* alphasize must be sufficient to map all ASCII values to an indice */
 size_t alphasize = 128;
@@ -8,6 +10,52 @@ typedef struct object {
 	int leaf; 
 	struct object **children; 
 }object;
+
+size_t leafNode(object *o)
+{
+        return (o->leaf != 0);
+}
+
+size_t trie_isfreenode(object *o)
+{
+        size_t i;
+        for(i = 0; i < alphasize; i++)
+        {
+                if( o->children[i] )
+                        return 0;
+        }
+
+        return 1;
+}
+
+bool deleteHelper(object *o, char key[], size_t level, size_t len)
+{
+        size_t index;
+        if( o )
+        {
+                if( level == len ) // Base case
+                {
+                        if( o->leaf )
+                        {
+                                o->leaf = 0; // Unmark leaf node 
+                                if( trie_isfreenode(o) )        // If empty, node to be deleted 
+                                        return true;
+                                return false;
+                        }
+                }
+                else // Recursive case
+                {
+                        index = key[level];
+                        if( deleteHelper(o->children[index], key, level+1, len) )
+                        {
+                                free(o->children[index]);// last node marked, delete it 
+                                o->children[index] = NULL;
+                                return ( !leafNode(o) && trie_isfreenode(o) );  // recursively climb up, and delete eligible nodes
+                        }
+                }
+        }
+        return false;
+}
 
 object *trie_init(void)
 {
@@ -54,6 +102,54 @@ int trie_search(object *o, const char *pat)
 	return 0; 
 }
 
+object *trie_nodel(object *o, const char *pat)
+{
+	object *t = o;
+        size_t i;
+	size_t j = 0;
+        size_t ind; 
+	object *n[10024] = { NULL } ;
+        for (i = 0; pat[i]; i++)
+        {
+                ind = pat[i];
+                if (!o->children[ind])
+			break;
+			
+                        //return 0; 
+		object *tmp = o;
+		
+		if (o)
+		{
+		n[j++] = o->children[ind];
+		o->leaf = 0;
+		}
+                o = o->children[ind];
+		
+		
+
+	
+		//free(tmp);
+		//free(o->children);
+        } 
+	for (j=0;j<i; ++j)
+	{
+		if (n[j])
+		{
+		
+		free(n[j]);
+		//n[j] = NULL;
+		//n[j]->leaf = 0;
+		
+		
+	
+		
+		}
+	}
+     return t;
+      
+       
+}
+
 void trie_free(object* root)
 { 
 	size_t i;
@@ -86,7 +182,6 @@ void trie_histogram(object* root, size_t level)
     
 	for (i = 0; i < alphasize; i++)
 	{ 
-		
 		if (root->children[i])
 		{
 			printf("-%c", (int)i);
@@ -135,6 +230,8 @@ int main(void)
 		else
 			printf("%s  -- Found\n", queries[i]);
 	} 
+	//trie_nodel(root, "the");
+	deleteHelper((root), patterns[1] , 0, strlen(patterns[1]));
 	printf("\n\n\n");
 	trie_display(root, 0);
 	printf("\n\n\n");
