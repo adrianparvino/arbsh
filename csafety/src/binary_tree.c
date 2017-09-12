7661b6a072a038f8794c6e2ef05dda2ffab5cea9
cmgraff <cm0graff@gmail.com>
2017-09-12 16:56:31 -0500

    morris traversal does not require recursion

--- csafety/src/binary_tree.c
+++ csafety/src/binary_tree.c
@@ -11,46 +11,6 @@ struct tnode {
 	struct tnode *left;
 	struct tnode *right;
 };
-void MorrisTraversal(struct tnode *root)
-{
-  struct tnode *current,*pre;
- 
-  if(root == NULL)
-     return; 
- 
-  current = root;
-  while(current != NULL)
-  {                 
-    if(current->left == NULL)
-    {
-      printf("%s ", current->word);
-      current = current->right;      
-    }    
-    else
-    {
-      /* Find the inorder predecessor of current */
-      pre = current->left;
-      while(pre->right != NULL && pre->right != current)
-        pre = pre->right;
- 
-      /* Make current as right child of its inorder predecessor */
-      if(pre->right == NULL)
-      {
-        pre->right = current;
-        current = current->left;
-      }
-             
-      /* Revert the changes made in if part to restore the original 
-        tree i.e., fix the right child of predecssor */   
-      else 
-      {
-        pre->right = NULL;
-        printf("%s ",current->word);
-        current = current->right;      
-      } /* End of if condition pre->right == NULL */
-    } /* End of if condition current->left == NULL*/
-  } /* End of while */
-}
  
 /* prototypes */ 
 int ngetch(FILE *); 
@@ -58,7 +18,7 @@ int getch(void);
 void ungetch(int);
 struct tnode *addtree(struct tnode *, char *);
 void treeprint(struct tnode *);
-void treeprint_inter(struct tnode *);
+void treeprint_iter(struct tnode *);
 int getword(char *, int, FILE *);
 
 /* defines */
@@ -84,7 +44,9 @@ int main(void)
 			root = addtree(root, word);
 	
 	treeprint(root);
-	MorrisTraversal(root);
+	printf("\n\n");
+	treeprint_iter(root);
+	printf("\n\n");
 	return 0;
 }
 
@@ -92,104 +54,71 @@ int main(void)
 struct tnode *addtree(struct tnode *p, char *w)
 {
 	int cond;
-	
-	
-
 	if (p == NULL) /* a new word has arrived */
-	{
-	//	printf("N %s\n", w);
-		
+	{ 
 		p = malloc(sizeof(struct tnode));
-		p->word = strdup(w); /* make a new node */
+		p->word = strdup(w);
 		p->count = 1;
-		p->left = p->right = NULL;
-	//	fflush(stdout);
-		//printf("  P %s\n", p->word);
+		p->left = p->right = NULL; 
 	} 
-	else if ((cond = strcmp(w, p->word)) == 0) /* repeated word */ 
+	else if ((cond = strcmp(w, p->word)) == 0)
 	{
 		p->count++; 
 	}
-	else if (cond < 0) /* less than into left subtree */
+	else if (cond < 0)
 	{ 
-		//printf("  P %s\n", p->word);
-		//printf("        L %s\n", w);
-		
-		//printf("        L %s\n", p->word);
-		//fflush(stdout);
 		p->left = addtree(p->left, w);
 	}
-	else /* greater than into right subtree */
+	else
 	{ 
-		//printf("  P %s\n", p->word);
-		//printf("                R %s\n", w);
-		//printf("                R %s\n", p->word);
-		//fflush(stdout);
 		p->right = addtree(p->right, w);
 	}
 	return p;
-
-} 
-
-void treeprint_inter(struct tnode *p)
-{
-	int depth = 10;
-	struct tnode *o = p;
-	if (p != NULL)
-	{
-		while (p && o)
-		{ 
-		        if (p->left)
-			{ 
-				p = p->left;
-				printf("%4d %s\n", p->count, p->word); 
-				o = p;
-			}
-			
-			
-		        if (o->right)
-			{
-				printf("%4d %s\n",o->count, o->word);
-				o = o->right;
-				p = o;
-			}
-			
-		}
-	//	treeprint(p->left); 
-		//printf("%.*s %4d %s\n", depth, "     ", p->count, p->word); 
-	//	treeprint(p->right);
-	}
 }
-/* treeprint: in-order print of tree p */
+
 void treeprint(struct tnode *p)
 {
-	int depth = 10;
+	/* recursive tree printing */
 	if (p != NULL)
 	{
 		treeprint(p->left); 
-		printf("%.*s %4d %s\n", depth, "     ", p->count, p->word); 
+		printf("\t%d %s\n", p->count, p->word); 
 		treeprint(p->right);
 	}
 }
-void treeprint2(struct tnode *p, char *s)
+
+void treeprint_iter(struct tnode *root)
 {
-	int depth = 10;
-	if (p != NULL)
-	{
-		treeprint2(p->left, "left");
-		if (!(strcmp(s, "start")))
-			depth = 5; 
-		printf("%.*s %4d %s   %s --> \n", depth, "                      ", p->count, p->word, s); 
-		
-		if (p->right && p->right->word)
-		printf("         %.*s %s       %s --> \n", depth, "                      ", p->right->word, s); 
-		if (p->left && p->left->word)
-		printf("         %.*s %s       %s --> \n", depth, "                      ", p->left->word, s); 
-		
-		if (!(strcmp(s, "start")))
-			depth = 10;
-		treeprint2(p->right, "right");
-	}
+	/* Morris traversal */
+	struct tnode *current,*pre; 
+	if(root == NULL)
+		return; 
+	current = root;
+	while(current != NULL)
+	{                 
+		if(current->left == NULL)
+		{
+			printf("\t%d %s\n", current->count, current->word);
+			current = current->right;      
+		}    
+		else
+		{ 
+			pre = current->left;
+			while(pre->right != NULL && pre->right != current)
+				pre = pre->right; 
+			if(pre->right == NULL)
+			{
+				pre->right = current;
+				current = current->left;
+			} 
+			else 
+			{
+				pre->right = NULL;
+				printf("\t%d %s\n", current->count, current->word);
+				current = current->right;      
+			}
+		} 
+	} 
 }
 
 int getword(char *word, int lim, FILE *fp)
