47f097e44178fed8be526556b43664c7de94f291
cmgraff <cm0graff@gmail.com>
2017-09-14 13:49:41 -0500

    simplify

--- csafety/src/words.c
+++ csafety/src/words.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> 
 
 int main(int argc, char *argv[])
 {
-	FILE *fp = fopen(argv[1], "r");
-	char buffer[1024], *p, *h;
-	
-	while ((fgets(buffer, 1024, fp)) != NULL)
-	{
-		h = buffer;
-		for(h = buffer;(p = strchr(h, ' ')) != NULL;h=p)
+	FILE *fp;
+	char b[1024], *p, *h;
+
+	if (argc != 2 || (fp = fopen(argv[1], "r")) == NULL)
+		{ fputs("needs file\n", stderr); return 1; };
+
+	for (;(fgets(b, 1024, fp)) != NULL;)
+	{ 
+		for(h=b;(p = strchr(h, ' ')) != NULL;h=p)
 		{
 			*p++ = '\0';
-			printf("%s\n", h); 
-			h = p; 
+			printf("%s\n", h);
 		}
 	}
 	return 0;
 }
+
+
