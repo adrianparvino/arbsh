05ae5ffa1b8a5e284edc829e924d4649b4a1ee5e
cmgraff <cm0graff@gmail.com>
2017-09-14 23:11:02 -0500

    namespace

--- csafety/src/unlimited_pipeline.c
+++ csafety/src/unlimited_pipeline.c
@@ -1,6 +1,6 @@
 /*
 Unix pipelines can be created between utilities without using a buffer of any
-kind. Using the typical object it becomes simple to store the information
+kind. Using the typical shell it becomes simple to store the information
 needed to seamlessly connect the stdin and stdout of a series of utilities.
 Pipe() is not set up for the final command in the sequence. Presented below is
 a small program that demonstrates the basics needed for writing a simple shell
@@ -28,9 +28,9 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 		char *outfp;
 		int outflags;
 		int boole;
-	} object; 
+	} shell; 
 
-	object *piped(object *o)
+	shell *piped(shell *o)
 	{
 		int fildes[2];
 		if ((pipe(fildes)) == -1)
@@ -40,7 +40,7 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 		return o;
 	}
 
-	object *child(object *o)
+	shell *child(shell *o)
 	{
 		dup2(o->in, STDIN_FILENO);
 		dup2(o->out, STDOUT_FILENO);
@@ -48,7 +48,8 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 		_exit(1);
 		return o;
 	}
-	object *foreground(object *o)
+
+	shell *foreground(shell *o)
 	{
 		waitpid(o->pids, &(o->err), 0);
 		if (o->out != -1)
@@ -58,7 +59,7 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 		return o;
 	}
 
-	object *execute(object *o)
+	shell *execute(shell *o)
 	{ 
 		if ( o->infp != NULL )			/* < */
 		        if((o->in = open(o->infp, O_RDONLY)) == -1)
@@ -78,7 +79,7 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 	int main(void)
 	{
 		size_t i = 0;
-		object p[10] = {{{ "ls", "-l", NULL}, -1, -1, 0, 0, 1,NULL, NULL, 0, -1},
+		shell p[10] = {{{ "ls", "-l", NULL}, -1, -1, 0, 0, 1,NULL, NULL, 0, -1},
 		{{ "wc", "-l", NULL}, -1, -1, 0, 0, 1 ,NULL, NULL, 0, -1},
 		{{ "wc", "-l", NULL}, -1, -1, 0, 0, 1 ,NULL, NULL, 0, -1},
 		{{ "wc", "-l", NULL}, -1, -1, 0, 0, 1 ,NULL, NULL, 0, -1},
@@ -89,7 +90,7 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 		{{ "wc", "-l", NULL}, -1, -1, 0, 0, 1 ,NULL, NULL, 0, -1},
 		{{ "wc", "-l", NULL}, -1, -1, 0, 0, 0 ,NULL, "outfile", O_APPEND|O_RDWR|O_CREAT, -1}};
 	
-		object *o = p;
+		shell *o = p;
 	
 		for (;i<10;o++,++i)
 		{
@@ -102,3 +103,4 @@ that supports arbitrary numbers of pipes. The sequence used is equivalent to
 	
 		return 0;
 	}
+
