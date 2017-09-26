71cd84f3d8c7a54a10413bf1fd9b9fbcd031cf16
cmgraff <cm0graff@gmail.com>
2017-09-26 18:00:14 -0500

    compiler warnings, better writing

--- csafety/src/c-objects.c
+++ csafety/src/c-objects.c
@@ -1,10 +1,4 @@
-/*
-The C standard identifies objects in a very broad sense and applies the term to
-"any object in memory". However this definition of object does not address the 
-power that C has to create and use objects in a modern sense of it being the 
-primary predecessor to "object oriented programing".
-
-Presented below is a method of "programming with objects" in C.
+/* 
 */
 
 	#include <stdlib.h>
@@ -20,12 +14,12 @@ Presented below is a method of "programming with objects" in C.
 	{
 		size_t lim = 10;
 	
-		/* create an array of objects */
+		/* create an "array" of objects */
 		object *p, *o;
 		if (!(o = p = malloc(sizeof(object) * lim)))
 			return 1;
 
-		/* allocate memory and populate array members */
+		/* allocate memory and populate "array" members */
 		for (o=p; (size_t)(o-p) < lim ; ++o)
 		{
 			if (!(o->rp = malloc(sizeof(char) * 100)))
@@ -33,7 +27,7 @@ Presented below is a method of "programming with objects" in C.
 			o->len = snprintf(o->rp, 100, "string %zu\n", o-p); 
 		} 
 
-		/* test the array of objects (fast write!) */
+		/* test the "array" of objects (fast write!) */
 		for (o=p; (size_t)(o-p) < lim ; ++o)
 			write(1, o->rp, o->len);
 
