1f2706d11366c426520a951a4a4c9b320a35e804
cmgraff <cm0graff@gmail.com>
2017-09-25 19:31:30 -0500

    use memmove instead of complex pointer manipulation

--- arbprec/tests/arbprec-driver.c
+++ arbprec/tests/arbprec-driver.c
@@ -57,12 +57,12 @@ int main(int argc, char **argv)
 	flt3 = arbprec_sqrt(flt1, flt3);
 	arbprec_print(flt3); 
 	
+	flt3 = arbprec_hypot(flt1, flt2, flt3);
+	arbprec_print(flt3);
 	
 	flt3 = arbprec_exp(flt1, flt3);
 	arbprec_print(flt3);
 
-	flt3 = arbprec_hypot(flt1, flt2, flt3);
-	arbprec_print(flt3);
 	
 
 	arbprec_log(flt1, flt3);
