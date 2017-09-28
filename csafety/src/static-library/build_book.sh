6a271188ae6dc5258c7fe2fbf5f13ab1d0f60bda
cmgraff <cm0graff@gmail.com>
2017-09-28 12:38:01 -0500

    update

--- csafety/src/static-library/build_book.sh
+++ csafety/src/static-library/build_book.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-for i in Makefile dog.c frog.c swan.c; do printf -- "%s:\n\n" "$i"; cat $i | sed 's|^|\t|g' ; done
+for i in dog.c frog.c swan.c animals.h main.c Makefile; do printf -- "%s:\n\n" "$i"; cat $i | sed 's|^|\t|g' ; done
 
 
 
