f8eecf308b45acd32d7387f16ab262c082c1393f
cmgraff <cm0graff@gmail.com>
2017-10-06 18:45:48 -0500

    floating point

---
+++ csafety/src/floating-point.c
@@ -0,0 +1,7 @@
+/*
+
+	"Floating-point arithmetic is a systematic approximation of real 
+	arithmetic, [...] Floating-point arithmetic can only represent a 
+	finite subset of the continuum of real numbers." (IEEE-754-2008 3.2)
+*/
+
