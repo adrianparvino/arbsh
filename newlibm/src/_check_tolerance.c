d8a762ddb4400ae6931a8a07b7b20cc0e4ed68fa
cmgraff <cm0graff@gmail.com>
2017-10-14 18:49:12 -0500

    full range for sin

--- newlibm/src/_check_tolerance.c
+++ newlibm/src/_check_tolerance.c
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 //#define TOLER .0000000000001
-#define TOLER .0000000000000001
+#define TOLER .000000000001
 int _check_tolerance(double x, double y)
 {
 	if ( x > y - TOLER && x < y + TOLER )
