c3ea2eef43089dcb746b8e6e9abd3133e2c7845d
cmgraff <cm0graff@gmail.com>
2017-10-14 00:51:09 -0500

    better tests, headers

--- newlibm/tests/math.c
+++ newlibm/tests/math.c
@@ -1,5 +1,4 @@
 #include <math.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,26 +11,19 @@ int main(int argc, char *argv[])
         else
                 x = 19;
 
-        printf("libc sin =     %19.19f\n", sin(x)); 
-	printf("\n\n"); 
+        printf("sin(%lf) =     %19.19lf\n", x, sin(x)); 
+	printf("floor(%lf) =     %19.19lf\n", x, floor(x)); 
 	/*
-        printf("libc cos =     %19.19f\n", cos(x)); 
-	printf("\n\n");
-        printf("libc sinh =    %19.19f\n", sinh(x)); 
-	printf("\n\n");
-        printf("libc cosh =    %19.19f\n", cosh(x)); 
-	printf("\n\n");
-	printf("libc log =     %19.19f\n", log(x)); 
-	printf("libc exp =     %19.19f\n", exp(x)); 
-	printf("\n\n"); 
-	printf("libc acos =    %19.19f\n", acos(x)); 
-	printf("\n\n");
-	printf("libc asin =    %19.19f\n", asin(x)); 
-	printf("\n\n");
-	//printf("libc tanh =    %19.19f\n", tanh(x));
-	//printf("libc tan =     %19.19f\n", tan(x)); 
-	
-	printf("libc atan =     %19.19f\n", atan(x)); 
+        printf("cos =     %19.19f\n", cos(x));
+        printf("sinh =    %19.19f\n", sinh(x));
+        printf("cosh =    %19.19f\n", cosh(x));
+	printf("log =     %19.19f\n", log(x));
+	printf("exp =     %19.19f\n", exp(x));
+	printf("acos =    %19.19f\n", acos(x));
+	printf("asin =    %19.19f\n", asin(x));
+	printf("tanh =    %19.19f\n", tanh(x));
+	printf("tan =     %19.19f\n", tan(x));
+	printf("atan =    %19.19f\n", atan(x));
 	*/
 
         return 0;
