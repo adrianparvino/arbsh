690c0d5049b63bd7ce2f39b4264a645abc2aa975
cmgraff <cm0graff@gmail.com>
2017-10-14 01:18:35 -0500

    test remez

--- newlibm/tests/math.c
+++ newlibm/tests/math.c
@@ -11,8 +11,12 @@ int main(int argc, char *argv[])
         else
                 x = 19;
 
+	double y[2];
+	y[0] = x;
+	y[1] = x;
         printf("sin(%lf) =     %19.19lf\n", x, sin(x)); 
 	printf("floor(%lf) =     %19.19lf\n", x, floor(x)); 
+	printf("floor(%lf) =     %d\n", x, ____rem_pio2(x, y));
 	/*
         printf("cos =     %19.19f\n", cos(x));
         printf("sinh =    %19.19f\n", sinh(x));
