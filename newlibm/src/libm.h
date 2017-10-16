d3febeeab30e49f85f24c23ef70e7478e2338963
cmgraff <cm0graff@gmail.com>
2017-10-15 23:01:58 -0500

    do-while loops are not needed

--- newlibm/src/libm.h
+++ newlibm/src/libm.h
@@ -29,7 +29,7 @@ union dshape {
 	uint64_t bits;
 };
 
-#define FORCE_EVAL(x) do {                          \
+#define FORCE_EVAL(x){                          \
 	if (sizeof(x) == sizeof(float)) {           \
 		volatile float ____x;                 \
 		____x = (x);                          \
@@ -40,92 +40,92 @@ union dshape {
 		volatile long double ____x;           \
 		____x = (x);                          \
 	}                                           \
-} while(0)
+}
 
 /* Get two 32 bit ints from a double.  */
 #define EXTRACT_WORDS(hi,lo,d)                                  \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.value = (d);                                              \
   (hi) = ____u.bits >> 32;                                        \
   (lo) = (uint32_t)____u.bits;                                    \
-} while (0)
+}
 
 /* Get a 64 bit int from a double.  */
 #define EXTRACT_WORD64(i,d)                                     \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.value = (d);                                              \
   (i) = ____u.bits;                                               \
-} while (0)
+}
 
 /* Get the more significant 32 bit int from a double.  */
 #define GET_HIGH_WORD(i,d)                                      \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.value = (d);                                              \
   (i) = ____u.bits >> 32;                                         \
-} while (0)
+}
 
 /* Get the less significant 32 bit int from a double.  */
 #define GET_LOW_WORD(i,d)                                       \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.value = (d);                                              \
   (i) = (uint32_t)____u.bits;                                     \
-} while (0)
+}
 
 /* Set a double from two 32 bit ints.  */
 #define INSERT_WORDS(d,hi,lo)                                   \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.bits = ((uint64_t)(hi) << 32) | (uint32_t)(lo);           \
   (d) = ____u.value;                                              \
-} while (0)
+}
 
 /* Set a double from a 64 bit int.  */
 #define INSERT_WORD64(d,i)                                      \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.bits = (i);                                               \
   (d) = ____u.value;                                              \
-} while (0)
+}
 
 /* Set the more significant 32 bits of a double from an int.  */
 #define SET_HIGH_WORD(d,hi)                                     \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.value = (d);                                              \
   ____u.bits &= 0xffffffff;                                       \
   ____u.bits |= (uint64_t)(hi) << 32;                             \
   (d) = ____u.value;                                              \
-} while (0)
+}
 
 /* Set the less significant 32 bits of a double from an int.  */
 #define SET_LOW_WORD(d,lo)                                      \
-do {                                                            \
+{                                                            \
   union dshape ____u;                                             \
   ____u.value = (d);                                              \
   ____u.bits &= 0xffffffff00000000ull;                            \
   ____u.bits |= (uint32_t)(lo);                                   \
   (d) = ____u.value;                                              \
-} while (0)
+}
 
 /* Get a 32 bit int from a float.  */
 #define GET_FLOAT_WORD(i,d)                                     \
-do {                                                            \
+{                                                            \
   union fshape ____u;                                             \
   ____u.value = (d);                                              \
   (i) = ____u.bits;                                               \
-} while (0)
+}
 
 /* Set a float from a 32 bit int.  */
 #define SET_FLOAT_WORD(d,i)                                     \
-do {                                                            \
+{                                                            \
   union fshape ____u;                                             \
   ____u.bits = (i);                                               \
   (d) = ____u.value;                                              \
-} while (0)
+}
 
 /* fdlibm kernel functions */
 
@@ -135,10 +135,10 @@ double ____sin(double,double,int);
 double ____cos(double,double);
 double ____tan(double,double,int);
 
-#define STRICT_ASSIGN(type, lval, rval) do {    \
+#define STRICT_ASSIGN(type, lval, rval){    \
         volatile type ____v = (rval);             \
         (lval) = ____v;                           \
-} while (0)
+}
 
 //#define STRICT_ASSIGN(type, lval, rval) ((lval) = (type)(rval))
 int _check_tolerance(double x, double y);
