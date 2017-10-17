993b7b77e141e2df5aeaeb46f592b9482720b7a8
cmgraff <cm0graff@gmail.com>
2017-10-17 01:01:25 -0500

    verbosify

--- newlibm/src/_check_tolerance.c
+++ newlibm/src/_check_tolerance.c
@@ -1,15 +1,15 @@
 #include <math.h>
 #include <stdio.h>
+#include <float.h>
 #define TOLER .0000000000000001
-//            .3139443789225921377
-//#define TOLER .000000000001
+
 int _check_tolerance(double x, double y)
 {
 	if ( x > y - TOLER && x < y + TOLER )
 	{
-		fprintf(stderr, "tolerance met, breaking\n");
+		fprintf(stderr, "tolerance met, breaking x=%19.19lf   y=%19.19lf\n", x, y);
 		return 1;
 	}
-	fprintf(stderr, "tolerance not yet met\n");
+	fprintf(stderr, "tolerance not yet met x=%19.19lf   y=%19.19lf\n", x, y);
 	return 0;
 }
--- newlibm/tests/math.c
+++ newlibm/tests/math.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
 double mysin(double);
 int main(int argc, char *argv[])
 {
@@ -11,9 +12,11 @@ int main(int argc, char *argv[])
         else
                 x = 19;
 
-        printf("sin(%lf) =     %19.19lf\n", x, sin(x));
+	printf("sin(123) solution is -0.4599034906895912512924357152932318108085806073810425809277\n");
+	printf("sin(987.234234) solution is 0.6991019671507811957162690088283454921684870494514649920197948454594776556950092560712278850735646365\n");
+        printf("sin(%lf) =     %19.25lf\n", x, sin(x));
 	printf("floor(%lf) =     %19.19lf\n", x, floor(x));
-	printf("mysin() =   %19.19lf  \n", mysin(x));
+	printf("mysin() =   %19.25lf  \n", mysin(x));
         printf("cos =     %19.19f\n", cos(x));
 	/*
         printf("sinh =    %19.19f\n", sinh(x));
@@ -26,7 +29,8 @@ int main(int argc, char *argv[])
 	printf("tan =     %19.19f\n", tan(x));
 	printf("atan =    %19.19f\n", atan(x));
 	*/
-
+	printf("DBL_DIG %d\n", DBL_DIG);
+	printf("DBL_EPSILON %19.19lf\n", DBL_EPSILON);
         return 0;
 }
 
