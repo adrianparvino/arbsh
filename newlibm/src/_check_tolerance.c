fe103df2ef5e39a212e7da880aa404b26ac4d8ec
cmgraff <cm0graff@gmail.com>
2017-10-18 05:43:37 -0500

    continued fraction with remez algorithm achieves +/- .5 ULP

--- newlibm/src/_check_tolerance.c
+++ newlibm/src/_check_tolerance.c
@@ -1,11 +1,16 @@
 #include <math.h>
 #include <stdio.h>
 #include <float.h>
-#define TOLER .0000000000000001
+//#define TOLER .0000000000000001
+#define TOLER .000000000000000001
 
 int _check_tolerance(double x, double y)
 {
-	if ( x > y - TOLER && x < y + TOLER )
+	if ( x == y )
+		return 1;
+	else
+		return 0;
+	if ( x >= y - TOLER && x <= y + TOLER )
 	{
 		fprintf(stderr, "tolerance met, breaking x=%19.19lf   y=%19.19lf\n", x, y);
 		return 1;
