9987c485cbeb376a93946c275d03a909a6664f0c
cmgraff <cm0graff@gmail.com>
2017-11-04 12:09:57 -0500

    test gmp division

--- arbprec/tests/gmp-arbprec-driver.c
+++ arbprec/tests/gmp-arbprec-driver.c
@@ -7,12 +7,18 @@ int main(int argc, char *argv[])
 	argc = argc;
 	mpf_t n;
 	mpf_t j;
+	mpf_t a;
 	mpf_init(n); 
 	mpf_init(j); 
-	mpf_set_str(n,argv[1], 10); 
+	mpf_init(a); 
+	mpf_set_str(n,argv[1], 50); 
+	mpf_set_str(a,argv[2], 50); 
 	gmp_printf("%.100Ff\n", n); 
 	mpf_sqrt(j, n);
 	gmp_printf("gmp sqrt %.100Ff\n", j); 
+	
+	mpf_div(j, a, n);
+	gmp_printf("gmp mpf_div %.100Ff\n", j); 
 
 	printf ("\n");
 }
