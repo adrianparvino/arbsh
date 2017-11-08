6b797cde48236ac75602f8fa295dc9837344a827
cmgraff <cm0graff@gmail.com>
2017-11-08 07:35:44 -0600

    update everthing

--- csafety/src/opaque/shared.h
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-
-typedef struct object object; 
-size_t object_size(void); 
-object *object_init(object *);
-object *object_populate(object *, char *);
-void object_write(object *);
-void object_free(object *);
-
