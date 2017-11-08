14ba605a9629f820839f4d1d1675695b41cf1a07
cmgraff <cm0graff@gmail.com>
2017-11-07 21:30:22 -0600

    newlibm does not use floats

--- newlibm/src/libm.h
+++ newlibm/src/libm.h
@@ -15,15 +15,10 @@
 
 #include <stdint.h>
 #include <stdio.h>
-//#include <float.h> /* this should probably be removed -- CM Graff */
+
 
 double scalbn(double, int);
 double floor(double);
-
-union fshape {
-	float value;
-	uint32_t bits;
-};
 
 union dshape {
 	double value;
@@ -39,21 +34,12 @@ union dshape {
 	(lo) = (uint32_t)____u.bits;				\
 }
 
-/* Get a 64 bit int from a double.*/
-#define EXTRACT_WORD64(i,d)					\
-{								\
-	union dshape ____u;					\
-	____u.value = (d);					\
-	(i) = ____u.bits;					\
-}
-
 /* Get the more significant 32 bit int from a double.*/
 #define GET_HIGH_WORD(i,d)					\
 {								\
 	union dshape ____u;					\
 	____u.value = (d);					\
 	(i) = ____u.bits >> 32;					\
-	fprintf(stderr, "GET_HIGH_WORD %d\n", i); \
 }
 
 /* Get the less significant 32 bit int from a double.*/
@@ -62,7 +48,6 @@ union dshape {
 	union dshape ____u;					\
 	____u.value = (d);					\
 	(i) = (uint32_t)____u.bits;				\
-	fprintf(stderr, "GET_LOW_WORD %d\n", i); \
 }
 
 /* Set a double from two 32 bit ints.*/
@@ -72,14 +57,6 @@ union dshape {
 	____u.bits = ((uint64_t)(hi) << 32) | (uint32_t)(lo);	\
 	(d) = ____u.value;					\
 	fprintf(stderr, "INSERT_WORDS %lf\n", d); \
-}
-
-/* Set a double from a 64 bit int.*/
-#define INSERT_WORD64(d,i)					\
-{								\
-	union dshape ____u;					\
-	____u.bits = (i);					\
-	(d) = ____u.value;					\
 }
 
 /* Set the more significant 32 bits of a double from an int.*/
@@ -104,22 +81,6 @@ union dshape {
 	fprintf(stderr, "SET_LOW_WORD %lf\n", d); \
 }
 
-/* Get a 32 bit int from a float. */
-#define GET_FLOAT_WORD(i,d)					\
-{								\
-	union fshape ____u;					\
-	____u.value = (d);					\
-	(i) = ____u.bits;					\
-}
-
-/* Set a float from a 32 bit int. */
-#define SET_FLOAT_WORD(d,i)					\
-{								\
-	union fshape ____u;					\
-	____u.bits = (i);					\
-	(d) = ____u.value;					\
-}
-
 /* fdlibm kernel functions */
 int ____rem_pio2_large(double *, double *, int, int, int);
 int ____rem_pio2(double, double *);
@@ -132,8 +93,6 @@ double ____tan(double, double, int);
 	(lval) = ____v;						\
 	fprintf(stderr, "STRICT_ASSIGN  lval %19.19lf   rval %19.19lf\n", lval, rval); \
 }
-
 //#define STRICT_ASSIGN(type, lval, rval) ((lval) = (type)(rval))
-int _check_tolerance(double x, double y);
 
 #endif
