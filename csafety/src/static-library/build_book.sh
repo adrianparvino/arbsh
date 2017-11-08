7c897dee211f242a6f9ae2b8f0d79997bf98d57d
cmgraff <cm0graff@gmail.com>
2017-11-08 07:35:44 -0600

    update everthing

--- csafety/src/static-library/build_book.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-for i in dog.c frog.c swan.c animals.h main.c Makefile; do printf -- "%s:\n\n" "$i"; cat $i | sed 's|^|\t|g' ; done
-
-
-
--- csafety/src/static-library/dog.c
+++
@@ -1,6 +0,0 @@
-int dogs = 10;
-
-int inc_dogs(int d)
-{
-	return d + 1;
-}
