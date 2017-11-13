38392634c79bfe246a1810f70a5ebd79f147ac2f
cmgraff <cm0graff@gmail.com>
2017-11-13 14:45:50 -0600

    newtonian division is starting to work

--- arbprec/src/newtonian-division.c
+++ arbprec/src/newtonian-division.c
@@ -21,33 +21,53 @@ double newton_iteration(double x, double eps)
         printf("iterations = %zu\n", i);
         return guess;
 }
+void arb_copy(fxdpnt *dest, fxdpnt *src)
+{ 
+	dest = arb_expand(dest, MIN(src->len, dest->len));
+	memcpy(dest->number, src->number, src->len);
+	dest->sign = src->sign;
+	dest->lp = src->lp;
+	dest->rp = src->rp;
+	dest->len = src->len;
+}
 
 fxdpnt *arb_newtonian_div(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, int scale)
 {
 	(void)scale; // scale will never be needed
-	fxdpnt *guess = arb_expand(NULL, a->len + b->len);
-	fxdpnt *newguess = arb_expand(NULL, a->len + b->len);
-	fxdpnt *hold = arb_expand(NULL, a->len + b->len);
-	fxdpnt *hold2 = arb_expand(NULL, a->len + b->len);
+	fxdpnt *guess = arb_expand(NULL, a->len);
+	//fxdpnt *newguess = arb_expand(NULL, a->len + b->len);
+	fxdpnt *newguess = arb_expand(NULL, 1);
+	//fxdpnt *hold = arb_expand(NULL, a->len + b->len);
+	fxdpnt *hold = arb_expand(NULL, 1);
+	//fxdpnt *hold2 = arb_expand(NULL, a->len + b->len);
+	fxdpnt *hold2 = arb_expand(NULL, 1);
 	fxdpnt *two = arb_str2fxdpnt("2");;
 	fxdpnt *reciprocal;
 
 	guess->lp = 0;
-	guess->rp = a->len + b->len;
-	memset(guess->number, 0, a->len + b->len);
-	guess->number[a->len + b->len -1] = 5;
+	guess->rp = a->len;
+	memset(guess->number, 0, a->len);
+	guess->number[a->len -1] = 5;
+	guess->len = a->len;
 
 	arb_printold(guess);
-	
+	arb_printold(two);
 	size_t i = 0;
-	while (i < 100)
+	while (i < 10)
 	{
-		hold = arb_mul(a, guess, hold, base); // x * guess
-		hold2 = arb_sub(hold, two, hold2, base); // 2 - x * guess
-		newguess = arb_mul(guess, hold2, newguess, base);
+		//  new_guess = guess * (2 - x * guess);
+		hold = arb_mul(b, guess, hold, base); // x * guess
+		//arb_printold(a);
+		//arb_printold(hold);
+		hold2 = arb_sub(two, hold, hold2, base); // 2 - x * guess
+		//arb_printold(hold2);
+		newguess = arb_mul(guess, hold2, newguess, base); // guess * (2-x*guess)
+		arb_copy(guess, newguess);
+		arb_printold(newguess);
+		//guess = newguess;
 		++i;
 	}
 	reciprocal = guess;
-	c = arb_mul(reciprocal, b, c, base);
+	c = arb_mul(reciprocal, a, c, base);
 	return c;
 }
