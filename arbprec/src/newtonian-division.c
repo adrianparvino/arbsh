0d1ce906f6b2a157a507b4be6559d1530cfe1f6e
cmgraff <cm0graff@gmail.com>
2017-11-13 13:18:55 -0600

    more effort toward newtonian iteration

--- arbprec/src/newtonian-division.c
+++ arbprec/src/newtonian-division.c
@@ -25,18 +25,27 @@ double newton_iteration(double x, double eps)
 fxdpnt *arb_newtonian_div(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, int scale)
 {
 	(void)scale; // scale will never be needed
-	fxdpnt *guess = arb_expand(NULL, 1);
-	fxdpnt *newguess = arb_expand(NULL, 1);
-	fxdpnt *hold = arb_expand(NULL, 1);
-	fxdpnt *hold2 = arb_expand(NULL, 1);
+	fxdpnt *guess = arb_expand(NULL, a->len + b->len);
+	fxdpnt *newguess = arb_expand(NULL, a->len + b->len);
+	fxdpnt *hold = arb_expand(NULL, a->len + b->len);
+	fxdpnt *hold2 = arb_expand(NULL, a->len + b->len);
 	fxdpnt *two = arb_str2fxdpnt("2");;
 	fxdpnt *reciprocal;
+
+	guess->lp = 0;
+	guess->rp = a->len + b->len;
+	memset(guess->number, 0, a->len + b->len);
+	guess->number[a->len + b->len -1] = 5;
+
+	arb_printold(guess);
+	
 	size_t i = 0;
 	while (i < 100)
 	{
 		hold = arb_mul(a, guess, hold, base); // x * guess
 		hold2 = arb_sub(hold, two, hold2, base); // 2 - x * guess
 		newguess = arb_mul(guess, hold2, newguess, base);
+		++i;
 	}
 	reciprocal = guess;
 	c = arb_mul(reciprocal, b, c, base);
