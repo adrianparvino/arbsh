2ae84e3c2fd5f24b691882c72aa59629bad80136
cmgraff <cm0graff@gmail.com>
2017-11-14 10:54:34 -0600

    use size_t for iterators. use arrays not pointers for clarity

--- arbprec/src/divide2.c
+++ arbprec/src/divide2.c
@@ -8,12 +8,17 @@ fxdpnt *arb_divide2(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, int scale)
 	unsigned char *qptr;
 	int scale1;
 	int val;
-	unsigned int lea, leb, qdigits, offset;
-	unsigned int qdig, qguess, borrow, carry;
+	//unsigned int lea, leb, qdigits, offset;
+	unsigned int qdigits, offset;
+	size_t lea = 0;
+	size_t leb = 0;
+	//unsigned int qdig, qguess, borrow, carry;
+	unsigned int qguess, borrow, carry;
 	unsigned char *mval;
 	int out_of_scale;
 	unsigned int normalize;
 	size_t i, j;
+	size_t qdig = 0;
 	/* these variables are simply to help deduce the big O properties of the equation */
 	size_t iterations = 0;
 	size_t subs = 0;
@@ -25,36 +30,32 @@ fxdpnt *arb_divide2(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, int scale)
 
 	lea = a->lp + b->rp;
 	scale1 = a->rp - b->rp;
-	
+
 	if (scale1 < scale)
 		offset = scale - scale1;
 	else
 		offset = 0;
-	num1 = arb_malloc(a->lp+a->rp+offset+2);
+
+	num1 = arb_malloc(a->lp+a->rp+offset+2); // fix this +2
 	memset (num1, 0, a->lp+a->rp+offset+2);
 	memcpy (num1+1, a->number, a->lp+a->rp);
 
 	leb = b->lp + b->rp;
 	num2 = arb_malloc (leb+1);
 	memcpy (num2, b->number, leb);
-	*(num2+leb) = 0;
+	num2[leb] = 0;
 
 	unsigned char *freesave = num2;
 	for (;*num2 == 0;num2++,leb--);
 
-	if (leb > lea+scale)
-	{
-		qdigits = scale+1;
-		out_of_scale = 1;
-	}
+	qdigits = scale+1;
+	out_of_scale = 0;
+	if (leb > lea+scale) 
+		out_of_scale = 1; 
 	else
-	{
-		out_of_scale = 0;
-		if (leb>lea)
-			qdigits = scale+1;
-		else
+		if (!(leb>lea))
 			qdigits = lea-leb+scale+1;
-	}
+
 	qval = arb_new_num (qdigits-scale,scale);
 	memset (qval->number, 0, qdigits);
 	mval = arb_malloc (leb+1);
