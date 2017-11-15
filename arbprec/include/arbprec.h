4f52ca688aea4f894d197353da0ed7078f319d5a
cmgraff <cm0graff@gmail.com>
2017-11-14 19:26:28 -0600

    use the ARBT type

--- arbprec/include/arbprec.h
+++ arbprec/include/arbprec.h
@@ -73,6 +73,6 @@ fxdpnt *arb_divide2(fxdpnt *, fxdpnt *, fxdpnt *, int, int);
 fxdpnt *arb_newtonian_div(fxdpnt *, fxdpnt *, fxdpnt *, int, int);
 void arb_printold(fxdpnt *flt);
 void short_multiply(unsigned char *, int, int, unsigned char *, int);
-void short_mul2(char *, char *, size_t, int, int);
-void arb_short_mul(char *, size_t, int, int);
+void short_mul2(ARBT *, ARBT *, size_t, int, int);
+void arb_short_mul(ARBT *, size_t, int, int);
 
