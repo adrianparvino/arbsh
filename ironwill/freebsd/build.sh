8941b4acee4d34650e3ad3ab4051ea1e515d15aa
cmgraff <cm0graff@gmail.com>
2017-11-18 17:38:13 -0600

    remove unused

--- ironwill/freebsd/build.sh
+++
@@ -1,4 +0,0 @@
-#https://download.freebsd.org/ftp/releases/i386/i386/ISO-IMAGES/11.1/FreeBSD-11.1-RELEASE-i386-memstick.img
-#wget https://download.freebsd.org/ftp/releases/VM-IMAGES/11.1-RELEASE/i386/Latest/FreeBSD-11.1-RELEASE-i386.raw.xz
-wget https://download.freebsd.org/ftp/development/tarballs/src_stable_11.tar.gz
-
