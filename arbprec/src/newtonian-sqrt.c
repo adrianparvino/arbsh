f7156bdeec15a2a8e491afe058741c2dd4126e97
cmgraff <cm0graff@gmail.com>
2017-11-24 16:08:32 -0600

    comments about initial guess

--- arbprec/src/newtonian-sqrt.c
+++ arbprec/src/newtonian-sqrt.c
@@ -5,15 +5,18 @@ fxdpnt *arb_newton_sqrt(fxdpnt *a, fxdpnt *c, int base, int scale)
 	fxdpnt *new_guess = NULL;
 	new_guess = arb_expand(new_guess, a->len); 
 	fxdpnt *guess = NULL; 
-	guess = arb_expand(guess, a->len /50);
-	guess->number[0] = 5; 
-	printf("%zu size\n", guess->len);
+	/* 1/2( 123 + 123 / 123) = 62.0  ==  (123+123 / 123)/2 */
+	guess = arb_expand(guess, a->len);
+	guess->number[0] = 5;
 	fxdpnt *ans = NULL;
 	ans = arb_expand(ans, a->len); 
 	fxdpnt *hold = NULL;
 	hold = arb_expand(hold, a->len); 
 	fxdpnt *two = NULL;
 	two = arb_str2fxdpnt("2.0");
+	
+	fxdpnt *p5 = NULL;
+	p5 = arb_str2fxdpnt("0.5");
 	size_t i = 0;
 	while (i++ < 10000)
 	{
@@ -21,6 +24,7 @@ fxdpnt *arb_newton_sqrt(fxdpnt *a, fxdpnt *c, int base, int scale)
 		ans = arb_alg_d(a, guess, ans, base, scale); 
 		hold = arb_add(ans, new_guess, hold, base);
 		guess = arb_alg_d(hold, two, guess, base, scale);
+		//guess = arb_mul(hold, p5, guess, base); // not working
 		if (arb_compare(guess, new_guess, base) == 0)
 			break; 
 	}
