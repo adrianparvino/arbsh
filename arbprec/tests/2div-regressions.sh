e1f7fa4b919c338c8d496a7027e24619734bf8b4
cmgraff <cm0graff@gmail.com>
2017-11-25 21:24:21 -0600

    add regression testing for 2div

---
+++ arbprec/tests/2div-regressions.sh
@@ -0,0 +1,22 @@
+
+
+#extra="valgrind"
+#extra="time"
+extra=""
+
+rm log log2
+
+$extra ./control/tests/division-test $1 $2 $3 $4 >log
+
+$extra ./tests/2division $1 $2 $3 $4> log2
+
+
+
+if diff log log2 
+then 	if [ -s log -a -s log2 ]
+	then	printf "%s\n" "control files are equal and non empty."
+	else	printf "%s\n" "control files ere equal, but empty. something went wrong!"
+	fi
+else	printf "%s\n" "The diff failed!"
+fi
+
