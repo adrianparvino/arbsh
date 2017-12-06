fee6082dae6d6cd2b5e1bec3eea69b699196fe02
cmgraff <cm0graff@gmail.com>
2017-12-06 09:33:21 -0600

    clean up and consolidate io functions

--- arbprec/src/arb_print.c
+++ arbprec/src/arb_print.c
@@ -1,5 +1,18 @@
 #include <arbprec/arbprec.h>
 
+int arb_highbase(int a)
+{
+	// Handle high bases
+	static int glph[36] = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+				'9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+				'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+				'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+	if (a < 36) 
+		return glph[a]; 
+	else // just use the ascii values for bases that are very high
+		return a;
+}
+
 void _print_core(FILE *fp, ARBT *number, size_t len, size_t radix)
 {
 	size_t i = 0;
@@ -18,4 +31,3 @@ void arb_print(fxdpnt *flt)
 		putchar(flt->sign);
 	_print_core(stdout, flt->number, flt->len, flt->lp);
 }
-
