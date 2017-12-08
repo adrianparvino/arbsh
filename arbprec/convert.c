9298f105a25520e769b4c094cee86cc681fbac31
cmgraff <cm0graff@gmail.com>
2017-12-08 00:48:07 -0600

    support all bases

--- arbprec/convert.c
+++ arbprec/convert.c
@@ -2,11 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-static int * inithexarray(char *pDecStr, int *pnElements);
-static void adddecvalue(int *pMyArray, int nElements, int value);
-static void printarray(int *pHexArray, int nElements);
 
-static void adddecvalue(int *pHexArray, int nElements, int value)
+
+
+
+static void adddecvalue(int *pHexArray, int nElements, int value, int ibase, int obase)
 {
 	int carry = value;
 	int tmp = 0;
@@ -14,13 +14,17 @@ static void adddecvalue(int *pHexArray, int nElements, int value)
 	
 	for (i = nElements-1; i >= 0; i--)
 	{
-		tmp = (pHexArray[i] * 10) + carry;
-		pHexArray[i] = tmp % 16;
-		carry = tmp / 16;
+		//tmp = (pHexArray[i] * 10) + carry;
+		//pHexArray[i] = tmp % 16;
+		//carry = tmp / 16;
+		
+		tmp = (pHexArray[i] * ibase) + carry;
+		pHexArray[i] = tmp % obase;
+		carry = tmp / obase;
 	}
 }
 
-static int *inithexarray(char *pDecStr, int *pnElements)
+static int *inithexarray(char *pDecStr, int *pnElements, int ibase, int obase)
 {
 	int *pArray = NULL;
 	int lenDecStr = strlen(pDecStr);
@@ -28,7 +32,7 @@ static int *inithexarray(char *pDecStr, int *pnElements)
 	pArray = calloc(lenDecStr, sizeof (int));
 
 	for (; i < lenDecStr; i++)
-		adddecvalue(pArray, lenDecStr, pDecStr[i] - '0');
+		adddecvalue(pArray, lenDecStr, pDecStr[i] - '0', ibase, obase);
 
 	*pnElements = lenDecStr;
 
@@ -50,14 +54,18 @@ int main(int argc, char * argv[])
 {
 	int *pMyArray = NULL;
 	int nElements;
+	int ibase = 0;
+	int obase = 0;
 
 	if (argc < 2)
 	{
 		printf ("Usage: %s decimalString\n", argv[0]);
+		
 		return 1;
 	}
-
-	pMyArray = inithexarray(argv[1], &nElements);
+	ibase = strtol(argv[2], 0, 0);
+	obase = strtol(argv[3], 0, 0);
+	pMyArray = inithexarray(argv[1], &nElements, ibase, obase);
 	printarray(pMyArray, nElements);
 	free (pMyArray);
 	return 0;
