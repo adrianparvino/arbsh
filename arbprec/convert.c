8aee878df36991c47f169dd110ce37f18f2ee06e
cmgraff <cm0graff@gmail.com>
2017-12-08 00:55:38 -0600

    clean up

--- arbprec/convert.c
+++ arbprec/convert.c
@@ -2,11 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
-
-
-static void adddecvalue(int *pHexArray, int nElements, int value, int ibase, int obase)
+void adddecvalue(int *pHexArray, int nElements, int value, int ibase, int obase)
 {
 	int carry = value;
 	int tmp = 0;
@@ -14,34 +10,26 @@ static void adddecvalue(int *pHexArray, int nElements, int value, int ibase, int
 	
 	for (i = nElements-1; i >= 0; i--)
 	{
-		//tmp = (pHexArray[i] * 10) + carry;
-		//pHexArray[i] = tmp % 16;
-		//carry = tmp / 16;
-		
 		tmp = (pHexArray[i] * ibase) + carry;
 		pHexArray[i] = tmp % obase;
 		carry = tmp / obase;
 	}
 }
 
-static int *inithexarray(char *pDecStr, int *pnElements, int ibase, int obase)
+int *inithexarray(char *pDecStr, int *pnElements, int ibase, int obase)
 {
 	int *pArray = NULL;
 	int lenDecStr = strlen(pDecStr);
 	int i = 0;
 	pArray = calloc(lenDecStr, sizeof (int));
-
 	for (; i < lenDecStr; i++)
 		adddecvalue(pArray, lenDecStr, pDecStr[i] - '0', ibase, obase);
-
 	*pnElements = lenDecStr;
-
 	return pArray;
 }
 
-static void printarray(int *pHexArray, int nElements)
+void printarray(int *pHexArray, int nElements)
 {
-	
 	char HexChar [16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', 
 			'9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	int i = 0;
@@ -52,21 +40,19 @@ static void printarray(int *pHexArray, int nElements)
 
 int main(int argc, char * argv[])
 {
-	int *pMyArray = NULL;
-	int nElements;
+	int *c = NULL;
+	int len;
 	int ibase = 0;
 	int obase = 0;
-
-	if (argc < 2)
+	if (argc < f)
 	{
-		printf ("Usage: %s decimalString\n", argv[0]);
-		
+		printf ("Usage: %s string ibase obase\n", argv[0]);
 		return 1;
 	}
 	ibase = strtol(argv[2], 0, 0);
 	obase = strtol(argv[3], 0, 0);
-	pMyArray = inithexarray(argv[1], &nElements, ibase, obase);
-	printarray(pMyArray, nElements);
-	free (pMyArray);
+	c = inithexarray(argv[1], &len, ibase, obase);
+	printarray(c, len);
+	free (c);
 	return 0;
 }
