a6da15451671316fb80463e8d3d10f06c9c499f2
cmgraff <cm0graff@gmail.com>
2017-12-08 01:10:45 -0600

    use size_t

--- arbprec/convert.c
+++ arbprec/convert.c
@@ -2,25 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
-void adddecvalue(int *pHexArray, int nElements, int value, int ibase, int obase)
+void adddecvalue(int *array, size_t len, int value, int ibase, int obase)
 {
 	int carry = value;
 	int tmp = 0;
-	int i; 
+	size_t i = 0;
 	
-	for (i = nElements-1; i >= 0; i--)
+	for (i = len; i > 0; i--)
 	{
-		tmp = (pHexArray[i] * ibase) + carry;
-		pHexArray[i] = tmp % obase;
+		tmp = (array[i-1] * ibase) + carry;
+		array[i-1] = tmp % obase;
 		carry = tmp / obase;
 	}
 }
 
-int *inithexarray(char *pDecStr, int *pnElements, int ibase, int obase)
+int *inithexarray(char *pDecStr, size_t *pnElements, int ibase, int obase)
 {
 	int *pArray = NULL;
-	int lenDecStr = strlen(pDecStr);
-	int i = 0;
+	size_t lenDecStr = strlen(pDecStr);
+	size_t i = 0;
 	pArray = calloc(lenDecStr, sizeof (int));
 	for (; i < lenDecStr; i++)
 		adddecvalue(pArray, lenDecStr, pDecStr[i] - '0', ibase, obase);
@@ -28,11 +28,11 @@ int *inithexarray(char *pDecStr, int *pnElements, int ibase, int obase)
 	return pArray;
 }
 
-void printarray(int *pHexArray, int nElements)
+void printarray(int *pHexArray, size_t nElements)
 {
 	char HexChar [16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', 
 			'9', 'A', 'B', 'C', 'D', 'E', 'F' };
-	int i = 0;
+	size_t i = 0;
 	for (; i < nElements; i++)
 		printf ("%c", HexChar[pHexArray[i]]);
 	printf ("\n");
@@ -41,7 +41,7 @@ void printarray(int *pHexArray, int nElements)
 int main(int argc, char * argv[])
 {
 	int *c = NULL;
-	int len;
+	size_t len;
 	int ibase = 0;
 	int obase = 0;
 	if (argc < 4)
