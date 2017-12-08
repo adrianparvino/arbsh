f77c604fbb49c811d963c2e6b86384e2678843dc
cmgraff <cm0graff@gmail.com>
2017-12-07 18:46:31 -0600

    cleanup

--- arbprec/src/compare.c
+++ arbprec/src/compare.c
@@ -1,29 +1,27 @@
 #include <arbprec/arbprec.h>
+
 int arb_compare(fxdpnt *a, fxdpnt *b, int base)
 {
-        size_t i = 0;
-        int sign = a->sign;
-
-        if (a->sign == '-' && b->sign == '+')
-                return -1; // smaller 
-        if (a->sign == '+' && b->sign == '-')
-                return 1;  // larger
-        // else signs are the same, so move to subtraction steps
-        fxdpnt *c = NULL;
+	size_t i = 0;
+	int sign = a->sign;
+	if (a->sign == '-' && b->sign == '+')
+		return -1;
+	if (a->sign == '+' && b->sign == '-')
+		return 1;
+	fxdpnt *c = NULL;
 	c = arb_expand(NULL, a->len + b->len); //FIXME initialize at runtime
-        c = arb_sub(a, b, c, base);
-        if (c->sign != sign){
-                arb_free(c);
-                return -1; // sign flipped so a < b  _smaller_
-        }
-        // check if c was all zeros indicating that a == b
-        for (;i< c->len;++i){
-                if (c->number[i] != 0){
-                        arb_free(c);
-                        return 1; // numbers were not equal so a > b (larger) 
-                }
-        }
-        arb_free(c);
-        return 0; // number were _equal_
+	c = arb_sub(a, b, c, base);
+	if (c->sign != sign){
+		arb_free(c);
+		return -1;
+	}
+	for (;i< c->len;++i){
+		if (c->number[i] != 0){
+			arb_free(c);
+			return 1;
+		}
+	}
+	arb_free(c);
+	return 0;
 }
 
