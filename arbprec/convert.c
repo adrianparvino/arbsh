e1b3eea02f492e9e8e6a2f00cae37a59d7564bcb
cmgraff <cm0graff@gmail.com>
2017-12-08 00:40:36 -0600

    better variable names

--- arbprec/convert.c
+++ arbprec/convert.c
@@ -8,15 +8,15 @@ static void printarray(int *pHexArray, int nElements);
 
 static void adddecvalue(int *pHexArray, int nElements, int value)
 {
-	int carryover = value;
+	int carry = value;
 	int tmp = 0;
 	int i; 
 	
 	for (i = nElements-1; i >= 0; i--)
 	{
-		tmp = (pHexArray[i] * 10) + carryover;
+		tmp = (pHexArray[i] * 10) + carry;
 		pHexArray[i] = tmp % 16;
-		carryover = tmp / 16;
+		carry = tmp / 16;
 	}
 }
 
@@ -25,7 +25,7 @@ static int *inithexarray(char *pDecStr, int *pnElements)
 	int *pArray = NULL;
 	int lenDecStr = strlen(pDecStr);
 	int i = 0;
-	pArray = calloc(lenDecStr,  sizeof (int));
+	pArray = calloc(lenDecStr, sizeof (int));
 
 	for (; i < lenDecStr; i++)
 		adddecvalue(pArray, lenDecStr, pDecStr[i] - '0');
@@ -57,12 +57,8 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	pMyArray = inithexarray (argv[1], &nElements);
-
-	printarray (pMyArray, nElements);
-
-	if (pMyArray != NULL)
-		free (pMyArray);
-
+	pMyArray = inithexarray(argv[1], &nElements);
+	printarray(pMyArray, nElements);
+	free (pMyArray);
 	return 0;
 }
