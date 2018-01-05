3cbe390cc63532353aee7b645f965470cc6eebf3
cmgraff <cm0graff@gmail.com>
2018-01-05 17:59:24 -0600

    support for converting to smaller bases

--- arbprec/src/convert.c
+++ arbprec/src/convert.c
@@ -7,17 +7,36 @@ fxdpnt *convert(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 	ARBT *p;
         size_t i = 0;
         size_t j = 0;
+	int notdone = 1;
+	size_t k = 0;
         p = arb_calloc(a->len, sizeof(ARBT));
-	ARBT *array = a->number;
-        for (; i < a->len; i++)
+	ARBT *array = arb_calloc(a->len, sizeof(ARBT));
+	memcpy(array, a->number, a->len * sizeof(ARBT));
+        for (; i < a->len  + k|| notdone; i++)
         { 
+		notdone = 1;
                 int carry = array[i];
                 int prod = 0;
-                for (j = a->len; j > 0; j--) {
+                for (j = a->len + k; j > 0; j--) {
                         prod = (p[j-1] * ibase) + carry;
                         p[j-1] = prod % obase;
                         carry = prod / obase;
                 }
+		
+		if (carry)
+		{
+			++k;
+			array = realloc(array, (a->len + k) * sizeof(ARBT));
+			p = realloc(p, (a->len + k) * sizeof(ARBT));
+			memset(p, 0, (a->len + k) * sizeof(ARBT));
+			memmove(array + 1, array, (a->len + k - 1) * sizeof(ARBT));//FIXME: replace with logical shift
+			array[0] = 0;
+			b->len++;
+			b->lp++;
+			i = 0;
+			
+		}else
+			notdone = 0;
         }
 	b->number = p;
 	
