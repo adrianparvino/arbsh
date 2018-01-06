093b2c3b82910a5357403944ded9ff515be09df0
cmgraff <cm0graff@gmail.com>
2018-01-06 00:49:47 -0600

    log tests

--- arbprec/notes/logtest.c
+++ arbprec/notes/logtest.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
+double logbase(double x, double base)
+{
+	// (ln x / ln b) is log for base b of x
+	double ret = log10(x) / log10(base);
+}
 int main(int argc, char *argv[])
 {
-	size_t len = floor(log2(strtod(argv[1], 0)));
-	printf("len = %zu\n", len);
+	// 2 args, number and base
+	printf("answer = %f\b", ceil(logbase(strtod(argv[1], 0), strtod(argv[2], 0))));
 	
-	len = floor(log10(strtod(argv[1], 0)));
-	printf("len = %zu\n", len);
-	
-	double nlen = log10(strtod(argv[1], 0));
-	printf("nlen = %f\n", nlen);
-
-	double con = log(10)/log(2);
-	printf("con = %f\n", con);
 	return 0;
 }
