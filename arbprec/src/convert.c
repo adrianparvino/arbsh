f03a1e1d0e652d150003abd1125b2929b6092339
cmgraff <cm0graff@gmail.com>
2018-01-09 02:01:31 -0600

    continue individuating the variable names for the two conversion functions

--- arbprec/src/convert.c
+++ arbprec/src/convert.c
@@ -5,7 +5,7 @@
 fxdpnt *convert(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 {
 	arb_copy(b, a);
-	ARBT *p;
+	ARBT *sv;
 	long long carry = 0;
 	long long prod = 0;
 	size_t i = 0;
@@ -17,23 +17,23 @@ fxdpnt *convert(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 		k = (size_t) ((2*a->len) / log10(obase)) ;
 	else 
 		k = a->len;
-	p = arb_calloc(k, sizeof(ARBT));
-	ARBT *array = arb_calloc(k, sizeof(ARBT));
-	memcpy(array + (k - a->len), a->number, a->len * sizeof(ARBT));
-	memset(array, 0, (k - a->len) * sizeof(ARBT));
+	sv = arb_calloc(k, sizeof(ARBT));
+	ARBT *real = arb_calloc(k, sizeof(ARBT));
+	memcpy(real + (k - a->len), a->number, a->len * sizeof(ARBT));
+	memset(real, 0, (k - a->len) * sizeof(ARBT));
 
 	for (i = 0; i < k; ++i) { 
-		carry = array[i];
+		carry = real[i];
 		prod = 0;
 		for (j = k; j > 0; j--) {
-			prod = (p[j-1] * ibase) + carry;
-			p[j-1] = prod % obase;
+			prod = (sv[j-1] * ibase) + carry;
+			sv[j-1] = prod % obase;
 			carry = prod / obase;
 		}
 	}
 	if (carry)
 		printf("we have a left over carry\n");
-	b->number = p;
+	b->number = sv;
 	b->len = k;
 	b->lp = k;
 	return b;
@@ -56,14 +56,14 @@ fxdpnt *conv_frac(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 	p = arb_calloc(k * 2, sizeof(ARBT));
 	o = arb_calloc(k * 2, sizeof(ARBT));
 	
-	ARBT *array = arb_calloc(z * 2, sizeof(ARBT));
-	memcpy(array, a->number, a->len * sizeof(ARBT));
+	ARBT *frac = arb_calloc(z * 2, sizeof(ARBT));
+	memcpy(frac, a->number, a->len * sizeof(ARBT));
 	for (i = 0; i < k; ++i) {
 		memset(o, 0, z * sizeof(ARBT)); 
-		len = arb_mul_core(array, z, obh->number, obh->len, o, ibase);
+		len = arb_mul_core(frac, z, obh->number, obh->len, o, ibase);
 		size = len - z;
 		p[i] = arb2hrdware(o, size , 10); /* note: absorbs leading zeros */
-		_arb_copy_core(array, o + size , z);
+		_arb_copy_core(frac, o + size , z);
         }
 
 	b->number = p;
@@ -72,40 +72,3 @@ fxdpnt *conv_frac(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 	arb_free(obh);
 	return b;
 }
-
-
-fxdpnt *allconv(fxdpnt *a, fxdpnt *b, int ibase, int obase)
-{
-	arb_copy(b, a);
-	ARBT *p;
-	int carry = 0;
-	int prod = 0;
-	size_t i = 0;
-	size_t j = 0;
-	size_t k = 0;
-	/* TODO: use an algorithm that doesn't require log() for # digits */
-	if (ibase > obase)
-		/* ln(x) / ln(base) == ln_base(x) */
-		k = (size_t) ((a->len* ibase) / log10(obase));
-	else 
-		k = a->len;
-	p = arb_calloc(k, sizeof(ARBT));
-	ARBT *array = arb_calloc(k, sizeof(ARBT));
-	memcpy(array + (k - a->len), a->number, a->len * sizeof(ARBT));
-	memset(array, 0, (k - a->len) * sizeof(ARBT));
-
-	for (; i < k; ++i) { 
-		carry = array[i];
-		prod = 0;
-		for (j = k; j > 0; j--) {
-			prod = (p[j-1] * ibase) + carry;
-			p[j-1] = prod % obase;
-			carry = prod / obase;
-		}
-	}
-	b->number = p;
-	b->len = k;
-	b->lp = k;
-	return b;
-}
-
