cff9bef29d40c5d377c3111a58141587c2c286d1
cmgraff <cm0graff@gmail.com>
2018-01-09 02:08:48 -0600

    convall is bulked out

--- arbprec/src/convert.c
+++ arbprec/src/convert.c
@@ -72,3 +72,69 @@ fxdpnt *conv_frac(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 	arb_free(obh);
 	return b;
 }
+
+
+fxdpnt *convall(fxdpnt *a, fxdpnt *b, int ibase, int obase)
+{
+	arb_copy(b, a);
+	ARBT *sv;
+	long long carry = 0;
+	long long prod = 0;
+	size_t i = 0;
+	size_t j = 0;
+	size_t k = 0;
+	size_t z = 0;
+	size_t len = 0;
+	size_t size = 0;
+	
+	ARBT *p;
+	ARBT *o;
+	if (ibase > obase)
+		/* ln(x) / ln(base) == ln_base(x) */
+		k = (size_t) ((2*a->len) / log10(obase)) ;
+	else 
+		k = a->len;
+	sv = arb_calloc(k, sizeof(ARBT));
+	ARBT *real = arb_calloc(k, sizeof(ARBT));
+	memcpy(real + (k - a->len), a->number, a->len * sizeof(ARBT));
+	memset(real, 0, (k - a->len) * sizeof(ARBT));
+
+	for (i = 0; i < k; ++i) { 
+		carry = real[i];
+		prod = 0;
+		for (j = k; j > 0; j--) {
+			prod = (sv[j-1] * ibase) + carry;
+			sv[j-1] = prod % obase;
+			carry = prod / obase;
+		}
+	}
+	if (carry)
+		printf("we have a left over carry\n");
+	b->number = sv;
+	b->len = k;
+	b->lp = k;
+	
+	
+	fxdpnt *obh = hrdware2arb(obase);
+	
+	k = a->len;
+	p = arb_calloc(k * 2, sizeof(ARBT));
+	o = arb_calloc(k * 2, sizeof(ARBT));
+	
+	ARBT *frac = arb_calloc(z * 2, sizeof(ARBT));
+	memcpy(frac, a->number, a->len * sizeof(ARBT));
+	for (i = 0; i < k; ++i) {
+		memset(o, 0, z * sizeof(ARBT)); 
+		len = arb_mul_core(frac, z, obh->number, obh->len, o, ibase);
+		size = len - z;
+		p[i] = arb2hrdware(o, size , 10); /* note: absorbs leading zeros */
+		_arb_copy_core(frac, o + size , z);
+        }
+
+	b->number = p;
+	b->len = z;
+	b->lp = 0; 
+
+	
+	return b;
+}
