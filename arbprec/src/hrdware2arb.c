50ddcd2a0d2652a8266f48808b8d1df49fcd0768
cmgraff <cm0graff@gmail.com>
2018-01-09 03:10:25 -0600

    use native numerical conversion

--- arbprec/src/hrdware2arb.c
+++ arbprec/src/hrdware2arb.c
@@ -1,9 +1,26 @@
 #include <arbprec/arbprec.h>
+static int __convtab[20] = { '0', '1', '2', '3', '4', '5', '6', '7',
+                             '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+static size_t __uint2str_inter(char *s, size_t n, int base, size_t i)
+{
+        if (n / base) {
+                i = __uint2str_inter(s, n / base, base, i);
+        }
+        s[i] = __convtab[(n % base)];
+        return ++i;
+}
+
+static size_t __uint2str(char *s, size_t n, int base)
+{
+        size_t i = 0;
+        return __uint2str_inter(s, n, base, i);
+}
 
 fxdpnt *hrdware2arb(size_t a)
 {
-	/* TODO: this is not very efficient, replace it with itoa style code */
-	char str[50]; /* there is no hardware type this long */
+
+	char str[50];
 	sprintf(str, "%zu", a);
 	fxdpnt *ret = arb_str2fxdpnt(str);
 	return ret;
