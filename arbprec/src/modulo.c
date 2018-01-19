824651367ec8abf8fb81c8fb90f4fe2783411ea0
cmgraff <cm0graff@gmail.com>
2018-01-19 14:55:21 -0600

    free memory

--- arbprec/src/modulo.c
+++ arbprec/src/modulo.c
@@ -1,30 +1,5 @@
-/*
-int
-bc_modulo (num1, num2, result, scale)
-     bc_num num1, num2, *result;
-     int scale;
-{
-  bc_num temp;
-  int rscale;
-
-  // Check for correct numbers. 
-  if (is_zero (num2)) return -1;
-
-  // Calculate final scale.
-  rscale = MAX (num1->n_scale, num2->n_scale+scale);
-  init_num (&temp);
-
-  // Calculate it. 
-  bc_divide (num1, num2, &temp, scale);
-  bc_multiply (temp, num2, &temp, rscale);
-  bc_sub (num1, temp, result);
-  free_num (&temp);
-
-  return 0;     // Everything is OK. 
-}
-
-*/
 #include <arbprec/arbprec.h>
+
 fxdpnt *arb_mod(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 {
 	size_t newscale = MAX(a->len, b->len + scale);
@@ -33,5 +8,7 @@ fxdpnt *arb_mod(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 	tmp = arb_alg_d(a, b, tmp, base, scale);
 	tmp2 = arb_mul(tmp, b, tmp2, base, newscale);
 	c = arb_sub(a, tmp2, c, base);
+	free(tmp);
+	free(tmp2);
 	return c;
 }
