ae946edbfc9b10d76c7a81bdd77ef21f080bd4e8
cmgraff <cm0graff@gmail.com>
2018-01-19 18:37:11 -0600

    giant mul shows bugs

--- arbprec/tests/long-mul-giant.c
+++ arbprec/tests/long-mul-giant.c
@@ -1,5 +1,5 @@
 #include <arbprec/arbprec.h>
-#define MAXIMA 1000000
+#define MAXIMA 1000
 int main(int argc, char *argv[])
 {
 
@@ -19,17 +19,23 @@ int main(int argc, char *argv[])
 		string2[i] = (i % 9) + '0';
 		++i;
 	}
-	fprintf(stderr, "past initialization\n");
-	write(1, string1, MAXIMA);
-	write(1, string2, MAXIMA);
-	fprintf(stderr, "past confirmation\n");
+	//fprintf(stderr, "past initialization\n");
+	write(2, string1, MAXIMA);
+	write(2, "*", 1);
+	write(2, string2, MAXIMA);
+	write(2, "\n", 1);
+	write(2, "quit\n", 5);
+
+	//fprintf(stderr, "past confirmation\n");
 	int base = 10;
 	fxdpnt *a, *b, *c;
 
 	a = arb_str2fxdpnt(string1);
 	b = arb_str2fxdpnt(string2);
-	c = arb_expand(NULL, 1);
-	c = arb_mul(a, b, c, base, 10);
+	arb_print(a);
+	arb_print(b);
+	c = arb_expand(NULL, MAXIMA*2);
+	c = arb_mul(a, b, c, 10, 10);
 	arb_print(c);
 	arb_free(a);
 	arb_free(b);
