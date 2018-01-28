f0e30499a0bda54627e7da94ffbe8cf37e97dfe5
cmgraff <cm0graff@gmail.com>
2018-01-27 23:11:16 -0600

    test a = function(a, a, a)

--- tests/ag-add.c
+++ tests/ag-add.c
@@ -2,21 +2,15 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 4 )
-		arb_error("Needs 3 args, such as:  123 / 123 base");
- 
+	if (argc < 3 )
+		arb_error("Needs 2 args, such as: 123 base");
 	
-	int base = strtol(argv[3], 0, 10);
-	fxdpnt *a, *b, *c;
+	int base = strtol(argv[2], 0, 10);
+	fxdpnt *a;
 	a = arb_str2fxdpnt(argv[1]);
-	b = arb_str2fxdpnt(argv[2]);
-	c = arb_expand(NULL, 1);
 	a = arb_add(a, a, a, base);
 	arb_print(a);
 	arb_free(a);
-	arb_free(b);
-	arb_free(c);
-
 	return 0;
 }
 
--- tests/ag-div.c
+++ tests/ag-div.c
@@ -2,13 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3 )
-		arb_error("Needs 2 args, such as: 123 base");
+	if (argc < 4 )
+		arb_error("Needs 3 args, such as: 123 base scale");
 	
 	int base = strtol(argv[2], 0, 10);
+	int scale = strtol(argv[3], 0, 10);
 	fxdpnt *a;
 	a = arb_str2fxdpnt(argv[1]);
-	a = arb_sub(a, a, a, base);
+	a = arb_alg_d(a, a, a, base, scale);
 	arb_print(a);
 	arb_free(a);
 	return 0;
