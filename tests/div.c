b96754972a9e427c41c036387c793a0dcc5e3649
cmgraff <cm0graff@gmail.com>
2018-01-28 16:09:19 -0600

    redundant allocations caused memory leak

--- tests/div.c
+++ tests/div.c
@@ -8,19 +8,19 @@ int main(int argc, char *argv[])
                 return 0;
         }
 	
-        int base = strtol(argv[3],0,  10);
-        int scale =  strtol(argv[4],0,  10);
+        int base = strtol(argv[3], 0, 10);
+        int scale =  strtol(argv[4], 0, 10);
 	fxdpnt *a = NULL;
 	fxdpnt *b = NULL;
 	fxdpnt *c = NULL;
-	a = arb_expand(a, 1);
-	b = arb_expand(b, 1);
-	c = arb_expand(c, scale);
-	//arb_init(c);
+	c = arb_expand(c, 1);
         a = arb_str2fxdpnt(argv[1]);
         b = arb_str2fxdpnt(argv[2]);
 	c = arb_alg_d(a, b, c, base, scale);
         arb_print(c);
+	arb_free(a);
+	arb_free(b);
+	arb_free(c);
         return 0;
 }
 
