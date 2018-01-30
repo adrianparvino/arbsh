10688b5f8465bd98c90b72db2e96e3445c703425
cmgraff <cm0graff@gmail.com>
2018-01-30 15:20:39 -0600

    remove rp

--- src/long-multiplication.c
+++ src/long-multiplication.c
@@ -28,22 +28,20 @@ size_t arb_mul_core(ARBT *a, size_t alen, ARBT *b, size_t blen, ARBT *c, int bas
 
 fxdpnt *arb_mul(fxdpnt *a, fxdpnt *b, fxdpnt *c, int base, size_t scale)
 {
-	arb_setsign(a, b, c);
-	c = arb_expand(c, a->len + b->len);
-	fxdpnt *a2 = arb_expand(NULL, MAX(scale, a->len));
-	fxdpnt *b2 = arb_expand(NULL, MAX(scale, b->len));
-	fxdpnt *c2 = arb_expand(NULL, a->len + b->len);
-	arb_copy(a2, a);
-	arb_copy(b2, b);
-	arb_mul_core(a2->number, a2->len, b2->number, b2->len, c2->number, base);
-	arb_copy(c, c2);
-	c->lp = a2->lp + b2->lp;
-	c->rp = MIN(a2->rp + b2->rp, MAX(scale, MAX(a2->rp, b2->rp)));
-	c->len = c->rp + c->lp;
-	c = remove_leading_zeros(c);
-	arb_free(a2);
-	arb_free(b2);
-	arb_free(c2);
-	return c;
+        arb_setsign(a, b, c);
+        c = arb_expand(c, a->len + b->len);
+        fxdpnt *a2 = arb_expand(NULL, MAX(scale, a->len));
+        fxdpnt *b2 = arb_expand(NULL, MAX(scale, b->len));
+        fxdpnt *c2 = arb_expand(NULL, a->len + b->len);
+        arb_copy(a2, a);
+        arb_copy(b2, b);
+        arb_mul_core(a2->number, a2->len, b2->number, b2->len, c2->number, base);
+        arb_copy(c, c2);
+        c->lp = a2->lp + b2->lp;
+        c->len = MIN(rr(a2) + rr(b2), MAX(scale, MAX(rr(a2), rr(b2)))) + c->lp;
+        c = remove_leading_zeros(c);
+        arb_free(a2);
+        arb_free(b2);
+        arb_free(c2);
+        return c;
 }
-
