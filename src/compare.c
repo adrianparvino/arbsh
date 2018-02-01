4fe2e5b57f6b2728c395e1462d0126fda802e560
Bao Hexing <HexingB@qq.com>
2018-02-01 15:48:25 +0800

    add remove_trailing_zeros() and rewrite arb_compare()

--- src/compare.c
+++ src/compare.c
@@ -1,5 +1,6 @@
 #include <arbprec/arbprec.h>
 
+#if 0 // remove this after fully test of new implementation
 int arb_compare(fxdpnt *a, fxdpnt *b, int base)
 {
 	size_t i = 0;
@@ -24,4 +25,52 @@ int arb_compare(fxdpnt *a, fxdpnt *b, int base)
 	arb_free(c);
 	return 0;
 }
+#endif
 
+int arb_compare(fxdpnt *a, fxdpnt *b, int base)
+{
+	size_t i;
+	int result = 0;
+	if (a->sign == '-' && b->sign == '+')
+		return -1;
+	if (a->sign == '+' && b->sign == '-')
+		return 1;
+
+	a = remove_leading_zeros(a);
+	b = remove_leading_zeros(b);
+
+	a = remove_trailing_zeros(a);
+	b = remove_trailing_zeros(b);
+
+	if (a->lp > b->lp) {
+		result = 1;
+	} else if (a->lp < b->lp) {
+		result = -1;
+	} else {
+		for (i = 0; i < a->len && i < b->len; ++i) {
+			if (a->number[i] > b->number[i]) {
+				result = 1;
+				break;
+			} else if (a->number[i] < b->number[i]) {
+				result = -1;
+				break;
+			}
+		}
+
+		// a->lp == b->lp, so fractional parts have different length
+		if (i == a->len && i < b->len) {
+			result = -1;
+		}
+
+		if (i == b->len && i < a->len) {
+			result = 1;
+		}
+	}
+
+	if (a->sign == '-') { // and also b->sign == '-'
+		result = 0 - result;
+	}
+
+	(void)base;
+	return result;
+}
