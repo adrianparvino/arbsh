40b6e2c1147f477b678e86a1c90f3715104668d6
cmgraff <cm0graff@gmail.com>
2018-02-01 04:25:08 -0600

    get rid of original compare()

--- src/compare.c
+++ src/compare.c
@@ -1,32 +1,5 @@
 #include <arbprec/arbprec.h>
 
-#if 0 // remove this after fully test of new implementation
-int arb_compare(fxdpnt *a, fxdpnt *b, int base)
-{
-	size_t i = 0;
-	int sign = a->sign;
-	if (a->sign == '-' && b->sign == '+')
-		return -1;
-	if (a->sign == '+' && b->sign == '-')
-		return 1;
-	fxdpnt *c = NULL;
-	c = arb_expand(NULL, a->len + b->len); //FIXME initialize at runtime
-	c = arb_sub(a, b, c, base);
-	if (c->sign != sign){
-		arb_free(c);
-		return -1;
-	}
-	for (;i< c->len;++i){
-		if (c->number[i] != 0){
-			arb_free(c);
-			return 1;
-		}
-	}
-	arb_free(c);
-	return 0;
-}
-#endif
-
 int arb_compare(fxdpnt *a, fxdpnt *b, int base)
 {
 	size_t i;
@@ -38,9 +11,6 @@ int arb_compare(fxdpnt *a, fxdpnt *b, int base)
 
 	a = remove_leading_zeros(a);
 	b = remove_leading_zeros(b);
-
-	a = remove_trailing_zeros(a);
-	b = remove_trailing_zeros(b);
 
 	if (a->lp > b->lp) {
 		result = 1;
@@ -74,3 +44,4 @@ int arb_compare(fxdpnt *a, fxdpnt *b, int base)
 	(void)base;
 	return result;
 }
+
