af6193448c6a131d2cc62aed667802de3bd6cf76
Bao Hexing <HexingB@qq.com>
2018-02-01 11:18:38 +0800

    add tests/comp.c

---
+++ tests/comp.c
@@ -0,0 +1,25 @@
+#include <arbprec/arbprec.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 4 )
+		arb_error("Needs 3 args, such as: 123 123 base");
+	
+	int base = strtol(argv[3], 0, 10);
+	fxdpnt *a, *b;
+	a = arb_str2fxdpnt(argv[1]);
+	b = arb_str2fxdpnt(argv[2]);
+	int c = arb_compare(a, b, base);
+    if (c > 0) {
+        fprintf(stdout, ">\n");
+    } else if (c < 0) {
+        fprintf(stdout, "<\n");
+    } else {
+        fprintf(stdout, "==\n");
+    }
+	arb_free(a);
+	arb_free(b);
+
+	return 0;
+}
+
