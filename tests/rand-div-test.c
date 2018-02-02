6fc4124c26f11570cfa53716665e9dc18d3080d7
cmgraff <cm0graff@gmail.com>
2018-02-01 22:42:49 -0600

    make pseudo-random tests have psuedo-random lengths

--- tests/rand-div-test.c
+++ tests/rand-div-test.c
@@ -5,30 +5,35 @@
 int main(void)
 {
 
-	char *string1 = malloc(MAXIMA + 1);
-	char *string2 = malloc(MAXIMA + 1);
-	
-	if (!string1 || !string2)
-		return 1;
-
 	size_t i = 0;
 
 	srandom(time(NULL));
 
-	while ( i < MAXIMA )
+	size_t MAXIMA1 = (random() % MAXIMA);
+	size_t MAXIMA2 = (random() % MAXIMA);
+	
+	char *string1 = malloc(MAXIMA1 + 1);
+	char *string2 = malloc(MAXIMA2 + 1);
+	
+	if (!string1 || !string2)
+		return 1;
+
+
+	while ( i < MAXIMA1 )
 	{
 		string1[i] = (random() % 10) + '0';
 		++i;
 	}
+	string1[i] = 0;
 	i = 0;
-	while ( i < MAXIMA )
+	while ( i < MAXIMA2 )
 	{
 		string2[i] = (random() % 10) + '0';
 		++i;
 	}
-	string1[(random() % MAXIMA)] = '.';
-	string2[(random() % MAXIMA)] = '.';
-	string1[i] = 0;
+	string1[(random() % MAXIMA1)] = '.';
+	string2[(random() % MAXIMA2)] = '.';
+	
 	string2[i] = 0;
 	size_t scale = random() % 10000;
 	fprintf(stderr, "scale=%zu;\n", scale);
