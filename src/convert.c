f4f949f7e361acd33fa01c2d09d3c54f300fbb7e
cmgraff <cm0graff@gmail.com>
2018-02-04 13:00:49 -0600

    rewrite conversion code to use less data copying

--- src/convert.c
+++ src/convert.c
@@ -6,54 +6,52 @@ fxdpnt *convscaled(fxdpnt *a, fxdpnt *b, int ibase, int obase)
 {
 	char digi[] = "0123456789ABCDEF";
 	arb_copy(b, a);
-	ARBT *sv;
 	long long carry = 0;
 	long long prod = 0;
 	size_t i = 0;
 	size_t j = 0;
 	size_t k = 0;
-	size_t z = 0;
 	size_t digit = 0;
-	
-	ARBT *p;
+	size_t q = 0;
 
-	// real
+	// integer
 	if (ibase > obase)
-		k = (size_t) ((2*a->lp) / log10(obase)) ;
+		k = (size_t) ((2*a->lp) / log10(obase));
 	else 
 		k = a->lp;
-	sv = arb_calloc(k, sizeof(ARBT));
-	ARBT *real = arb_calloc(k, sizeof(ARBT));
-	memcpy(real + (k - a->lp), a->number, a->lp * sizeof(ARBT));
-	memset(real, 0, (k - a->lp) * sizeof(ARBT));
-
+	
+	b = arb_expand(b, k);
+	memset(b->number, 0, k*sizeof(ARBT)); // something is wrong with arb_expand
+	b->len = b->lp = k;
+	
 	for (i = 0; i < k; ++i) { 
-		carry = real[i];
+		if ( i >= k-(a->lp))
+			carry = a->number[q++];
+		else {
+			q = 0;
+			carry = 0;
+		}
 		prod = 0;
 		for (j = k; j > 0; j--) {
-			prod = (sv[j-1] * ibase) + carry;
-			sv[j-1] = prod % obase;
+			prod = (b->number[j-1] * ibase) + carry;
+			b->number[j-1] = prod % obase;
 			carry = prod / obase;
 		}
 	}
-	b = arb_expand(b, k);
-	_arb_copy_core(b->number, sv, k);
-	b->len = k;
-	b->lp = k;
 	
-	// fractional
-	fxdpnt *obh = hrdware2arb(obase);
+	// non-integer
+	fxdpnt *ob = hrdware2arb(obase);
 	fxdpnt *ofrac = arb_expand(NULL, rr(a)*2);
 	memcpy(ofrac->number, a->number + a->lp, rr(a) * sizeof(ARBT));
 	ofrac->len = rr(a);
 	ofrac->lp = 0;
-	fxdpnt *intpart = arb_expand(NULL, a->lp);
+	fxdpnt *z = arb_expand(NULL, a->lp);
 	fxdpnt *t = arb_str2fxdpnt("1");
 	for (i = 0; t->len <= rr(a); ++i) {
-		ofrac = arb_mul(ofrac, obh, ofrac, ibase, 10);
+		ofrac = arb_mul(ofrac, ob, ofrac, ibase, 10);
 		digit = fxd2sizet(ofrac, 10);
-		intpart = hrdware2arb(digit);
-		ofrac = arb_sub(ofrac, intpart, ofrac, 10);
+		z = hrdware2arb(digit);
+		ofrac = arb_sub(ofrac, z, ofrac, 10);
 		b = arb_expand(b, (b->lp + i));
 		b->number[b->lp + i] = digi[digit];
 		b->len++;
