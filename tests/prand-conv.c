99f830e86eab9334fa75f2a132609e73109a4738
cmgraff <cm0graff@gmail.com>
2018-02-04 01:15:03 -0600

    clean up tests

--- tests/prand-conv.c
+++ tests/prand-conv.c
@@ -1,23 +1,19 @@
 #include <arbprec/arbprec.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAXIMA 100
-int main(int argc, char *argv[])
+#define MAXIMA 10000
+int main(void)
 {
-
 	size_t i = 0;
 
 	srandom(time(NULL));
 
 	size_t MAXIMA1 = (random() % MAXIMA);
-
 	
 	char *string1 = malloc(MAXIMA1 + 1);
-
 	
 	if (!string1)
 		return 1;
-
 
 	while ( i < MAXIMA1 )
 	{
@@ -30,9 +26,6 @@ int main(int argc, char *argv[])
 
 	size_t scale = random() % 10000;
 	fprintf(stderr, "scale=%zu;\n", scale);
-	
-	
-		
 
 	fxdpnt *a;
 	a = arb_str2fxdpnt(string1);
@@ -40,10 +33,8 @@ int main(int argc, char *argv[])
 
 	int base = 0;
 	while ( base <2 || (base > 10 && base < 16))
-	{
 		base = random() % 16;
-		//printf("finding scale\n");
-	}
+
 	fprintf(stderr, "ibase=10;obase=%d;%s+0\nquit\n", base, string1);
 	a = convscaled(a, b, 10, base);
 	
