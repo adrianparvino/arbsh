cd44ea5a19a350b713eee7a19b68d9efc731669d
cmgraff <cm0graff@gmail.com>
2018-02-13 14:56:15 -0600

    use prand-a-a-a instead of the normal program

--- tests/prand-a-a-a-wrapper.sh
+++ tests/prand-a-a-a-wrapper.sh
@@ -9,11 +9,11 @@ COUNT=0
 
 while [ "$COUNT" -lt 100 ]
 do	COUNT=$((COUNT +1))
-	./tests/pseudo-random-tests $1 2> some.bc > log
+	./tests/prand-a-a-a $1 2> some.bc > log
 	bc -lq some.bc > log2
 	if diff log log2
 	then	printf "%s\n" "Giant pseudo-random number test passed."
-	else	printf"%s\n"  "Test failed! look inside of 'some.bc' to reveal the failing numbers!"
+	else	printf "%s\n"  "Test failed! look inside of 'some.bc' to reveal the failing numbers!"
 		exit 1
 	fi
 	cat some.bc >> tests_passed.txt
